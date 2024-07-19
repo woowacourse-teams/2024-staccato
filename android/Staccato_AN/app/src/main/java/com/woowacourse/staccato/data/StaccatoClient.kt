63dc806b72eea5e0c7dd13c6c224b23079ea19ae
Junyoung-WON <wjyo60@gmail.com>
2024-07-19 21:27:15 +0900

    feat: Retrofit Client 작성

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/StaccatoClient.kt
@@ -0,0 +1,35 @@
+package com.woowacourse.staccato.data
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import com.woowacourse.staccato.BuildConfig
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+object StaccatoClient {
+    private const val BASE_URL = BuildConfig.BASE_URL
+
+    private val provideLoggingInterceptor = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+
+    private val provideHttpClient = OkHttpClient.Builder()
+        .addInterceptor(provideLoggingInterceptor)
+        .build()
+
+    private val jsonBuilder = Json { coerceInputValues = true }
+
+    private val provideRetrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .client(provideHttpClient)
+        .addConverterFactory(
+            jsonBuilder.asConverterFactory("application/json".toMediaType())
+        )
+        .build()
+
+    fun <T> create(service: Class<T>): T {
+        return provideRetrofit.create(service)
+    }
+}
