a746161d712e65f948ba910c00e541c4dcbbf651
somin <salth0601@gmail.com>
2024-07-20 11:01:46 +0900

    feat: TravelFragment 임시 화면 추가

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travel/TravelFragment.kt
@@ -0,0 +1,22 @@
+package com.woowacourse.staccato.presentation.travel
+
+import android.os.Bundle
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.woowacourse.staccato.R
+import com.woowacourse.staccato.databinding.FragmentTravelBinding
+import com.woowacourse.staccato.presentation.base.BindingFragment
+
+class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_travel) {
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        binding.btnTravelCreation.setOnClickListener {
+            findNavController().navigate(R.id.action_travelFragment_to_travelCreationFragment)
+        }
+        binding.btnVisit.setOnClickListener {
+            findNavController().navigate(R.id.action_travelFragment_to_visitFragment)
+        }
+    }
+}
