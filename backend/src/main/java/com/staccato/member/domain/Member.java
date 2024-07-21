3e6ea6e4a43edc6792b48d600d5604f34ff4d642
yoonjuho <dbswn990@naver.com>
2024-07-22 02:52:50 +0900

    feat: Member Entity 구성

---
+++ backend/src/main/java/com/staccato/member/domain/Member.java
@@ -0,0 +1,26 @@
+package com.staccato.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import com.staccato.config.domain.BaseEntity;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "member")
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String nickname;
+    @Column(columnDefinition = "TEXT")
+    private String imageUrl;
+}
