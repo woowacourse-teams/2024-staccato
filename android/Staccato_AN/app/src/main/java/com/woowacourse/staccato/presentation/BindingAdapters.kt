c11bb728e8ba4ef76006fd321601182791b23d15
Junyoung-WON <wjyo60@gmail.com>
2024-07-22 20:38:09 +0900

    feat: 이미지 로딩 바인딩 어댑터 설정
    - Glide, Coil 바인딩 어댑터를 각각 작성
    - placeholder 설정

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/BindingAdapters.kt
@@ -0,0 +1,60 @@
+package com.woowacourse.staccato.presentation
+
+import android.graphics.drawable.Drawable
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import coil.load
+import coil.transform.RoundedCornersTransformation
+
+@BindingAdapter("coilImageUrl")
+fun ImageView.loadImageWithCoil(
+    url: String?,
+    placeHolder: Drawable? = null,
+) {
+    url?.let {
+        this.load(it) {
+            placeholder(placeHolder)
+            error(placeHolder)
+        }
+    }
+}
+
+@BindingAdapter("coilCircleImageUrl")
+fun ImageView.setCircleImageWithCoil(
+    url: String?,
+    placeHolder: Drawable? = null,
+) {
+    url?.let {
+        this.load(it) {
+            placeholder(placeHolder)
+            transformations(RoundedCornersTransformation(1000f))
+            error(placeHolder)
+        }
+    }
+}
+
+@BindingAdapter("glideImageUrl")
+fun ImageView.loadImageWithGlide(
+    url: String?,
+    placeHolder: Drawable? = null,
+) {
+    Glide.with(context)
+        .load(url)
+        .placeholder(placeHolder)
+        .error(placeHolder)
+        .into(this)
+}
+
+@BindingAdapter("glideCircleImageUrl")
+fun ImageView.setCircleImageWithGlide(
+    url: String?,
+    placeHolder: Drawable? = null,
+) {
+    Glide.with(context)
+        .load(url)
+        .placeholder(placeHolder)
+        .circleCrop()
+        .error(placeHolder)
+        .into(this)
+}
