0f1fb227832124f405ffa21f2be2e552203421ee
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-23 13:29:02 +0900

    feat: 삭제 다이얼로그, 사진 첨부 바텀 시트 fragment 추가

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/PhotoAttachFragment.kt
@@ -0,0 +1,29 @@
+package com.woowacourse.staccato
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.woowacourse.staccato.databinding.FragmentPhotoAttachBinding
+
+class PhotoAttachFragment : BottomSheetDialogFragment() {
+    private var _binding: FragmentPhotoAttachBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPhotoAttachBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        const val TAG = "PhotoAttachModalBottomSheet"
+    }
+}
