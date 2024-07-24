a78bc559d85ff0ecba6cf103ddb636d114165d53
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-24 20:22:25 +0900

    feat: 함께 간 사람들, 방문 기록 adapter 연결

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travel/TravelFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travel/TravelFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.viewModels
 import com.woowacourse.staccato.R
 import com.woowacourse.staccato.databinding.FragmentTravelBinding
 import com.woowacourse.staccato.presentation.base.BindingFragment
+import com.woowacourse.staccato.presentation.travel.adapter.MatesAdapter
+import com.woowacourse.staccato.presentation.travel.adapter.VisitsAdapter
 import com.woowacourse.staccato.presentation.travel.viewmodel.TravelViewModel
 import com.woowacourse.staccato.presentation.travel.viewmodel.TravelViewModelFactory
 
@@ -14,11 +16,16 @@ class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_
         TravelViewModelFactory()
     }
 
+    private lateinit var matesAdapter: MatesAdapter
+    private lateinit var visitsAdapter: VisitsAdapter
+
     override fun onViewCreated(
         view: View,
         savedInstanceState: Bundle?,
     ) {
         initBinding()
+        initMatesAdapter()
+        initVisitsAdapter()
         observeTravel()
     }
 
@@ -30,7 +37,18 @@ class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_
     private fun observeTravel() {
         viewModel.loadTravel()
         viewModel.travel.observe(viewLifecycleOwner) { travel ->
-            // TODO: mates, visits adapter update
+            matesAdapter.updateMates(travel.mates)
+            visitsAdapter.updateVisits(travel.visits)
         }
+    }
+
+    private fun initMatesAdapter() {
+        matesAdapter = MatesAdapter()
+        binding.rvTravelMates.adapter = matesAdapter
+    }
+
+    private fun initVisitsAdapter() {
+        visitsAdapter = VisitsAdapter()
+        binding.rvTravelVisits.adapter = visitsAdapter
     }
 }
