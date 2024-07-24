0c26dd7f23751932dbdac97b97ae3d81bc4776da
BurningFalls <sherlocky31415@gmail.com>
2024-07-24 19:11:48 +0900

    feat: 방문 기록 생성 기능 구현

--- backend/src/main/java/com/staccato/visit/controller/VisitController.java
+++ backend/src/main/java/com/staccato/visit/controller/VisitController.java
@@ -1,9 +1,18 @@
 package com.staccato.visit.controller;
 
+import java.net.URI;
+
+import jakarta.validation.Valid;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.staccato.visit.service.VisitService;
+import com.staccato.visit.service.dto.request.VisitRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -12,4 +21,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class VisitController {
     private final VisitService visitService;
+
+    @PostMapping
+    public ResponseEntity<Void> createVisit(@Valid @RequestBody VisitRequest visitRequest) {
+        long visitId = visitService.createVisit(visitRequest);
+        return ResponseEntity.created(URI.create("/visits/" + visitId)).build();
+    }
 }
--- backend/src/main/java/com/staccato/visit/service/VisitService.java
+++ backend/src/main/java/com/staccato/visit/service/VisitService.java
@@ -1,8 +1,20 @@
 package com.staccato.visit.service;
 
+import java.util.List;
+
+import jakarta.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
+import com.staccato.pin.domain.Pin;
+import com.staccato.pin.repository.PinRepository;
+import com.staccato.travel.domain.Travel;
+import com.staccato.travel.repository.TravelRepository;
+import com.staccato.visit.domain.Visit;
+import com.staccato.visit.domain.VisitImage;
+import com.staccato.visit.repository.VisitImageRepository;
 import com.staccato.visit.repository.VisitRepository;
+import com.staccato.visit.service.dto.request.VisitRequest;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +22,38 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class VisitService {
     private final VisitRepository visitRepository;
+    private final PinRepository pinRepository;
+    private final TravelRepository travelRepository;
+    private final VisitImageRepository visitImageRepository;
+
+    @Transactional
+    public long createVisit(VisitRequest visitRequest) {
+        Pin pin = getPinById(visitRequest.pinId());
+        Travel travel = getTravelById(visitRequest.travelId());
+        Visit visit = visitRepository.save(visitRequest.toVisit(pin, travel));
+
+        List<VisitImage> visitImages = makeVisitImages(visitRequest.visitedImages(), visit);
+        visitImageRepository.saveAll(visitImages);
+
+        return visit.getId();
+    }
+
+    private List<VisitImage> makeVisitImages(List<String> visitedImages, Visit visit) {
+        return visitedImages.stream()
+                .map(visitImage -> VisitImage.builder()
+                        .imageUrl(visitImage)
+                        .visit(visit)
+                        .build())
+                .toList();
+    }
+
+    private Pin getPinById(long pinId) {
+        return pinRepository.findById(pinId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Operation"));
+    }
+
+    private Travel getTravelById(long travelId) {
+        return travelRepository.findById(travelId)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid Operation"));
+    }
 }
