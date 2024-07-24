fb2ac6706b8f109df1e03744700713663b8aa080
BurningFalls <sherlocky31415@gmail.com>
2024-07-24 19:35:21 +0900

    style: 코드 컨벤션 적용

--- backend/src/main/java/com/staccato/visit/controller/VisitController.java
+++ backend/src/main/java/com/staccato/visit/controller/VisitController.java
@@ -1,15 +1,14 @@
 package com.staccato.visit.controller;
 
+import java.net.URI;
+
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.net.URI;
-
-import jakarta.validation.Valid;
-
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
