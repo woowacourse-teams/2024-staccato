774e8fcbc423ff3494f63d31ad3fb0b396360e8a
yoonjuho <dbswn990@naver.com>
2024-07-24 19:27:14 +0900

    refactor: Transactional 읽기 전용 옵션 구성

--- backend/src/main/java/com/staccato/visit/service/VisitService.java
+++ backend/src/main/java/com/staccato/visit/service/VisitService.java
@@ -7,12 +7,13 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class VisitService {
     private final VisitRepository visitRepository;
     private final VisitLogRepository visitLogRepository;
 
+    @Transactional
     public void deleteById(Long visitId) {
         visitLogRepository.deleteByVisitId(visitId);
         visitRepository.deleteById(visitId);
