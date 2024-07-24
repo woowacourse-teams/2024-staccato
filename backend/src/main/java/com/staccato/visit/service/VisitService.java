34fadbc4a89332c69aa2d5be5614083c1bf64ab9
BurningFalls <sherlocky31415@gmail.com>
2024-07-24 19:34:52 +0900

    fix: 오타 수정

--- backend/src/main/java/com/staccato/visit/service/VisitService.java
+++ backend/src/main/java/com/staccato/visit/service/VisitService.java
@@ -17,12 +17,9 @@ import com.staccato.visit.repository.VisitRepository;
 import com.staccato.visit.service.dto.request.VisitRequest;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional(readOnly = true)
-@Transactional
 @RequiredArgsConstructor
 public class VisitService {
     private final VisitRepository visitRepository;
@@ -60,6 +57,7 @@ public class VisitService {
         return travelRepository.findById(travelId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid Operation"));
     }
+
     private final VisitLogRepository visitLogRepository;
 
     public void deleteById(Long visitId) {
