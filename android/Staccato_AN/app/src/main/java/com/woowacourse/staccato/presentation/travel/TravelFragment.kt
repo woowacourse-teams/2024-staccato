a11c5fe68195fab88ccb59dd3b6e99052a47deff
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-24 20:31:29 +0900

    feat: 여행 상세 화면 toolbar handler 구현
    - 뒤로가기
    - 여행 수정 화면으로 이동
    - 삭제 다이얼로그 show

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travel/TravelFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travel/TravelFragment.kt
@@ -2,16 +2,23 @@ package com.woowacourse.staccato.presentation.travel
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.woowacourse.staccato.DeleteDialogFragment
 import com.woowacourse.staccato.R
 import com.woowacourse.staccato.databinding.FragmentTravelBinding
+import com.woowacourse.staccato.presentation.ToolbarHandler
 import com.woowacourse.staccato.presentation.base.BindingFragment
+import com.woowacourse.staccato.presentation.main.MainActivity
 import com.woowacourse.staccato.presentation.travel.adapter.MatesAdapter
 import com.woowacourse.staccato.presentation.travel.adapter.VisitsAdapter
 import com.woowacourse.staccato.presentation.travel.viewmodel.TravelViewModel
 import com.woowacourse.staccato.presentation.travel.viewmodel.TravelViewModelFactory
+import com.woowacourse.staccato.presentation.travelupdate.TravelUpdateActivity
 
-class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_travel) {
+class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_travel),
+    ToolbarHandler {
     private val viewModel: TravelViewModel by viewModels {
         TravelViewModelFactory()
     }
@@ -24,6 +31,7 @@ class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_
         savedInstanceState: Bundle?,
     ) {
         initBinding()
+        initToolbar()
         initMatesAdapter()
         initVisitsAdapter()
         observeTravel()
@@ -32,6 +40,13 @@ class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_
     private fun initBinding() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
+        binding.toolbarHandler = this
+    }
+
+    private fun initToolbar() {
+        binding.includeTravelToolbar.toolbarDetail.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
     private fun observeTravel() {
@@ -50,5 +65,29 @@ class TravelFragment : BindingFragment<FragmentTravelBinding>(R.layout.fragment_
     private fun initVisitsAdapter() {
         visitsAdapter = VisitsAdapter()
         binding.rvTravelVisits.adapter = visitsAdapter
+    }
+
+    override fun onUpdateClicked() {
+        val travelUpdateLauncher = (activity as MainActivity).travelUpdateLauncher
+        TravelUpdateActivity.startWithResultLauncher(
+            requireActivity(),
+            travelUpdateLauncher,
+        )
+    }
+
+    override fun onDeleteClicked() {
+        val deleteDialog = DeleteDialogFragment()
+        val fragmentManager = parentFragmentManager
+        deleteDialog.apply {
+            setDialogHandler {
+                findNavController().popBackStack()
+                Toast.makeText(
+                    requireContext(),
+                    getString(R.string.travel_delete_complete),
+                    Toast.LENGTH_SHORT,
+                ).show()
+            }
+            show(fragmentManager, DeleteDialogFragment.TAG)
+        }
     }
 }
