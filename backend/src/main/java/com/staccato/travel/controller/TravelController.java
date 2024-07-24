f1628710d5ef42d04d5374e94529e9b9b8c38d2b
yoonjuho <dbswn990@naver.com>
2024-07-24 17:38:55 +0900

    feat: 특정 여행 상세 수정 컨트롤러 구현

--- backend/src/main/java/com/staccato/travel/controller/TravelController.java
+++ backend/src/main/java/com/staccato/travel/controller/TravelController.java
@@ -3,9 +3,13 @@ package com.staccato.travel.controller;
 import java.net.URI;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +21,7 @@ import com.staccato.travel.service.dto.response.TravelResponse;
 
 import lombok.RequiredArgsConstructor;
 
+@Validated
 @RestController
 @RequestMapping("/travels")
 @RequiredArgsConstructor
@@ -27,5 +32,14 @@ public class TravelController {
     public ResponseEntity<Void> createTravel(@Valid @RequestBody TravelRequest travelRequest, @MemberId Long memberId) {
         TravelResponse travelResponse = travelService.createTravel(travelRequest, memberId);
         return ResponseEntity.created(URI.create("/travels/" + travelResponse.travelId())).build();
+    }
+
+    @PutMapping("/{travelId}")
+    public ResponseEntity<Void> updateTravel(
+            @PathVariable @Min(value = 1L, message = "여행 식별자는 양수로 이루어져야 합니다.") Long travelId,
+            @Valid @RequestBody TravelRequest travelRequest,
+            @MemberId Long memberId) {
+        travelService.updateTravel(travelRequest, travelId);
+        return ResponseEntity.ok().build();
     }
 }
