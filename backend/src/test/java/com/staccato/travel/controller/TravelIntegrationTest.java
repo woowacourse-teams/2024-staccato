52a8b8415b339bc3db702bbcb55ecc31fec1a016
yoonjuho <dbswn990@naver.com>
2024-07-24 17:38:55 +0900

    feat: 특정 여행 상세 수정 컨트롤러 구현

--- backend/src/test/java/com/staccato/travel/controller/TravelIntegrationTest.java
+++ backend/src/test/java/com/staccato/travel/controller/TravelIntegrationTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -96,6 +97,66 @@ class TravelIntegrationTest extends IntegrationTest {
                 .body(travelRequest)
                 .when().log().all()
                 .post("/travels")
+                .then().log().all()
+                .assertThat().statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("message", is(expectedMessage))
+                .body("status", is(HttpStatus.BAD_REQUEST.toString()));
+    }
+
+    @DisplayName("사용자가 여행 상세 정보를 입력하면, 여행 상세를 수정한다.")
+    @ParameterizedTest
+    @MethodSource("travelRequestProvider")
+    void updateTravel(TravelRequest travelRequest) {
+        // given
+        createTravel(travelRequest);
+        Long travelId = 1L;
+
+        // when & then
+        RestAssured.given().pathParam("travelId", travelId).log().all()
+                .contentType(ContentType.JSON)
+                .header(HttpHeaders.AUTHORIZATION, USER_AUTHORIZATION)
+                .body(travelRequest)
+                .when().log().all()
+                .put("/travels/{travelId}")
+                .then().log().all()
+                .assertThat().statusCode(HttpStatus.OK.value());
+    }
+
+    @DisplayName("사용자가 잘못된 방식으로 정보를 입력하면, 여행 상세를 수정할 수 없다.")
+    @ParameterizedTest
+    @MethodSource("invalidTravelRequestProvider")
+    void failUpdateTravel(TravelRequest invalidTravelRequest, String expectedMessage) {
+        // given
+        Long travelId = 1L;
+
+        // when & then
+        RestAssured.given().pathParam("travelId", travelId).log().all()
+                .contentType(ContentType.JSON)
+                .header(HttpHeaders.AUTHORIZATION, USER_AUTHORIZATION)
+                .body(invalidTravelRequest)
+                .when().log().all()
+                .put("/travels/{travelId}")
+                .then().log().all()
+                .assertThat().statusCode(HttpStatus.BAD_REQUEST.value())
+                .body("message", is(expectedMessage))
+                .body("status", is(HttpStatus.BAD_REQUEST.toString()));
+    }
+
+    @DisplayName("사용자가 잘못된 여행식별자로 접근한다면 예외가 발생한다.")
+    @ParameterizedTest
+    @ValueSource(longs = {0, -1})
+    void failAccessTravel(Long travelId) {
+        // given
+        TravelRequest travelRequest = new TravelRequest("https://example.com/travels/geumohrm.jpg", "2023 여름 휴가", "친구들과 함께한 여름 휴가 여행", LocalDate.of(2023, 7, 1), LocalDate.of(2023, 7, 10));
+        String expectedMessage = "여행 식별자는 양수로 이루어져야 합니다.";
+
+        // when & then
+        RestAssured.given().pathParam("travelId", travelId).log().all()
+                .contentType(ContentType.JSON)
+                .header(HttpHeaders.AUTHORIZATION, USER_AUTHORIZATION)
+                .body(travelRequest)
+                .when().log().all()
+                .put("/travels/{travelId}")
                 .then().log().all()
                 .assertThat().statusCode(HttpStatus.BAD_REQUEST.value())
                 .body("message", is(expectedMessage))
