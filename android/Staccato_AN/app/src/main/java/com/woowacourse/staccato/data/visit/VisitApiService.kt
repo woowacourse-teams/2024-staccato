af711bd5869f15c574c9a0b4012d1e25309cf22f
somin <salth0601@gmail.com>
2024-07-26 05:06:00 +0900

    refactor: VisitApiService의 중복된 @Header 제거

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/visit/VisitApiService.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/visit/VisitApiService.kt
@@ -5,26 +5,22 @@ import com.woowacourse.staccato.data.dto.visit.VisitResponse
 import com.woowacourse.staccato.data.dto.visit.VisitUpdateRequest
 import retrofit2.http.Body
 import retrofit2.http.GET
-import retrofit2.http.Header
 import retrofit2.http.POST
 import retrofit2.http.Path
 
 interface VisitApiService {
     @GET("/visits/{visitId}")
     suspend fun requestVisit(
-        @Header("Authorization") authorization: String = "1",
         @Path(value = "visitId") visitId: Long,
     ): VisitResponse
 
     @POST("/visits")
     suspend fun requestCreateVisit(
-        @Header("Authorization") authorization: String = "1",
         @Body visitCreationRequest: VisitCreationRequest,
     )
 
     @POST("/visits")
     suspend fun requestUpdateVisit(
-        @Header("Authorization") authorization: String = "1",
         @Body visitUpdateRequest: VisitUpdateRequest,
     )
 }
