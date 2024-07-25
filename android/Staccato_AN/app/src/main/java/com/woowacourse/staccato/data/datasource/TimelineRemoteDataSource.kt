18e994bfd2918e580147db239d08f53f4b133c3c
Junyoung-WON <wjyo60@gmail.com>
2024-07-25 16:01:50 +0900

    feat: TimelineDataSource 구현체 생성

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/datasource/TimelineRemoteDataSource.kt
@@ -0,0 +1,35 @@
+package com.woowacourse.staccato.data.datasource
+
+import com.woowacourse.staccato.data.StaccatoClient
+import com.woowacourse.staccato.data.apiservice.TimeLineApiService
+import com.woowacourse.staccato.data.dto.timeline.TimelineResponse
+import org.json.JSONObject
+
+class TimelineRemoteDataSource(
+    private val service: TimeLineApiService = StaccatoClient.create(
+        TimeLineApiService::class.java
+    ),
+) : TimelineDataSource {
+    override suspend fun loadAll(): Result<TimelineResponse> {
+        return loadTimeline(null)
+    }
+
+    override suspend fun loadByYear(year: Int): Result<TimelineResponse> {
+        return loadTimeline(year)
+    }
+
+    private suspend fun loadTimeline(year: Int?): Result<TimelineResponse> {
+        val response = service.requestTimeline(year)
+        val body = response.body()
+        return if (response.isSuccessful && body != null) {
+            Result.success(body)
+        } else {
+            val errorBody = JSONObject(response.errorBody()?.string()!!)
+            Result.failure(
+                IllegalStateException(
+                    "${errorBody.getString("status")} : ${errorBody.getString("messages")}"
+                ),
+            )
+        }
+    }
+}
