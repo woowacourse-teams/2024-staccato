70d41629c1225f002976a48124c43aa17fc02b75
Junyoung-WON <wjyo60@gmail.com>
2024-07-25 20:23:57 +0900

    feat: Response(dto)에서 도메인 모델로 변환하는 확장함수 구현

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/LocalDateConverters.kt
@@ -0,0 +1,10 @@
+package com.woowacourse.staccato
+
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
+
+const val DATETIME_FORMAT_PATTERN = "yyyy-MM-dd"
+
+fun String.toLocalDate(): LocalDate {
+    return LocalDate.parse(this, DateTimeFormatter.ofPattern(DATETIME_FORMAT_PATTERN))
+}
---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/mapper/TimelineMapper.kt
@@ -0,0 +1,41 @@
+package com.woowacourse.staccato.data.dto.mapper
+
+import com.woowacourse.staccato.data.dto.MemberDto
+import com.woowacourse.staccato.data.dto.timeline.TimelineResponse
+import com.woowacourse.staccato.data.dto.timeline.TimelineTravelDto
+import com.woowacourse.staccato.domain.model.Member
+import com.woowacourse.staccato.domain.model.Timeline
+import com.woowacourse.staccato.domain.model.Travel
+import com.woowacourse.staccato.toLocalDate
+
+fun TimelineResponse.toDomain(): Timeline {
+    val travels = travels.map { timelineTravelDto ->
+        timelineTravelDto.toDomain()
+    }
+    return Timeline(travels)
+}
+
+fun TimelineTravelDto.toDomain(): Travel {
+    return Travel(
+        travelId = travelId,
+        travelThumbnail = travelThumbnail,
+        travelTitle = travelTitle,
+        startAt = startAt.toLocalDate(),
+        endAt = endAt.toLocalDate(),
+        description = description,
+        mates = mates.toDomain(),
+        visits = emptyList(),
+    )
+}
+
+fun List<MemberDto>.toDomain(): List<Member> {
+    return map { it.toDomain() }
+}
+
+fun MemberDto.toDomain(): Member {
+    return Member(
+        memberId = memberId,
+        nickName = nickName,
+        memberImage = memberImage,
+    )
+}
