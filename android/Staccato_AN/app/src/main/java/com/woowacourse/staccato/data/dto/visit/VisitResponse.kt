44bc1c9ec9db814b117b0a0ec0eb0191ada89a24
somin <salth0601@gmail.com>
2024-07-26 03:04:34 +0900

    feat: VisitApiService 인터페이스 및 관련 data class 추가

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/visit/VisitResponse.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/visit/VisitResponse.kt
@@ -7,7 +7,7 @@ import kotlinx.serialization.Serializable
 data class VisitResponse(
     @SerialName("visitId") val visitId: Long,
     @SerialName("placeName") val placeName: String,
-    @SerialName("visitedImages") val visitedImages: List<String>,
+    @SerialName("visitImages") val visitImages: List<String>,
     @SerialName("address") val address: String,
     @SerialName("visitedAt") val visitedAt: String,
     @SerialName("visitedCount") val visitedCount: Long,
