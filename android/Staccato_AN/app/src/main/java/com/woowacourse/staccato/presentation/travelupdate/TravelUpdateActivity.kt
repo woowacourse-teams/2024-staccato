a2c44109add4ae52bf0f6f49d68dddc7336e3974
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-25 13:39:37 +0900

    feat: 여행 수정 view 연결

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travelupdate/TravelUpdateActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travelupdate/TravelUpdateActivity.kt
@@ -5,17 +5,57 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.viewModels
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.woowacourse.staccato.R
-import com.woowacourse.staccato.databinding.ActivityTravelUpdateBinding
 import com.woowacourse.staccato.presentation.base.BindingActivity
+import com.woowacourse.staccato.presentation.travelcreation.TravelCreationHandler
+import com.woowacourse.staccato.presentation.travelupdate.viewmodel.TravelUpdateViewModel
+import com.woowacourse.staccato.presentation.travelupdate.viewmodel.TravelUpdateViewModelFactory
 
-class TravelUpdateActivity : BindingActivity<ActivityTravelUpdateBinding>() {
+class TravelUpdateActivity : BindingActivity<com.woowacourse.staccato.databinding.ActivityTravelUpdateBinding>(), TravelCreationHandler {
     override val layoutResourceId = R.layout.activity_travel_update
+    private val viewModel: TravelUpdateViewModel by viewModels { TravelUpdateViewModelFactory() }
 
     override fun initStartView(savedInstanceState: Bundle?) {
-        binding.btnTravelUpdateDone.setOnClickListener {
-            val resultIntent = Intent()
-            setResult(Activity.RESULT_OK, resultIntent)
+        initBinding()
+        navigateToTravel()
+    }
+
+    override fun onPeriodSelectionClicked() {
+        val dateRangePicker =
+            MaterialDatePicker.Builder.dateRangePicker()
+                .setTheme(R.style.DatePickerStyle)
+                .setSelection(
+                    androidx.core.util.Pair(
+                        MaterialDatePicker.thisMonthInUtcMilliseconds(),
+                        MaterialDatePicker.todayInUtcMilliseconds(),
+                    ),
+                ).build()
+
+        dateRangePicker.show(supportFragmentManager, dateRangePicker.toString())
+
+        dateRangePicker.addOnPositiveButtonClickListener { selection ->
+            val startDate: Long = selection.first
+            val endDate: Long = selection.second
+            viewModel.setTravelPeriod(startDate, endDate)
+        }
+    }
+
+    override fun onSaveClicked() {
+        val resultIntent = Intent()
+        setResult(Activity.RESULT_OK, resultIntent)
+        finish()
+    }
+
+    private fun initBinding() {
+        binding.lifecycleOwner = this
+        binding.viewModel = viewModel
+        binding.handler = this
+    }
+
+    private fun navigateToTravel() {
+        binding.toolbarTravelUpdate.setNavigationOnClickListener {
             finish()
         }
     }
