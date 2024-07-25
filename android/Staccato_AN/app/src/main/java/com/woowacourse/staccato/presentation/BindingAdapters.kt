7d7ce225cc1610c9e26e1453a8ecbf0e771365f8
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-25 12:55:53 +0900

    feat: 여행 저장 버튼, 여행 기간 BindingAdapter 추가

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/BindingAdapters.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/BindingAdapters.kt
@@ -1,13 +1,18 @@
 package com.woowacourse.staccato.presentation
 
 import android.graphics.drawable.Drawable
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.app.NotificationCompat.getColor
 import androidx.databinding.BindingAdapter
 import coil.load
 import coil.transform.RoundedCornersTransformation
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
+import com.woowacourse.staccato.R
+import java.time.LocalDate
 
 @BindingAdapter(
     value = ["coilImageUrl", "coilPlaceHolder"],
@@ -79,4 +84,37 @@ fun ImageView.setRoundedCornerImageWithGlide(
         .apply(RequestOptions.bitmapTransform(RoundedCorners(roundingRadius)))
         .error(placeHolder)
         .into(this)
+}
+
+@BindingAdapter(
+    value = ["travelTitle", "startDate", "endDate"],
+)
+fun Button.setTravelSaveButtonActive(
+    title: String?,
+    startDate: LocalDate?,
+    endDate: LocalDate?,
+) {
+    isEnabled =
+        if (title.isNullOrEmpty() || startDate == null || endDate == null) {
+            setTextColor(resources.getColor(R.color.gray4, null))
+            false
+        } else {
+            setTextColor(resources.getColor(R.color.white, null))
+            true
+        }
+}
+
+@BindingAdapter(
+    value = ["startDate", "endDate"],
+)
+fun TextView.setTravelPeriod(
+    startDate: LocalDate?,
+    endDate: LocalDate?,
+) {
+    if (startDate == null || endDate == null) {
+        text = resources.getString(R.string.travel_creation_period_hint)
+    } else {
+        text = resources.getString(R.string.travel_creation_period_description).format(startDate, endDate)
+        setTextColor(resources.getColor(R.color.staccato_black, null))
+    }
 }
