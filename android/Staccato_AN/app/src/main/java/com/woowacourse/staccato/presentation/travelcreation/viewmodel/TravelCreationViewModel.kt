391d058344eec923e726a9cbf9d4bc3b405fdc0c
hxeyexn <kck85075@kumoh.ac.kr>
2024-07-26 09:40:23 +0900

    feat: 여행 생성 api 연결

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travelcreation/viewmodel/TravelCreationViewModel.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/travelcreation/viewmodel/TravelCreationViewModel.kt
@@ -4,10 +4,19 @@ import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.woowacourse.staccato.data.ApiResponseHandler.onException
+import com.woowacourse.staccato.data.ApiResponseHandler.onServerError
+import com.woowacourse.staccato.data.ApiResponseHandler.onSuccess
+import com.woowacourse.staccato.domain.repository.TravelRepository
 import com.woowacourse.staccato.presentation.travelcreation.DateConverter.convertLongToLocalDate
+import com.woowacourse.staccato.presentation.travelcreation.TravelCreationUiModel
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 
-class TravelCreationViewModel : ViewModel() {
+class TravelCreationViewModel(
+    private val travelRepository: TravelRepository,
+) : ViewModel() {
     private val _imageUrl = MutableLiveData<String>()
     val imageUrl: LiveData<String> get() = _imageUrl
 
@@ -20,11 +29,34 @@ class TravelCreationViewModel : ViewModel() {
     private val _endDate = MutableLiveData<LocalDate>(null)
     val endDate: LiveData<LocalDate> get() = _endDate
 
+    private val _createdTravelId = MutableLiveData<Long>()
+    val createdTravelId: LiveData<Long> get() = _createdTravelId
+
     fun setTravelPeriod(
         startAt: Long,
         endAt: Long,
     ) {
         _startDate.value = convertLongToLocalDate(startAt)
         _endDate.value = convertLongToLocalDate(endAt)
+    }
+
+    fun createTravel() {
+        viewModelScope.launch {
+            val travel =
+                TravelCreationUiModel(
+                    travelThumbnail = imageUrl.value,
+                    travelTitle = title.get() ?: return@launch,
+                    startAt = startDate.value ?: return@launch,
+                    endAt = endDate.value ?: return@launch,
+                    description = description.get() ?: return@launch,
+                )
+            travelRepository.createTravel(travel).onSuccess {
+                _createdTravelId.value = it.split("/").last().toLong()
+            }.onServerError { code, message ->
+                // TODO: Error 핸들링
+            }.onException { e, message ->
+                // TODO: Exception 핸들링
+            }
+        }
     }
 }
