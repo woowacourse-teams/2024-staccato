8b913e4037013b495da9be8d1771e91101c4895b
linirini <101927543+linirini@users.noreply.github.com>
2024-07-31 16:29:15 +0900

    docs: swagger 컨벤션 설정 및 적용 (#116)
    * build: 중복 의존성 정의 제거
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>
    
    * build: OpenApi 의존성 추가
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>
    
    * chore: 전역적인 media type 설정
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>
    
    * chore: open api skeleton code 작성
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>
    
    * fix: constraint redefine 불가로 인한 오류 수정
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>
    
    * style: 의미없는 개행 제거
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>
    
    * docs: 누락된 설명 추가
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>
    
    ---------
    
    Co-authored-by: YoonJuHo <dbswn990@naver.com>
    Co-authored-by: devhoya97 <diajbh123@gmail.com>
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>

--- backend/src/main/java/com/staccato/travel/service/dto/request/TravelRequest.java
+++ backend/src/main/java/com/staccato/travel/service/dto/request/TravelRequest.java
@@ -9,16 +9,24 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import com.staccato.travel.domain.Travel;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "여행 상세를 생성/수정하기 위한 요청 형식입니다.")
 public record TravelRequest(
+        @Schema(example = "http://example.com/london.png")
         String travelThumbnail,
+        @Schema(example = "런던 여행")
         @NotNull(message = "여행 제목을 입력해주세요.")
         @Size(max = 30, message = "제목의 최대 허용 글자수는 공백 포함 30자입니다.")
         String travelTitle,
+        @Schema(example = "런던 시내 탐방")
         @Size(max = 500, message = "내용의 최대 허용 글자수는 공백 포함 500자입니다.")
         String description,
+        @Schema(example = "2024-07-27")
         @NotNull(message = "여행 시작 날짜를 입력해주세요.")
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         LocalDate startAt,
+        @Schema(example = "2024-07-29")
         @NotNull(message = "여행 끝 날짜를 입력해주세요.")
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         LocalDate endAt) {
