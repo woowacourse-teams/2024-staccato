c3fd8fcd70f0d4dd09a250215f2455df92f3d3b6
Junyoung-WON <wjyo60@gmail.com>
2024-08-02 21:27:27 +0900

    feat: 로그인 Repository 생성

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/login/LoginDefaultRepository.kt
@@ -0,0 +1,20 @@
+package com.woowacourse.staccato.data.login
+
+import com.woowacourse.staccato.data.ResponseResult
+import com.woowacourse.staccato.domain.repository.LoginRepository
+
+class LoginDefaultRepository(
+    private val loginDataSource: LoginDataSource,
+) : LoginRepository {
+    override suspend fun loginWithNickname(nickname: String): ResponseResult<String> {
+        return when (val result = loginDataSource.requestLoginWithNickname(nickname)) {
+            is ResponseResult.Exception -> ResponseResult.Exception(result.e, EXCEPTION_ERROR_MESSAGE)
+            is ResponseResult.ServerError -> ResponseResult.ServerError(result.code, result.message)
+            is ResponseResult.Success -> ResponseResult.Success(result.data)
+        }
+    }
+
+    companion object {
+        private const val EXCEPTION_ERROR_MESSAGE = "예기치 못한 에러가 발생했습니다. 다시 시도해주세요."
+    }
+}
