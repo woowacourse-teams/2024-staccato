c7e44c80177913ecd85197b90fa359065f32fa15
devhoya97 <diajbh123@gmail.com>
2024-08-03 10:59:04 +0900

    fix: 여행 식별자가 양수인지 검증하는 코드 추가

--- backend/src/main/java/com/staccato/visit/service/dto/request/VisitRequest.java
+++ backend/src/main/java/com/staccato/visit/service/dto/request/VisitRequest.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.List;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import org.springframework.format.annotation.DateTimeFormat;
@@ -12,19 +13,20 @@ import com.staccato.travel.domain.Travel;
 import com.staccato.visit.domain.Visit;
 
 public record VisitRequest(
-        @NotNull
+        @NotNull(message = "방문한 장소 이름을 입력해주세요.")
         String placeName,
-        @NotNull
+        @NotNull(message = "방문한 장소의 주소를 입력해주세요.")
         String address,
-        @NotNull
+        @NotNull(message = "방문한 장소의 위도를 입력해주세요.")
         BigDecimal latitude,
-        @NotNull
+        @NotNull(message = "방문한 장소의 경도를 입력해주세요.")
         BigDecimal longitude,
         List<String> visitImagesUrl,
         @NotNull(message = "방문 날짜를 입력해주세요.")
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         LocalDate visitedAt,
         @NotNull(message = "여행 상세를 선택해주세요.")
+        @Min(value = 1L, message = "여행 식별자는 양수로 이루어져야 합니다.")
         Long travelId
 ) {
     public Visit toVisit(Travel travel) {
