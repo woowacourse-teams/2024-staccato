cde7456961ed1013ccdff2c99de6398044f9561f
linirini <2001yerin@naver.com>
2024-08-04 17:50:55 +0900

    refactor: 상수 재활용

--- backend/src/main/java/com/staccato/member/domain/Nickname.java
+++ backend/src/main/java/com/staccato/member/domain/Nickname.java
@@ -19,7 +19,7 @@ public class Nickname {
     private static final Pattern NICKNAME_REGEX = Pattern.compile("^[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z._]+$");
     private static final int MAX_LENGTH = 20;
 
-    @Column(nullable = false, length = 20)
+    @Column(nullable = false, length = MAX_LENGTH)
     private String nickname;
 
     public Nickname(String nickname) {
@@ -34,7 +34,7 @@ public class Nickname {
 
     private void validateLength(String nickname) {
         if (nickname.isEmpty() || nickname.length() > MAX_LENGTH) {
-            throw new StaccatoException("20자 이내의 닉네임으로 설정해주세요.");
+            throw new StaccatoException(MAX_LENGTH + "자 이내의 닉네임으로 설정해주세요.");
         }
     }
 
