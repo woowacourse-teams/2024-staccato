7f508c6b521a50dec390ff6a6deff425b0b6bb1a
devhoya97 <diajbh123@gmail.com>
2024-08-04 11:12:39 +0900

    feat: 방문 기록 생성 DTO에 Swagger 적용

--- backend/src/main/java/com/staccato/visit/service/dto/request/VisitRequest.java
+++ backend/src/main/java/com/staccato/visit/service/dto/request/VisitRequest.java
@@ -12,19 +12,28 @@ import org.springframework.format.annotation.DateTimeFormat;
 import com.staccato.travel.domain.Travel;
 import com.staccato.visit.domain.Visit;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record VisitRequest(
+        @Schema(example = "런던 박물관")
         @NotNull(message = "방문한 장소의 이름을 입력해주세요.")
         String placeName,
+        @Schema(example = "Great Russell St, London WC1B 3DG")
         @NotNull(message = "방문한 장소의 주소를 입력해주세요.")
         String address,
+        @Schema(example = "51.51978412729915")
         @NotNull(message = "방문한 장소의 위도를 입력해주세요.")
         BigDecimal latitude,
+        @Schema(example = "-0.12712788587027796")
         @NotNull(message = "방문한 장소의 경도를 입력해주세요.")
         BigDecimal longitude,
+        @Schema(example = "[\"http://example.com/image1.jpg\"]")
         List<String> visitImagesUrl,
+        @Schema(example = "2024-07-27")
         @NotNull(message = "방문 날짜를 입력해주세요.")
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         LocalDate visitedAt,
+        @Schema(example = "1")
         @NotNull(message = "여행 상세를 선택해주세요.")
         @Min(value = 1L, message = "여행 식별자는 양수로 이루어져야 합니다.")
         Long travelId
