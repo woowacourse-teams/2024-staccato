1586378fc544df35722bec43710eb10b9d056350
hxeyexn <kck85075@kumoh.ac.kr>
2024-08-05 21:55:49 +0900

    refactor: MembersDto 삭제

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/member/MembersDto.kt
+++
@@ -1,9 +0,0 @@
-package com.woowacourse.staccato.data.dto.member
-
-import kotlinx.serialization.SerialName
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class MembersDto(
-    @SerialName("members") val members: List<MemberDto>,
-)
