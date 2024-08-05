c5b94afd619c67cf540afc2fd3782b5ee2b84ddf
linirini <2001yerin@naver.com>
2024-08-05 09:59:11 +0900

    docs: 응답 명세 작성

--- backend/src/main/java/com/staccato/auth/service/dto/response/LoginResponse.java
+++ backend/src/main/java/com/staccato/auth/service/dto/response/LoginResponse.java
@@ -1,4 +1,10 @@
 package com.staccato.auth.service.dto.response;
 
-public record LoginResponse(String token) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "회원 등록 시 발급되는 토큰에 대한 응답 형식입니다.")
+public record LoginResponse(
+        @Schema(example = "{tokenString}")
+        String token
+) {
 }
