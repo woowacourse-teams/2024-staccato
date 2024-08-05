b920dce666765f238985403dfc84fa1d0e6cf344
linirini <2001yerin@naver.com>
2024-08-06 00:44:36 +0900

    docs: 공통 예외 문서화

--- backend/src/main/java/com/staccato/exception/ExceptionResponse.java
+++ backend/src/main/java/com/staccato/exception/ExceptionResponse.java
@@ -1,4 +1,12 @@
 package com.staccato.exception;
 
-public record ExceptionResponse(String status, String message) {
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(description = "예외에 대한 응답 형식입니다.")
+public record ExceptionResponse(
+        @Schema(example = "400 BAD_REQUEST")
+        String status,
+        @Schema(example = "요청하신 정보를 찾을 수 없어요.")
+        String message
+) {
 }
