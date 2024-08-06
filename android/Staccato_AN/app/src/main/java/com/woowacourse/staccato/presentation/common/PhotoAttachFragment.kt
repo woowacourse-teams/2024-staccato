60b519d37645a64f6f5eebce8ed20024a706c015
somin <salth0601@gmail.com>
2024-08-06 21:47:49 +0900

    feat: PhotoAttachFragment 앨범에서 불러온 이미지의 URI 추출하기

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/common/PhotoAttachFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/common/PhotoAttachFragment.kt
@@ -2,12 +2,14 @@ package com.woowacourse.staccato.presentation.common
 
 import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.app.Activity.RESULT_OK
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
@@ -19,27 +21,39 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import com.google.android.material.snackbar.Snackbar
 import com.woowacourse.staccato.R
 import com.woowacourse.staccato.databinding.FragmentPhotoAttachBinding
+import com.woowacourse.staccato.presentation.util.showToast
 
 class PhotoAttachFragment : BottomSheetDialogFragment(), PhotoAttachHandler {
     private var _binding: FragmentPhotoAttachBinding? = null
     private val binding get() = _binding!!
 
     private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
 
     override fun onAttach(context: Context) {
         super.onAttach(context)
         initRequestPermissionLauncher()
+        initGalleryLauncher()
     }
 
     private fun initRequestPermissionLauncher() {
         requestPermissionLauncher =
             registerForActivityResult(ActivityResultContracts.RequestPermission()) { isPermissionGranted ->
                 if (isPermissionGranted) {
-                    // 갤러리 열기
+                    launchGallery()
                 } else {
                     showPermissionSnackBar()
                 }
             }
+    }
+
+    private fun launchGallery() {
+        val intent =
+            Intent(Intent.ACTION_PICK).apply {
+                setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+                putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+            }
+        galleryLauncher.launch(intent)
     }
 
     private fun showPermissionSnackBar() {
@@ -57,6 +71,34 @@ class PhotoAttachFragment : BottomSheetDialogFragment(), PhotoAttachHandler {
             startActivity(intent)
         }
         snackBar.show()
+    }
+
+    private fun initGalleryLauncher() {
+        galleryLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val imageUris = extractImageUris(result.data)
+                    if (imageUris.isNotEmpty()) {
+                        // URI 전달
+                    } else {
+                        showToast("사진을 불러올 수 없습니다.")
+                    }
+                }
+            }
+    }
+
+    private fun extractImageUris(intent: Intent?): List<Uri> {
+        val imageUris = mutableListOf<Uri>()
+        intent?.let {
+            it.clipData?.let { clipData ->
+                repeat(clipData.itemCount) { index ->
+                    imageUris.add(clipData.getItemAt(index).uri)
+                }
+            } ?: it.data?.let { uri ->
+                imageUris.add(uri)
+            }
+        }
+        return imageUris
     }
 
     override fun onCreateView(
@@ -99,7 +141,7 @@ class PhotoAttachFragment : BottomSheetDialogFragment(), PhotoAttachHandler {
                 permission,
             ) == PackageManager.PERMISSION_GRANTED
         if (isPermissionGranted) {
-            // 갤러리 열기
+            launchGallery()
         } else {
             requestPermissionLauncher.launch(permission)
         }
