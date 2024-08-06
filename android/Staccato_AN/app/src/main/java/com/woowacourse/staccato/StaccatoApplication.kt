fb0881bc51f0f62ca9b000f030ddc122ccac7c44
Junyoung-WON <wjyo60@gmail.com>
2024-08-06 23:40:54 +0900

    feat: Night 모드 비활성화

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/StaccatoApplication.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/StaccatoApplication.kt
@@ -1,11 +1,13 @@
 package com.woowacourse.staccato
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.woowacourse.staccato.data.UserInfoPreferencesManager
 
 class StaccatoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         userInfoPrefsManager = UserInfoPreferencesManager(applicationContext)
     }
 
