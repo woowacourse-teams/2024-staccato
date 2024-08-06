6e8973604871efd43cb80bc76e8ab8acdc6897b0
linirini <2001yerin@naver.com>
2024-08-07 00:18:26 +0900

    docs: example 제거

--- backend/src/main/java/com/staccato/exception/ExceptionResponse.java
+++ backend/src/main/java/com/staccato/exception/ExceptionResponse.java
@@ -4,9 +4,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 @Schema(description = "예외에 대한 응답 형식입니다.")
 public record ExceptionResponse(
-        @Schema(example = "400 BAD_REQUEST")
         String status,
-        @Schema(example = "요청하신 정보를 찾을 수 없어요.")
         String message
 ) {
 }
