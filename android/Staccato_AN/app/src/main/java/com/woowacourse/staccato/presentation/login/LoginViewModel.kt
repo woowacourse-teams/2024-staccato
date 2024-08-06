889bc35fd2ea152947957037bb0741bc4f0fcb11
Junyoung-WON <wjyo60@gmail.com>
2024-08-06 14:05:29 +0900

    feat: LoginViewModel 및 Handler 작성

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/login/LoginViewModel.kt
@@ -0,0 +1,58 @@
+package com.woowacourse.staccato.presentation.login
+
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.woowacourse.staccato.StaccatoApplication
+import com.woowacourse.staccato.data.ApiResponseHandler.onException
+import com.woowacourse.staccato.data.ApiResponseHandler.onServerError
+import com.woowacourse.staccato.data.ApiResponseHandler.onSuccess
+import com.woowacourse.staccato.domain.repository.LoginRepository
+import com.woowacourse.staccato.presentation.common.MutableSingleLiveData
+import com.woowacourse.staccato.presentation.common.SingleLiveData
+import kotlinx.coroutines.launch
+
+class LoginViewModel(private val repository: LoginRepository) : ViewModel(), LoginHandler {
+    val nickname = MutableLiveData("")
+
+    private val _isLoginSuccess = MutableSingleLiveData(false)
+    val isLoginSuccess: SingleLiveData<Boolean>
+        get() = _isLoginSuccess
+
+    private val _errorMessage = MutableSingleLiveData<String>()
+    val errorMessage: SingleLiveData<String>
+        get() = _errorMessage
+
+    override fun onStartClicked() {
+        Log.d("hodu", "nickname: ${nickname.value}")
+        val nickname = nickname.value ?: ""
+        viewModelScope.launch {
+            repository.loginWithNickname(nickname)
+                .onSuccess(::saveUserToken)
+                .onServerError(::handleError)
+                .onException(::handleException)
+        }
+    }
+
+    private fun saveUserToken(newToken: String) {
+        StaccatoApplication.userInfoPrefsManager.setToken(newToken)
+        _isLoginSuccess.setValue(true)
+    }
+
+    private fun handleError(
+        code: Int,
+        errorMessage: String,
+    ) {
+        Log.e(this::class.java.simpleName, "Error Occurred | code: $code, message: $errorMessage")
+        _errorMessage.setValue(errorMessage)
+    }
+
+    private fun handleException(
+        e: Throwable,
+        errorMessage: String,
+    ) {
+        Log.e(this::class.java.simpleName, "Exception Caught | throwable: $e, message: $errorMessage")
+        _errorMessage.setValue(errorMessage)
+    }
+}
