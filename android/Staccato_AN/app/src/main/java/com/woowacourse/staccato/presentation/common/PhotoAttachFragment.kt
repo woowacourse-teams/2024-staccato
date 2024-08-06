cbfa3b28b5b3322f4b433b8d0e23c07f0a9d845f
somin <salth0601@gmail.com>
2024-08-06 21:40:52 +0900

    feat: PhotoAttachFragment 앨범 접근 권한 관련 로직 구현
    - API level 33 이상 : READ_MEDIA_IMAGES
    - API level 33 이하 : READ_EXTERNAL_STORAGE
    - ActivityResultLauncher를 이용한 권한 요청
    - 권한 거부 시, 설정으로 이동하는 스낵바 띄우기

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/common/PhotoAttachFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/common/PhotoAttachFragment.kt
@@ -1,15 +1,63 @@
 package com.woowacourse.staccato.presentation.common
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
+import com.woowacourse.staccato.R
 import com.woowacourse.staccato.databinding.FragmentPhotoAttachBinding
 
 class PhotoAttachFragment : BottomSheetDialogFragment(), PhotoAttachHandler {
     private var _binding: FragmentPhotoAttachBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        initRequestPermissionLauncher()
+    }
+
+    private fun initRequestPermissionLauncher() {
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isPermissionGranted ->
+                if (isPermissionGranted) {
+                    // 갤러리 열기
+                } else {
+                    showPermissionSnackBar()
+                }
+            }
+    }
+
+    private fun showPermissionSnackBar() {
+        val snackBar =
+            Snackbar.make(
+                binding.root,
+                R.string.snack_bar_require_photo_album_permission,
+                Snackbar.LENGTH_LONG,
+            )
+        snackBar.setAction(R.string.snack_bar_move_to_setting) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            val uri = Uri.fromParts(SCHEME, requireContext().packageName, null)
+            intent.data = uri
+            startActivity(intent)
+        }
+        snackBar.show()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -37,10 +85,28 @@ class PhotoAttachFragment : BottomSheetDialogFragment(), PhotoAttachHandler {
     }
 
     override fun onGalleryClicked() {
-        // 갤러리 열기
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkPermissionsAndLaunch(READ_MEDIA_IMAGES)
+        } else {
+            checkPermissionsAndLaunch(READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun checkPermissionsAndLaunch(permission: String) {
+        val isPermissionGranted =
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                permission,
+            ) == PackageManager.PERMISSION_GRANTED
+        if (isPermissionGranted) {
+            // 갤러리 열기
+        } else {
+            requestPermissionLauncher.launch(permission)
+        }
     }
 
     companion object {
         const val TAG = "PhotoAttachModalBottomSheet"
+        const val SCHEME = "package"
     }
 }
