704d154de0f3aceb11842368af3ad2297c2021b4
Junyoung-WON <wjyo60@gmail.com>
2024-08-06 16:25:03 +0900

    fix: 불필요한 ConverterFactory 제거
    - 더 이상 사용하지 않는 text/plain 변환용 ScalarsConverterFactory 제거

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/StaccatoClient.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/StaccatoClient.kt
@@ -11,7 +11,6 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
-import retrofit2.converter.scalars.ScalarsConverterFactory
 
 object StaccatoClient {
     private val BASE_URL = BuildConfig.BASE_URL
@@ -50,7 +49,6 @@ object StaccatoClient {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .client(provideHttpClient)
-            .addConverterFactory(ScalarsConverterFactory.create())
             .addConverterFactory(
                 jsonBuilder.asConverterFactory("application/json".toMediaType()),
             )
