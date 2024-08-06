6d5ffa980ff6982ce8f2f6b77ed071aab2391fd9
hxeyexn <kck85075@kumoh.ac.kr>
2024-08-07 00:31:56 +0900

    feat: 특정 여행 삭제 api service 구현

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/travel/TravelApiService.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/travel/TravelApiService.kt
@@ -4,6 +4,7 @@ import com.woowacourse.staccato.data.dto.travel.TravelRequest
 import com.woowacourse.staccato.data.dto.travel.TravelResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.DELETE
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
@@ -25,4 +26,15 @@ interface TravelApiService {
         @Path("travelId") travelId: Long,
         @Body travelRequest: TravelRequest,
     ): Response<String>
+
+    @DELETE(TRAVEL_PATH_WITH_ID)
+    suspend fun deleteTravel(
+        @Path("travelId") travelId: Long,
+    ): Response<Unit>
+
+    companion object {
+        private const val TRAVELS_PATH = "/travels"
+        private const val TRAVEL_ID = "/{travelId}"
+        private const val TRAVEL_PATH_WITH_ID = "$TRAVELS_PATH$TRAVEL_ID"
+    }
 }
