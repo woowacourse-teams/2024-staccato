1d9669263fe4106bf9f4140d51570594f1185877
Junyoung-WON <wjyo60@gmail.com>
2024-08-06 14:02:06 +0900

    feat: Preferences 로부터 사용자 토큰 값을 가져와 헤더에 추가하도록 변경

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/HeaderInterceptor.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/HeaderInterceptor.kt
@@ -1,20 +1,30 @@
 package com.woowacourse.staccato.data
 
+import com.woowacourse.staccato.StaccatoApplication
+import kotlinx.coroutines.runBlocking
 import okhttp3.Interceptor
+import okhttp3.Request
 import okhttp3.Response
 
-class HeaderInterceptor(private val headerValue: String = DEFAULT_VALUE) : Interceptor {
+class HeaderInterceptor : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
-        val request =
-            chain.request()
-                .newBuilder()
-                .addHeader(AUTHORIZATION_HEADER, headerValue)
-                .build()
-        return chain.proceed(request)
+        return runBlocking {
+            val token = StaccatoApplication.userInfoPrefsManager.getToken()
+            val request = if (!token.isNullOrEmpty()) {
+                chain.request().addTokenHeader(token)
+            } else {
+                chain.request()
+            }
+            chain.proceed(request)
+        }
     }
+
+    private fun Request.addTokenHeader(token: String): Request =
+        this.newBuilder()
+            .addHeader(AUTHORIZATION_HEADER, token)
+            .build()
 
     companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
-        const val DEFAULT_VALUE = "1"
     }
 }
