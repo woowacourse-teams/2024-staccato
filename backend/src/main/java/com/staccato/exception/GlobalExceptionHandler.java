330f3d9827fd2ef59344654dab7e32b0bad2e179
yoonjuho <dbswn990@naver.com>
2024-08-07 22:33:01 +0900

    feat: 예외에 대한 로거 형식 적용

--- backend/src/main/java/com/staccato/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/staccato/exception/GlobalExceptionHandler.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.staccato.config.log.LogForm;
+
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,20 +25,20 @@ public class GlobalExceptionHandler {
     @ApiResponse(responseCode = "400")
     public ResponseEntity<ExceptionResponse> handleDateTimeParseException(DateTimeParseException e) {
         String errorMessage = "올바르지 않은 날짜 형식입니다.";
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, errorMessage);
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), errorMessage));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), errorMessage);
+        log.warn(LogForm.EXCEPTION_LOGGING_FORM, exceptionResponse);
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ApiResponse(responseCode = "400")
     public ResponseEntity<ExceptionResponse> handleValidationException(MethodArgumentNotValidException e) {
-        String message = Optional.ofNullable(e.getBindingResult().getFieldError())
+        String errorMessage = Optional.ofNullable(e.getBindingResult().getFieldError())
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .orElse("요청 형식이 잘못되었습니다.");
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, message);
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), message));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), errorMessage);
+        log.warn(LogForm.EXCEPTION_LOGGING_FORM, exceptionResponse);
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(ConstraintViolationException.class)
@@ -46,50 +48,49 @@ public class GlobalExceptionHandler {
                 .iterator()
                 .next()
                 .getMessage();
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, errorMessage);
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), errorMessage));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), errorMessage);
+        log.warn(LogForm.EXCEPTION_LOGGING_FORM, exceptionResponse);
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ApiResponse(responseCode = "400")
     public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(HttpMessageNotReadableException e) {
         String errorMessage = "요청 본문을 읽을 수 없습니다. 올바른 형식으로 데이터를 제공해주세요.";
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, errorMessage);
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), errorMessage));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), errorMessage);
+        log.warn(LogForm.EXCEPTION_LOGGING_FORM, exceptionResponse);
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(StaccatoException.class)
     @ApiResponse(responseCode = "400")
     public ResponseEntity<ExceptionResponse> handleStaccatoException(StaccatoException e) {
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, e.getMessage());
-        return ResponseEntity.badRequest()
-                .body(new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage()));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), e.getMessage());
+        log.warn(LogForm.EXCEPTION_LOGGING_FORM, exceptionResponse);
+        return ResponseEntity.badRequest().body(exceptionResponse);
     }
 
     @ExceptionHandler(UnauthorizedException.class)
     @ApiResponse(description = "사용자 인증 실패", responseCode = "401")
     public ResponseEntity<ExceptionResponse> handleUnauthorizedException(UnauthorizedException e) {
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, e.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ExceptionResponse(HttpStatus.UNAUTHORIZED.toString(), e.getMessage()));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.UNAUTHORIZED.toString(), e.getMessage());
+        log.warn(LogForm.EXCEPTION_LOGGING_FORM, exceptionResponse);
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exceptionResponse);
     }
-
 
     @ExceptionHandler(ForbiddenException.class)
     @ApiResponse(description = "사용자가 권한을 가지고 있지 않은 작업을 시도 시 발생", responseCode = "403")
     public ResponseEntity<ExceptionResponse> handleForbiddenException(ForbiddenException e) {
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, e.getMessage());
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
-                .body(new ExceptionResponse(HttpStatus.UNAUTHORIZED.toString(), e.getMessage()));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.FORBIDDEN.toString(), e.getMessage());
+        log.warn(LogForm.EXCEPTION_LOGGING_FORM, exceptionResponse);
+        return ResponseEntity.status(HttpStatus.FORBIDDEN).body(exceptionResponse);
     }
 
     @ExceptionHandler(RuntimeException.class)
     @ApiResponse(responseCode = "500")
     public ResponseEntity<ExceptionResponse> handleInternalServerErrorException(RuntimeException e) {
-        log.warn("ExceptionType : {}, ExceptionMessage : {}", e, e.getMessage());
-        return ResponseEntity.internalServerError()
-                .body(new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "예기치 못한 서버 오류입니다. 다시 시도해주세요."));
+        ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.INTERNAL_SERVER_ERROR.toString(), "예기치 못한 서버 오류입니다. 다시 시도해주세요.");
+        log.error(LogForm.ERROR_LOGGING_FORM, exceptionResponse, e.getMessage());
+        return ResponseEntity.internalServerError().body(exceptionResponse);
     }
 }
