041e1a3f750b9692a643947e3bc08153fb60b736
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2024-08-07 15:59:36 +0900

    feat: 방문 생성 화면, 방문 수정 화면에서 갤러리 사진 불러오기 구현 #150 (#155)
    * feat: PhotoAttachFragment에 PhotoAttachHandler 연결
    
    * feat: PhotoAttachFragment 앨범 접근 권한 관련 로직 구현
    
    - API level 33 이상 : READ_MEDIA_IMAGES
    - API level 33 이하 : READ_EXTERNAL_STORAGE
    - ActivityResultLauncher를 이용한 권한 요청
    - 권한 거부 시, 설정으로 이동하는 스낵바 띄우기
    
    * feat: PhotoAttachFragment 앨범에서 불러온 이미지의 URI 추출하기
    
    * feat: 불러온 이미지의 URI를 호스트 Activity로 전달
    
    - OnUrisSelectedListener 인터페이스 추가
    
    * feat: Uri를 File로 변환하는 메서드 파일 추가
    
    * refactor: pr 리뷰 반영
    
    * refactor: pr 리뷰 반영2

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/common/PhotoAttachFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/common/PhotoAttachFragment.kt
@@ -1,15 +1,123 @@
 package com.woowacourse.staccato.presentation.common
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
+import android.Manifest.permission.READ_MEDIA_IMAGES
+import android.app.Activity.RESULT_OK
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.snackbar.Snackbar
+import com.woowacourse.staccato.R
 import com.woowacourse.staccato.databinding.FragmentPhotoAttachBinding
+import com.woowacourse.staccato.presentation.util.showToast
+import com.woowacourse.staccato.presentation.visitcreation.OnUrisSelectedListener
 
-class PhotoAttachFragment : BottomSheetDialogFragment() {
+class PhotoAttachFragment : BottomSheetDialogFragment(), PhotoAttachHandler {
     private var _binding: FragmentPhotoAttachBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var uriSelectedListener: OnUrisSelectedListener
+    private lateinit var requestPermissionLauncher: ActivityResultLauncher<String>
+    private lateinit var galleryLauncher: ActivityResultLauncher<Intent>
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        initUriSelectedListner(context)
+        initRequestPermissionLauncher()
+        initGalleryLauncher()
+    }
+
+    private fun initUriSelectedListner(context: Context) {
+        if (context is OnUrisSelectedListener) {
+            uriSelectedListener = context
+        } else {
+            throw RuntimeException()
+        }
+    }
+
+    private fun initRequestPermissionLauncher() {
+        requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) { isPermissionGranted ->
+                if (isPermissionGranted) {
+                    launchGallery()
+                } else {
+                    showPermissionSnackBar()
+                }
+            }
+    }
+
+    private fun launchGallery() {
+        val intent =
+            Intent(Intent.ACTION_PICK)
+                .setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*")
+                .putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true)
+        galleryLauncher.launch(intent)
+    }
+
+    private fun showPermissionSnackBar() {
+        val snackBar = makeSnackBar()
+        setSnackBarAction(snackBar)
+        snackBar.show()
+    }
+
+    private fun makeSnackBar(): Snackbar {
+        return Snackbar.make(
+            binding.root,
+            R.string.snack_bar_require_photo_album_permission,
+            Snackbar.LENGTH_LONG,
+        )
+    }
+
+    private fun setSnackBarAction(snackBar: Snackbar) {
+        snackBar.setAction(R.string.snack_bar_move_to_setting) {
+            val intent = Intent()
+            intent.action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+            val uri = Uri.fromParts(PACKAGE_SCHEME, requireContext().packageName, null)
+            intent.data = uri
+            startActivity(intent)
+        }
+    }
+
+    private fun initGalleryLauncher() {
+        galleryLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+                if (result.resultCode == RESULT_OK) {
+                    val imageUris = extractImageUris(result.data)
+                    if (imageUris.isNotEmpty()) {
+                        uriSelectedListener.onUrisSelected(imageUris)
+                        dismiss()
+                    } else {
+                        showToast("사진을 불러올 수 없습니다.")
+                    }
+                }
+            }
+    }
+
+    private fun extractImageUris(intent: Intent?): List<Uri> {
+        val imageUris = mutableListOf<Uri>()
+        intent?.let {
+            it.clipData?.let { clipData ->
+                repeat(clipData.itemCount) { index ->
+                    imageUris.add(clipData.getItemAt(index).uri)
+                }
+            } ?: it.data?.let { uri ->
+                imageUris.add(uri)
+            }
+        }
+        return imageUris
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -20,12 +128,45 @@ class PhotoAttachFragment : BottomSheetDialogFragment() {
         return binding.root
     }
 
+    override fun onViewCreated(
+        view: View,
+        savedInstanceState: Bundle?,
+    ) {
+        binding.handler = this
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
+    override fun onCameraClicked() {
+        // 카메라 열기
+    }
+
+    override fun onGalleryClicked() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            checkPermissionsAndLaunch(READ_MEDIA_IMAGES)
+        } else {
+            checkPermissionsAndLaunch(READ_EXTERNAL_STORAGE)
+        }
+    }
+
+    private fun checkPermissionsAndLaunch(permission: String) {
+        val isPermissionGranted =
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                permission,
+            ) == PackageManager.PERMISSION_GRANTED
+        if (isPermissionGranted) {
+            launchGallery()
+        } else {
+            requestPermissionLauncher.launch(permission)
+        }
+    }
+
     companion object {
         const val TAG = "PhotoAttachModalBottomSheet"
+        const val PACKAGE_SCHEME = "package"
     }
 }
