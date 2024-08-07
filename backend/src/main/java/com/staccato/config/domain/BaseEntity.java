9e1247b35ec8e9a05ea4aee620981526c699eb19
linirini <2001yerin@naver.com>
2024-08-07 11:30:16 +0900

    refactor: member 외 soft delete 제거

--- backend/src/main/java/com/staccato/config/domain/BaseEntity.java
+++ backend/src/main/java/com/staccato/config/domain/BaseEntity.java
@@ -19,5 +19,4 @@ public abstract class BaseEntity {
     private LocalDateTime createdAt;
     @LastModifiedDate
     private LocalDateTime updatedAt;
-    private Boolean isDeleted = false;
 }
--- backend/src/main/java/com/staccato/travel/domain/Travel.java
+++ backend/src/main/java/com/staccato/travel/domain/Travel.java
@@ -12,9 +12,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 
-import org.hibernate.annotations.SQLDelete;
-import org.hibernate.annotations.SQLRestriction;
-
 import com.staccato.config.domain.BaseEntity;
 import com.staccato.exception.StaccatoException;
 import com.staccato.member.domain.Member;
@@ -29,8 +26,6 @@ import lombok.NonNull;
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@SQLDelete(sql = "UPDATE travel SET is_deleted = true WHERE id = ?")
-@SQLRestriction("is_deleted = false")
 public class Travel extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
