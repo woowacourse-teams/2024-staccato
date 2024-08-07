d8e767208bbe6753211f6d8357e4385e1941ea0f
hxeyexn <kck85075@kumoh.ac.kr>
2024-08-07 21:50:15 +0900

    refactor: DEFAULT_VALUE 상수 제거

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/HeaderInterceptor.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/HeaderInterceptor.kt
@@ -4,7 +4,7 @@ import com.woowacourse.staccato.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.Response
 
-class HeaderInterceptor(private val headerValue: String = DEFAULT_VALUE) : Interceptor {
+class HeaderInterceptor(private val headerValue: String = BuildConfig.TOKEN) : Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         val request =
             chain.request()
@@ -16,6 +16,5 @@ class HeaderInterceptor(private val headerValue: String = DEFAULT_VALUE) : Inter
 
     companion object {
         const val AUTHORIZATION_HEADER = "Authorization"
-        const val DEFAULT_VALUE = BuildConfig.TOKEN
     }
 }
