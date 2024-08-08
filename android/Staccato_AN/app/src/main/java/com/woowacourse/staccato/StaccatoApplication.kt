975baf80428c55b96cb2e117307476460a564294
hxeyexn <kck85075@kumoh.ac.kr>
2024-08-09 05:54:58 +0900

    feat: Application 구현

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/StaccatoApplication.kt
@@ -0,0 +1,11 @@
+package com.woowacourse.staccato
+
+import android.app.Application
+import com.google.firebase.FirebaseApp
+
+class StaccatoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        FirebaseApp.initializeApp(this)
+    }
+}
