25501f8400efb6e102940aaacde6179b7d0e1e8f
Seongju Lee <sherlocky31415@gmail.com>
2024-08-09 08:59:25 +0900

    feat: swagger https 적용하기 #184 (#185)
    * feat: swagger가 https 접근 가능하도록 하는 기능 구현
    
    * chore: pull_request에도 CD가 적용되도록 임시 변경
    
    * style: push에만 CD가 적용되도록 롤백

--- backend/src/main/java/com/staccato/StaccatoApplication.java
+++ backend/src/main/java/com/staccato/StaccatoApplication.java
@@ -3,6 +3,10 @@ package com.staccato;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://staccato.kr", description = "Default Server URL")})
 @SpringBootApplication
 public class StaccatoApplication {
 
