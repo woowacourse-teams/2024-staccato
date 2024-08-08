a9c25f84f00bb6b27786a5b95e1d64e507294e39
hodu <92203597+Junyoung-WON@users.noreply.github.com>
2024-08-09 03:15:06 +0900

    refactor: 타임라인 화면 리팩터링 #162 (#179)
    * ui: 타임라인 RecyclerView의 크기 조정 및 여백 수정
    
    * feat: Activity와 Fragment 간 데이터를 공유하는 공유 ViewModel 생성
    
    * feat: 공유 ViewModel을 이용하여 타임라인 업데이트 여부를 공유
    
    * refactor: RecyclerView.Adapter에서 ListAdapter로 변환
    
    * refactor: 처음 타임라인을 불러오는 동작을 ViewModel 초기화 시에 수행
    
    * fix: ListAdapter 수정
    
    - 불필요한 travels 프로퍼티와 getItemCounts 메서드 제거
    - currentList를 사용하는 것으로 변경
    
    * refactor: 에러 핸들링 방식 수정 및 ViewModel 수정
    
    - TimelineDefaultRepository로 네이밍 변경
    - ApiResponseHandler와 ResponseResult를 이용하여 에러 핸들링 처리
    - TimelineViewModel 에러 메시지 LiveData 사용
    - 그 외 repository, dataSource 프로퍼티 이름 수정하여 통일
    
    * refactor: MutableLiveData의 값 업데이트를 setValue로 변경 및 메서드 분리
    
    * style: ktlint 적용

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/timeline/TimelineDataSource.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/timeline/TimelineDataSource.kt
@@ -1,9 +1,10 @@
 package com.woowacourse.staccato.data.timeline
 
+import com.woowacourse.staccato.data.ResponseResult
 import com.woowacourse.staccato.data.dto.timeline.TimelineResponse
 
 interface TimelineDataSource {
-    suspend fun getAllTimeline(): Result<TimelineResponse>
+    suspend fun getAllTimeline(): ResponseResult<TimelineResponse>
 
-    suspend fun getTimeline(year: Int): Result<TimelineResponse>
+    suspend fun getTimeline(year: Int): ResponseResult<TimelineResponse>
 }
--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/timeline/TimelineFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/timeline/TimelineFragment.kt
@@ -3,18 +3,21 @@ package com.woowacourse.staccato.presentation.timeline
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
+import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.woowacourse.staccato.R
 import com.woowacourse.staccato.databinding.FragmentTimelineBinding
 import com.woowacourse.staccato.presentation.base.BindingFragment
+import com.woowacourse.staccato.presentation.main.SharedViewModel
 import com.woowacourse.staccato.presentation.timeline.adapter.TimelineAdapter
 import com.woowacourse.staccato.presentation.util.showToast
 
 class TimelineFragment :
     BindingFragment<FragmentTimelineBinding>(R.layout.fragment_timeline),
     TimelineHandler {
-    private val viewModel: TimelineViewModel by viewModels { TimelineViewModelFactory() }
+    private val timelineViewModel: TimelineViewModel by viewModels { TimelineViewModelFactory() }
+    private val sharedViewModel: SharedViewModel by activityViewModels<SharedViewModel>()
     private lateinit var adapter: TimelineAdapter
 
     override fun onViewCreated(
@@ -23,7 +26,6 @@ class TimelineFragment :
     ) {
         setUpAdapter()
         setUpObserving()
-        viewModel.loadTimeline()
     }
 
     private fun setUpAdapter() {
@@ -32,14 +34,17 @@ class TimelineFragment :
     }
 
     private fun setUpObserving() {
-        viewModel.travels.observe(viewLifecycleOwner) { timeline ->
-            adapter.setTravels(timeline)
+        timelineViewModel.travels.observe(viewLifecycleOwner) { timeline ->
+            adapter.updateTimeline(timeline)
         }
-        viewModel.errorState.observe(viewLifecycleOwner) { event ->
-            event.getContentIfNotHandled()?.let { state ->
-                if (state) {
-                    showToast(ERROR_MESSAGE)
-                }
+
+        timelineViewModel.errorMessage.observe(viewLifecycleOwner) { message ->
+            showToast(message)
+        }
+
+        sharedViewModel.isTimelineUpdated.observe(viewLifecycleOwner) { isUpdated ->
+            if (isUpdated) {
+                timelineViewModel.loadTimeline()
             }
         }
     }
@@ -50,12 +55,10 @@ class TimelineFragment :
 
     override fun onTravelClicked(travelId: Long) {
         val bundle: Bundle = bundleOf(TRAVEL_ID_KEY to travelId)
-        // Log.d("ㅌㅅㅌ", "clicked item: $travelId")
         navigateToTravel(bundle)
     }
 
     companion object {
         const val TRAVEL_ID_KEY = "travelId"
-        const val ERROR_MESSAGE = "일시적인 오류가 발생했습니다.\n잠시 후 다시 시도해주세요."
     }
 }
