185313a3c9d18425b0528ab01c4636916e7f2f44
yoonjuho <dbswn990@naver.com>
2024-08-08 15:54:28 +0900

    feat: 로깅 White List 추가

--- backend/src/main/java/com/staccato/config/log/LoggingFilter.java
+++ backend/src/main/java/com/staccato/config/log/LoggingFilter.java
@@ -1,6 +1,7 @@
 package com.staccato.config.log;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Objects;
 
 import jakarta.servlet.FilterChain;
@@ -10,6 +11,7 @@ import jakarta.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import lombok.extern.slf4j.Slf4j;
@@ -17,6 +19,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class LoggingFilter extends OncePerRequestFilter {
+    private static final List<String> WHITE_LIST = List.of("/h2-console/**", "/favicon/**", "/swagger-ui/**", "/v3/api-docs/**");
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         long startTime = System.currentTimeMillis();
@@ -35,5 +39,12 @@ public class LoggingFilter extends OncePerRequestFilter {
 
     private boolean tokenExists(String token) {
         return !(Objects.isNull(token) || token.isBlank());
+    }
+
+    @Override
+    protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
+        String requestURI = request.getRequestURI();
+        AntPathMatcher antPathMatcher = new AntPathMatcher();
+        return WHITE_LIST.stream().anyMatch(path -> antPathMatcher.match(path, requestURI));
     }
 }
