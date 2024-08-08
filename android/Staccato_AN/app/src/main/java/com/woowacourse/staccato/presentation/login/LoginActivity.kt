058720fc2a5ab10da07db03429ff50a218d93354
Junyoung-WON <wjyo60@gmail.com>
2024-08-08 14:18:19 +0900

    feat: 키보드 활성화 상태에서 화면 터치 시 키보드를 내리는 기능 추가

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/login/LoginActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/login/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.woowacourse.staccato.presentation.login
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityOptionsCompat
@@ -33,6 +35,7 @@ class LoginActivity : AppCompatActivity() {
         setBinding()
         observeLoginState()
         observeErrorEvent()
+        setHideKeyboardAction()
     }
 
     private fun checkIfLoggedIn(splashScreen: SplashScreen) {
@@ -72,10 +75,6 @@ class LoginActivity : AppCompatActivity() {
         binding.handler = loginViewModel
     }
 
-    private fun observeErrorEvent() {
-        loginViewModel.errorMessage.observe(this, ::showToast)
-    }
-
     private fun observeLoginState() {
         loginViewModel.isLoginSuccess.observe(this, ::checkLoginSuccess)
     }
@@ -84,6 +83,27 @@ class LoginActivity : AppCompatActivity() {
         if (success) {
             showToast(LOGIN_SUCCESS_MESSAGE)
             navigateToMainActivity()
+        }
+    }
+
+    private fun observeErrorEvent() {
+        loginViewModel.errorMessage.observe(this, ::showToast)
+    }
+
+    private fun setHideKeyboardAction() {
+        binding.root.setOnClickListener {
+            hideKeyboard()
+        }
+    }
+
+    private fun hideKeyboard() {
+        if (this.currentFocus != null) {
+            val inputManager: InputMethodManager =
+                this.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputManager.hideSoftInputFromWindow(
+                this.currentFocus?.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
         }
     }
 
