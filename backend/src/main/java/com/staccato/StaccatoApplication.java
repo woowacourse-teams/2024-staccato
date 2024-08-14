edda4790cd88922f68ca2462743537661cd32229
BurningFalls <sherlocky31415@gmail.com>
2024-08-14 21:31:10 +0900

    refactor: dev, stage, local 환경의 swagger url 설정

--- backend/src/main/java/com/staccato/StaccatoApplication.java
+++ backend/src/main/java/com/staccato/StaccatoApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(servers = {@Server(url = "https://staccato.kr", description = "Default Server URL")})
 @SpringBootApplication
 public class StaccatoApplication {
 
