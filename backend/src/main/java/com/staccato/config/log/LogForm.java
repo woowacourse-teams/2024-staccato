0f6df887d949a7fac960c5decfbcc1ccfdc179a5
yoonjuho <dbswn990@naver.com>
2024-08-15 22:01:07 +0900

    feat: MDC 적용

--- backend/src/main/java/com/staccato/config/log/LogForm.java
+++ backend/src/main/java/com/staccato/config/log/LogForm.java
@@ -1,9 +1,18 @@
 package com.staccato.config.log;
 
 public class LogForm {
+    private static final String START_DELIMITER = "[";
+    private static final String END_DELIMITER = "]";
     private static final String DELIMITER = " | ";
-    public static final String REQUEST_LOGGING_FORM =
-            "HTTP Method : {}" + DELIMITER + "Request URI : {}" + DELIMITER + "Token is Exist : {}" + DELIMITER + "HTTP Status : {}" + DELIMITER + "Processing Time(ms) : {}";
+    public static final String REQUEST_LOGGING_FORM = START_DELIMITER +
+            "HTTP Status : {}" + DELIMITER +
+            "HTTP Method : {}" + DELIMITER +
+            "Request URI : {}" + DELIMITER +
+            "Token is Exist : {}" + DELIMITER +
+            "Processing Time(ms) : {}" + END_DELIMITER;
+    public static final String LOGIN_MEMBER_FORM = START_DELIMITER +
+            "Login Member ID : {}" + DELIMITER +
+            "Login Member Nickname : {}" + END_DELIMITER;
     public static final String EXCEPTION_LOGGING_FORM = "Exception Response : {} ";
     public static final String ERROR_LOGGING_FORM = "Exception Response : {} " + DELIMITER + "Exception Message : {}";
 }
