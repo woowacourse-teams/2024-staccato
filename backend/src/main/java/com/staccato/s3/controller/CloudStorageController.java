89e260d66c13d5825844fe5027d71b4eb43ebff7
BurningFalls <sherlocky31415@gmail.com>
2024-08-15 16:47:21 +0900

    refactor: requestparam -> requestpart로 변경

--- backend/src/main/java/com/staccato/s3/controller/CloudStorageController.java
+++ backend/src/main/java/com/staccato/s3/controller/CloudStorageController.java
@@ -2,12 +2,15 @@ package com.staccato.s3.controller;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Size;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -22,7 +25,7 @@ public class CloudStorageController {
     private final CloudStorageService cloudStorageService;
 
     @PostMapping
-    public ResponseEntity<List<String>> uploadFiles(@RequestParam("imageFiles") List<MultipartFile> files) {
+    public ResponseEntity<List<String>> uploadFiles(@RequestPart(value = "imagefiles") List<MultipartFile> files) {
         List<String> fileUrls = cloudStorageService.uploadFiles(files);
         return ResponseEntity.status(HttpStatus.CREATED).body(fileUrls);
     }
