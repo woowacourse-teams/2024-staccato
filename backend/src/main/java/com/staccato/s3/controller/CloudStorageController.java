ea19ddf31169edf39db8747e4ee8285bc56dd6a0
BurningFalls <sherlocky31415@gmail.com>
2024-08-15 20:13:57 +0900

    feat: validated 어노테이션 추가해서 유효성 검사 수행

--- backend/src/main/java/com/staccato/s3/controller/CloudStorageController.java
+++ backend/src/main/java/com/staccato/s3/controller/CloudStorageController.java
@@ -8,6 +8,7 @@ import jakarta.validation.constraints.Size;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,6 +24,7 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequestMapping("/captures")
 @RequiredArgsConstructor
+@Validated
 public class CloudStorageController {
     private final CloudStorageService cloudStorageService;
 
