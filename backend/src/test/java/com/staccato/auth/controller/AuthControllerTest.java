4c31848b572f0572b1eedfdeae5beb17aa36bf9d
linirini <2001yerin@naver.com>
2024-08-15 17:03:04 +0900

    style: code convention 적용

--- backend/src/test/java/com/staccato/auth/controller/AuthControllerTest.java
+++ backend/src/test/java/com/staccato/auth/controller/AuthControllerTest.java
@@ -7,8 +7,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
