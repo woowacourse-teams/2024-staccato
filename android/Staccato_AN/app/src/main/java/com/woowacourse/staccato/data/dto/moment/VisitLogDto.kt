e2b7cb2dc15a4dac256d53ca13995fd0a3a6082f
hxeyexn <kck85075@kumoh.ac.kr>
2024-08-16 02:39:53 +0900

    refactor: VisitLogDto 내 SerialName 변경
    - visitLogId를 commentId로 변경

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/moment/VisitLogDto.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/moment/VisitLogDto.kt
@@ -5,7 +5,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class VisitLogDto(
-    @SerialName("visitLogId") val visitLogId: Long,
+    @SerialName("commentId") val visitLogId: Long,
     @SerialName("memberId") val memberId: Long,
     @SerialName("nickname") val nickname: String,
     @SerialName("memberImageUrl") val memberImageUrl: String,
