2155f10ee9a968bec4570103b1e68ccf3baa4a25
YoonJuHo <dbswn990@naver.com>
2024-08-16 11:21:55 +0900

    feat: 사용자 로깅, Nginx 로깅, DB 로깅 #190 (#224)
    * feat: MDC 적용
    
    * refactor: 중복 예외 제거
    
    * feat: 사용자 식별 로깅 추가
    
    * refactor: 예외 메세지 형식 json으로 변경

--- backend/src/main/java/com/staccato/config/log/LogForm.java
+++ backend/src/main/java/com/staccato/config/log/LogForm.java
@@ -1,9 +1,28 @@
 package com.staccato.config.log;
 
 public class LogForm {
-    private static final String DELIMITER = " | ";
-    public static final String REQUEST_LOGGING_FORM =
-            "HTTP Method : {}" + DELIMITER + "Request URI : {}" + DELIMITER + "Token is Exist : {}" + DELIMITER + "HTTP Status : {}" + DELIMITER + "Processing Time(ms) : {}";
-    public static final String EXCEPTION_LOGGING_FORM = "Exception Response : {} ";
-    public static final String ERROR_LOGGING_FORM = "Exception Response : {} " + DELIMITER + "Exception Message : {}";
+    private static final String DELIMITER = ",\n    ";
+    private static final String INDENT = "    ";
+
+    public static final String REQUEST_LOGGING_FORM = "\n{\n" +
+            INDENT + "\"httpStatus\": \"{}\"" + DELIMITER +
+            "\"httpMethod\": \"{}\"" + DELIMITER +
+            "\"requestUri\": \"{}\"" + DELIMITER +
+            "\"tokenExists\": \"{}\"" + DELIMITER +
+            "\"processingTimeMs\": \"{}\"\n" +
+            "}";
+
+    public static final String LOGIN_MEMBER_FORM = "\n{\n" +
+            INDENT + "\"loginMemberId\": \"{}\"" + DELIMITER +
+            "\"loginMemberNickname\": \"{}\"\n" +
+            "}";
+
+    public static final String EXCEPTION_LOGGING_FORM = "\n{\n" +
+            INDENT + "\"exceptionResponse\": \"{}\"\n" +
+            "}";
+
+    public static final String ERROR_LOGGING_FORM = "\n{\n" +
+            INDENT + "\"exceptionResponse\": \"{}\"" + DELIMITER +
+            "\"exceptionMessage\": \"{}\"\n" +
+            "}";
 }
