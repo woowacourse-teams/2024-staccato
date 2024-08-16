d733d73bf2fb13be27ae07bfa10c5780a5a2a63d
BurningFalls <sherlocky31415@gmail.com>
2024-08-17 07:42:11 +0900

    refactor: 미사용 import문 삭제

--- backend/src/main/java/com/staccato/image/controller/ImageController.java
+++ backend/src/main/java/com/staccato/image/controller/ImageController.java
@@ -3,7 +3,6 @@ package com.staccato.image.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestPart;
