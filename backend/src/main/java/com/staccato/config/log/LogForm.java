5051627a5c4b08b37bfc54b6d1244569260646db
yoonjuho <dbswn990@naver.com>
2024-08-16 11:19:57 +0900

    refactor: 예외 메세지 형식 json으로 변경

--- backend/src/main/java/com/staccato/config/log/LogForm.java
+++ backend/src/main/java/com/staccato/config/log/LogForm.java
@@ -1,18 +1,28 @@
 package com.staccato.config.log;
 
 public class LogForm {
-    private static final String START_DELIMITER = "[";
-    private static final String END_DELIMITER = "]";
-    private static final String DELIMITER = " | ";
-    public static final String REQUEST_LOGGING_FORM = START_DELIMITER +
-            "HTTP Status : {}" + DELIMITER +
-            "HTTP Method : {}" + DELIMITER +
-            "Request URI : {}" + DELIMITER +
-            "Token is Exist : {}" + DELIMITER +
-            "Processing Time(ms) : {}" + END_DELIMITER;
-    public static final String LOGIN_MEMBER_FORM = START_DELIMITER +
-            "Login Member ID : {}" + DELIMITER +
-            "Login Member Nickname : {}" + END_DELIMITER;
-    public static final String EXCEPTION_LOGGING_FORM = "Exception Response : {} ";
-    public static final String ERROR_LOGGING_FORM = "Exception Response : {} " + DELIMITER + "Exception Message : {}";
+    private static final String DELIMITER = ",\n    ";
+    private static final String INDENT = "    ";
+
+    public static final String REQUEST_LOGGING_FORM = "\n{\n" +
+            INDENT + "\"httpStatus\": \"{}\"" + DELIMITER +
+            "\"httpMethod\": \"{}\"" + DELIMITER +
+            "\"requestUri\": \"{}\"" + DELIMITER +
+            "\"tokenExists\": \"{}\"" + DELIMITER +
+            "\"processingTimeMs\": \"{}\"\n" +
+            "}";
+
+    public static final String LOGIN_MEMBER_FORM = "\n{\n" +
+            INDENT + "\"loginMemberId\": \"{}\"" + DELIMITER +
+            "\"loginMemberNickname\": \"{}\"\n" +
+            "}";
+
+    public static final String EXCEPTION_LOGGING_FORM = "\n{\n" +
+            INDENT + "\"exceptionResponse\": \"{}\"\n" +
+            "}";
+
+    public static final String ERROR_LOGGING_FORM = "\n{\n" +
+            INDENT + "\"exceptionResponse\": \"{}\"" + DELIMITER +
+            "\"exceptionMessage\": \"{}\"\n" +
+            "}";
 }
