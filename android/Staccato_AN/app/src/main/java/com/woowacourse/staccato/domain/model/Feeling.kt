4f9ded27f0ee5aaa74226f62b3718f6380efc25f
Junyoung-WON <wjyo60@gmail.com>
2024-08-16 17:20:20 +0900

    feat: 기분 도메인 모델 생성

---
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/domain/model/Feeling.kt
@@ -0,0 +1,11 @@
+package com.woowacourse.staccato.domain.model
+
+enum class Feeling(val value: String) {
+    HAPPY("happy"),
+    ANGRY("angry"),
+    SAD("sad"),
+    SCARED("scared"),
+    EXCITED("excited"),
+    NOTHING("nothing"),
+    ;
+}
