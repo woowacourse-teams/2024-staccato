ae746e3e642f45207438a5d01b18ebe35a47bb60
BurningFalls <sherlocky31415@gmail.com>
2024-08-17 05:34:09 +0900

    test: 테스트 Disabled

--- backend/src/test/java/com/staccato/s3/controller/CloudStorageControllerTest.java
+++ backend/src/test/java/com/staccato/s3/controller/CloudStorageControllerTest.java
@@ -10,6 +10,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,7 @@ import com.staccato.exception.ExceptionResponse;
 import com.staccato.member.domain.Member;
 import com.staccato.s3.service.CloudStorageService;
 
+@Disabled
 @WebMvcTest(controllers = CloudStorageController.class)
 public class CloudStorageControllerTest {
     @Autowired
