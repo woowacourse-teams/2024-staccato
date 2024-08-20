25575a54c484f7c862a6c66890b98d28bac7fe4e
hxeyexn <kck85075@kumoh.ac.kr>
2024-08-20 12:54:30 +0900

    build: 구글맵 관련 properties ignore 추가

--- android/Staccato_AN/app/build.gradle.kts
+++ android/Staccato_AN/app/build.gradle.kts
@@ -122,3 +122,11 @@ dependencies {
     // Google Map
     implementation(libs.play.services.maps)
 }
+
+secrets {
+    propertiesFileName = "secrets.properties"
+    defaultPropertiesFileName = "local.defaults.properties"
+
+    ignoreList.add("keyToIgnore")
+    ignoreList.add("sdk.*")
+}
