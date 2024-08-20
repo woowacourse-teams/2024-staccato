dc25df181d3540844a27b41ea2482efd8443d788
hxeyexn <kck85075@kumoh.ac.kr>
2024-08-20 20:54:10 +0900

    feat: 위치 권한 요청 구현
    - ACCESS_FINE_LOCATION 권한 요청
    - ACCESS_COARSE_LOCATION 권한 요청

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/home/MapsFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/home/MapsFragment.kt
@@ -1,24 +1,45 @@
 package com.woowacourse.staccato.presentation.home
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.snackbar.Snackbar
 import com.woowacourse.staccato.R
 
 class MapsFragment : Fragment() {
     private val callback =
         OnMapReadyCallback { googleMap ->
             val woowacourse = LatLng(37.505, 127.050)
-            googleMap.addMarker(MarkerOptions().position(woowacourse).title("우아한테크코스"))
+            googleMap.addMarker(MarkerOptions().position(woowacourse).title("우아한테크코스 선릉 캠퍼스"))
             googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(woowacourse, 13f))
         }
+
+    private val locationPermissions: List<String> =
+        listOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+        )
+
+    private val requestPermission = initRequestPermissionsLauncher()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkLocationPermissions()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -35,5 +56,56 @@ class MapsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+    }
+
+    private fun checkLocationPermissions() {
+        val isAccessFineLocationGranted =
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ) == PackageManager.PERMISSION_GRANTED
+
+        val isAccessCoarseLocationGranted =
+            ContextCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ) == PackageManager.PERMISSION_GRANTED
+
+        if (isAccessFineLocationGranted || isAccessCoarseLocationGranted) {
+            // TODO: 현 위치 가져오기
+        } else {
+            requestPermission.launch(locationPermissions.toTypedArray())
+        }
+    }
+
+    private fun initRequestPermissionsLauncher() =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.forEach { (_, isGranted) ->
+                if (isGranted) {
+                    makeSnackBar(getString(R.string.maps_location_permission_granted_message)).show()
+                } else {
+                    showPermissionRequestSnackBar()
+                }
+            }
+        }
+
+    private fun showPermissionRequestSnackBar() {
+        val snackBar = makeSnackBar(getString(R.string.maps_location_permission_required_message))
+        snackBar.setAction()
+        snackBar.show()
+    }
+
+    private fun makeSnackBar(message: String): Snackbar = Snackbar.make(requireView(), message, Snackbar.LENGTH_LONG)
+
+    private fun Snackbar.setAction() {
+        setAction(R.string.snack_bar_move_to_setting) {
+            val uri = Uri.fromParts(PACKAGE_SCHEME, context.packageName, null)
+            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).setData(uri)
+            startActivity(intent)
+        }
+    }
+
+    companion object {
+        private const val PACKAGE_SCHEME = "package"
     }
 }
