bc8672c3fd595c222a7e81b3be642f6b89b25e22
Junyoung-WON <wjyo60@gmail.com>
2024-08-21 21:26:53 +0900

    feat: 순간(현 스타카토)의 Ui Model Mapper 변경
    MomentDetailUiModel.CommentsUiModel -> CommentUiModel
    
    MomentDetailUiModel.MomentDefaultUiModel -> MomentDetailUiModel
    
    Feeling을 Ui Model로 변환하는 Mapper 추가

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/mapper/VisitUiModelMapper.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/mapper/VisitUiModelMapper.kt
@@ -1,12 +1,16 @@
 package com.woowacourse.staccato.presentation.mapper
 
+import com.woowacourse.staccato.R
+import com.woowacourse.staccato.domain.model.Feeling
 import com.woowacourse.staccato.domain.model.Moment
 import com.woowacourse.staccato.domain.model.VisitLog
-import com.woowacourse.staccato.presentation.moment.model.MomentDetailUiModel
+import com.woowacourse.staccato.presentation.moment.comments.CommentUiModel
+import com.woowacourse.staccato.presentation.moment.detail.MomentDetailUiModel
+import com.woowacourse.staccato.presentation.moment.feeling.FeelingUiModel
 import com.woowacourse.staccato.presentation.visitupdate.model.VisitUpdateDefaultUiModel
 
-fun Moment.toVisitDefaultUiModel(): MomentDetailUiModel.MomentDefaultUiModel {
-    return MomentDetailUiModel.MomentDefaultUiModel(
+fun Moment.toMomentDetailUiModel(): MomentDetailUiModel {
+    return MomentDetailUiModel(
         id = momentId,
         placeName = placeName,
         momentImageUrls = momentImageUrls,
@@ -23,11 +27,23 @@ fun Moment.toVisitUpdateDefaultUiModel(): VisitUpdateDefaultUiModel {
     )
 }
 
-fun VisitLog.toVisitLogUiModel() =
-    MomentDetailUiModel.CommentsUiModel(
+fun VisitLog.toCommentUiModel() =
+    CommentUiModel(
         id = visitLogId,
         memberId = memberId,
         nickname = nickname,
         memberImageUrl = memberImageUrl,
         content = content,
     )
+
+fun Feeling.toFeelingUiModel(selectedFeeling: String = Feeling.NOTHING.value): FeelingUiModel {
+    val src = when (this) {
+        Feeling.HAPPY -> R.drawable.feeling_happy
+        Feeling.ANGRY -> R.drawable.feeling_angry
+        Feeling.SAD -> R.drawable.feeling_sad
+        Feeling.SCARED -> R.drawable.feeling_scared
+        Feeling.EXCITED -> R.drawable.feeling_excited
+        Feeling.NOTHING -> null
+    }
+    return FeelingUiModel(value, src, selectedFeeling == this.value)
+}
