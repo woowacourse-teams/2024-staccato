07c23887815a22192272b690d84643b088ad3d84
Junyoung-WON <wjyo60@gmail.com>
2024-08-21 22:40:09 +0900

    feat: 순간 조회 화면 재구성

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/moment/viewmodel/MomentViewModel.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/moment/viewmodel/MomentViewModel.kt
@@ -4,20 +4,25 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.woowacourse.staccato.domain.model.Feeling
 import com.woowacourse.staccato.domain.repository.MomentRepository
 import com.woowacourse.staccato.presentation.common.MutableSingleLiveData
 import com.woowacourse.staccato.presentation.common.SingleLiveData
-import com.woowacourse.staccato.presentation.mapper.toVisitDefaultUiModel
-import com.woowacourse.staccato.presentation.mapper.toVisitLogUiModel
-import com.woowacourse.staccato.presentation.moment.model.MomentDetailUiModel
+import com.woowacourse.staccato.presentation.mapper.toCommentUiModel
+import com.woowacourse.staccato.presentation.mapper.toMomentDetailUiModel
+import com.woowacourse.staccato.presentation.moment.comments.CommentUiModel
+import com.woowacourse.staccato.presentation.moment.detail.MomentDetailUiModel
 import kotlinx.coroutines.launch
 
 class MomentViewModel(private val momentRepository: MomentRepository) : ViewModel() {
-    private val _momentDefault = MutableLiveData<MomentDetailUiModel.MomentDefaultUiModel>()
-    val momentDefault: LiveData<MomentDetailUiModel.MomentDefaultUiModel> get() = _momentDefault
+    private val _momentDetail = MutableLiveData<MomentDetailUiModel>()
+    val momentDetail: LiveData<MomentDetailUiModel> get() = _momentDetail
 
-    private val _visitLogs = MutableLiveData<List<MomentDetailUiModel.CommentsUiModel>>()
-    val visitLogs: LiveData<List<MomentDetailUiModel.CommentsUiModel>> get() = _visitLogs
+    private val _comments = MutableLiveData<List<CommentUiModel>>()
+    val comments: LiveData<List<CommentUiModel>> get() = _comments
+
+    private val _feeling = MutableLiveData<Feeling>()
+    val feeling: LiveData<Feeling> get() = _feeling
 
     private val _isDeleted = MutableSingleLiveData(false)
     val isDeleted: SingleLiveData<Boolean> get() = _isDeleted
@@ -25,7 +30,7 @@ class MomentViewModel(private val momentRepository: MomentRepository) : ViewMode
     private val _isError = MutableSingleLiveData(false)
     val isError: SingleLiveData<Boolean> get() = _isError
 
-    fun fetchMomentDetailData(momentId: Long) {
+    fun loadMoment(momentId: Long) {
         fetchMomentData(momentId)
     }
 
@@ -39,8 +44,9 @@ class MomentViewModel(private val momentRepository: MomentRepository) : ViewMode
     private fun fetchMomentData(momentId: Long) {
         viewModelScope.launch {
             momentRepository.getMoment(momentId).onSuccess { moment ->
-                _momentDefault.value = moment.toVisitDefaultUiModel()
-                _visitLogs.value = moment.comments.map { it.toVisitLogUiModel() }
+                _momentDetail.value = moment.toMomentDetailUiModel()
+                _feeling.value = moment.feeling
+                _comments.value = moment.comments.map { it.toCommentUiModel() }
             }.onFailure {
                 _isError.postValue(true)
             }
