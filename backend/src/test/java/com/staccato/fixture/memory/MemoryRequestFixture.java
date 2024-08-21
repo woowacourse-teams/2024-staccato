9a12e01abd24d46a5b68643bc90ac17f413dd9f7
linirini <101927543+linirini@users.noreply.github.com>
2024-08-21 20:42:52 +0900

    feat: 추억 이름 중복 불가 예외 처리 #280 (#282)
    * feat: 추억 제목 중복 검사 구현
    
    * docs: 예외 발생 케이스 문서화
    
    * test: 픽스처 활용
    
    * feat: 추억 수정 시 이미 존재하는 타 추억 이름으로 변경 불가능 예외 처리
    
    * test: 주석 오타 수정

--- backend/src/test/java/com/staccato/fixture/memory/MemoryRequestFixture.java
+++ backend/src/test/java/com/staccato/fixture/memory/MemoryRequestFixture.java
@@ -15,6 +15,16 @@ public class MemoryRequestFixture {
         );
     }
 
+    public static MemoryRequest create(LocalDate startAt, LocalDate endAt, String title) {
+        return new MemoryRequest(
+                "https://example.com/memorys/geumohrm.jpg",
+                title,
+                "친구들과 함께한 여름 휴가 추억",
+                startAt,
+                endAt
+        );
+    }
+
     public static MemoryRequest create(String imageUrl, LocalDate startAt, LocalDate endAt) {
         return new MemoryRequest(
                 imageUrl,
