dadc9621689262c87d4c96c0ea00d15fca8f5038
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-08-23 00:39:10 +0900

    feat: 스타카토 생성 시 현 위치의 주소를 가져오는 기능 구현 #314 (#324)
    * feat: MainActivity에서 주소를 가져오는 로직 삭제
    
    * feat: 스타카토 생성 시 현 위치의 주소를 가져오는 로직 구현
    
    * refactor: 메서드 순서 정렬

--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/moment/MomentCreationRequest.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/data/dto/moment/MomentCreationRequest.kt
@@ -7,8 +7,8 @@ import kotlinx.serialization.Serializable
 data class MomentCreationRequest(
     @SerialName("memoryId") val memoryId: Long,
     @SerialName("placeName") val placeName: String,
-    @SerialName("latitude") val latitude: String,
-    @SerialName("longitude") val longitude: String,
+    @SerialName("latitude") val latitude: Double,
+    @SerialName("longitude") val longitude: Double,
     @SerialName("address") val address: String,
     @SerialName("visitedAt") val visitedAt: String,
     @SerialName("momentImageUrls") val momentImageUrls: List<String>,
--- android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/momentcreation/MomentCreationActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/woowacourse/staccato/presentation/momentcreation/MomentCreationActivity.kt
@@ -1,18 +1,33 @@
 package com.woowacourse.staccato.presentation.momentcreation
 
+import android.Manifest.permission.ACCESS_COARSE_LOCATION
+import android.Manifest.permission.ACCESS_FINE_LOCATION
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_APPLICATION_DETAILS_SETTINGS
 import android.view.MotionEvent
 import android.view.View
 import android.view.WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.material.snackbar.Snackbar
 import com.woowacourse.staccato.R
 import com.woowacourse.staccato.databinding.ActivityVisitCreationBinding
 import com.woowacourse.staccato.presentation.base.BindingActivity
@@ -42,6 +57,9 @@ class MomentCreationActivity :
     private lateinit var itemTouchHelper: ItemTouchHelper
     private val memoryId by lazy { intent.getLongExtra(MEMORY_ID_KEY, 0L) }
     private val memoryTitle by lazy { intent.getStringExtra(MEMORY_TITLE_KEY) ?: "" }
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var address: String
+
     private val inputManager: InputMethodManager by lazy {
         getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
     }
@@ -67,6 +85,37 @@ class MomentCreationActivity :
         return super.dispatchTouchEvent(event)
     }
 
+    override fun initStartView(savedInstanceState: Bundle?) {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        initAddress()
+        initBinding()
+        initMemoryInfo()
+        initAdapter()
+        initItemTouchHelper()
+        initToolbar()
+        observeViewModelData()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray,
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            LOCATION_PERMISSION_REQUEST_CODE -> {
+                if (grantResults.isNotEmpty() && grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+                    showToast(getString(R.string.maps_location_permission_granted_message))
+                } else {
+                    val snackBar = makeSnackBar(getString(R.string.maps_location_permission_required_message))
+                    snackBar.setAction()
+                    snackBar.show()
+                }
+                return
+            }
+        }
+    }
+
     private fun isTouchInsideView(
         event: MotionEvent,
         view: View,
@@ -88,13 +137,14 @@ class MomentCreationActivity :
         )
     }
 
-    override fun initStartView(savedInstanceState: Bundle?) {
-        initBinding()
-        initMemoryInfo()
-        initAdapter()
-        initItemTouchHelper()
-        initToolbar()
-        observeViewModelData()
+    private fun makeSnackBar(message: String): Snackbar = Snackbar.make(binding.root, message, Snackbar.LENGTH_LONG)
+
+    private fun Snackbar.setAction() {
+        setAction(R.string.snack_bar_move_to_setting) {
+            val uri = Uri.fromParts(PhotoAttachFragment.PACKAGE_SCHEME, context.packageName, null)
+            val intent = Intent(ACTION_APPLICATION_DETAILS_SETTINGS).setData(uri)
+            startActivity(intent)
+        }
     }
 
     private fun initBinding() {
@@ -165,6 +215,67 @@ class MomentCreationActivity :
         }
     }
 
+    private fun initAddress() {
+        val isAccessFineLocationGranted =
+            ContextCompat.checkSelfPermission(
+                this,
+                ACCESS_FINE_LOCATION,
+            ) == PackageManager.PERMISSION_GRANTED
+
+        val isAccessCoarseLocationGranted =
+            ContextCompat.checkSelfPermission(
+                this,
+                ACCESS_COARSE_LOCATION,
+            ) == PackageManager.PERMISSION_GRANTED
+
+        if (isAccessFineLocationGranted || isAccessCoarseLocationGranted) {
+            val currentLocation =
+                fusedLocationProviderClient.getCurrentLocation(LocationRequest.PRIORITY_HIGH_ACCURACY, null)
+            currentLocation.addOnSuccessListener { location ->
+                fetchAddress(location)
+            }
+            return
+        } else {
+            requestLocationPermissions()
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                ACCESS_FINE_LOCATION,
+                ACCESS_COARSE_LOCATION,
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE,
+        )
+    }
+
+    private fun fetchAddress(location: Location) {
+        val geocoder = Geocoder(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val geocodeListener = initGeocodeListener(location)
+            geocoder.getFromLocation(location.latitude, location.longitude, 1, geocodeListener)
+        } else {
+            address = geocoder.getFromLocation(location.latitude, location.longitude, 1)?.get(0)?.getAddressLine(0).toString()
+            viewModel.setLocationInformation(address, location)
+        }
+    }
+
+    private fun initGeocodeListener(location: Location) =
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        object : Geocoder.GeocodeListener {
+            override fun onGeocode(addresses: MutableList<Address>) {
+                address = addresses[0].getAddressLine(0)
+                viewModel.setLocationInformation(address, location)
+            }
+
+            override fun onError(errorMessage: String?) {
+                showToast(getString(R.string.moment_creation_not_found_address))
+            }
+        }
+
     companion object {
         const val MEMORY_TITLE_KEY = "memoryTitle"
 
@@ -180,5 +291,7 @@ class MomentCreationActivity :
                 activityLauncher.launch(this)
             }
         }
+
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
     }
 }
