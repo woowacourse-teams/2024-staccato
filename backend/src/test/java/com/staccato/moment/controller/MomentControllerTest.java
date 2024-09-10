bd5b1578d6d0f89291b2b1b5664606c95f38636c
devhoya97 <diajbh123@gmail.com>
2024-09-10 16:21:35 +0900

    feat: 스타카토 수정 시 장소, 날짜, 추억도 변경할 수 있도록 컨트롤러 메서드 구현

--- backend/src/test/java/com/staccato/moment/controller/MomentControllerTest.java
+++ backend/src/test/java/com/staccato/moment/controller/MomentControllerTest.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
@@ -48,6 +49,18 @@ import com.staccato.moment.service.dto.response.MomentLocationResponses;
 
 @WebMvcTest(controllers = MomentController.class)
 class MomentControllerTest {
+    private static final String VALID_UPDATE_REQUEST_JSON = "{"
+            + "\"placeName\": \"placeName\","
+            + "\"address\": \"address\","
+            + "\"latitude\": 1.0,"
+            + "\"longitude\": 1.0,"
+            + "\"visitedAt\": \"2023-07-01T10:00:00\","
+            + "\"memoryId\": 1,"
+            + "\"momentImageUrls\": ["
+            + "  \"https://example.com/images/namsan_tower.jpg\""
+            + "]"
+            + "}";
+
     @Autowired
     private MockMvc mockMvc;
     @Autowired
@@ -309,6 +322,80 @@ class MomentControllerTest {
                         .content(updateRequestJson))
                 .andExpect(status().isBadRequest())
                 .andExpect(content().json(objectMapper.writeValueAsString(exceptionResponse)));
+    }
+
+    @Nested
+    @DisplayName("updateMomentByIdV2 테스트")
+    class UpdateMomentTests {
+        @DisplayName("적합한 경로변수를 통해 스타카토 수정에 성공한다.")
+        @Test
+        void updateMomentById() throws Exception {
+            // given
+            long momentId = 1L;
+            when(authService.extractFromToken(anyString())).thenReturn(MemberFixture.create());
+
+            // when & then
+            mockMvc.perform(put("/moments/v2/{momentId}", momentId)
+                            .header(HttpHeaders.AUTHORIZATION, "token")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(VALID_UPDATE_REQUEST_JSON))
+                    .andExpect(status().isOk());
+        }
+
+        @DisplayName("추가하려는 사진이 5장이 넘는다면 스타카토 수정에 실패한다.")
+        @Test
+        void failUpdateMomentByImagesSize() throws Exception {
+            // given
+            long momentId = 1L;
+            ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), "사진은 5장까지만 추가할 수 있어요.");
+            String updateRequestWithTooManyImagesJson = "{"
+                    + "\"placeName\": \"placeName\","
+                    + "\"address\": \"address\","
+                    + "\"latitude\": 1.0,"
+                    + "\"longitude\": 1.0,"
+                    + "\"visitedAt\": \"2023-07-01T10:00:00\","
+                    + "\"memoryId\": 1,"
+                    + "\"momentImageUrls\": ["
+                    + "  \"https://example.com/images/namsan_tower1.jpg\","
+                    + "  \"https://example.com/images/namsan_tower2.jpg\","
+                    + "  \"https://example.com/images/namsan_tower3.jpg\","
+                    + "  \"https://example.com/images/namsan_tower4.jpg\","
+                    + "  \"https://example.com/images/namsan_tower5.jpg\","
+                    + "  \"https://example.com/images/namsan_tower6.jpg\""
+                    + "]"
+                    + "}";
+            when(authService.extractFromToken(anyString())).thenReturn(MemberFixture.create());
+
+            // when & then
+            mockMvc.perform(put("/moments/v2/{momentId}", momentId)
+                            .header(HttpHeaders.AUTHORIZATION, "token")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(updateRequestWithTooManyImagesJson))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().json(objectMapper.writeValueAsString(exceptionResponse)));
+        }
+
+        @DisplayName("적합하지 않은 경로변수의 경우 스타카토 수정에 실패한다.")
+        @Test
+        void failUpdateMomentById() throws Exception {
+            // given
+            long momentId = 0L;
+            ExceptionResponse exceptionResponse = new ExceptionResponse(HttpStatus.BAD_REQUEST.toString(), "스타카토 식별자는 양수로 이루어져야 합니다.");
+            when(authService.extractFromToken(anyString())).thenReturn(MemberFixture.create());
+
+            // when & then
+            mockMvc.perform(put("/moments/v2/{momentId}", momentId)
+                            .header(HttpHeaders.AUTHORIZATION, "token")
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(VALID_UPDATE_REQUEST_JSON))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(content().json(objectMapper.writeValueAsString(exceptionResponse)));
+        }
+
+        /*
+        TODO: 컨트롤러 테스트 코드 리팩토링 PR이 머지되면
+         해당 코드 참고하여 MomentRequest의 필수값 필드가 빠져있는 경우에 대한 테스트 추가
+         */
     }
 
     @DisplayName("스타카토를 삭제한다.")
