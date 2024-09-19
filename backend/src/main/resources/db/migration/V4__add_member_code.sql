a38f76004e02b623f678adf235228548d7c8b309
BurningFalls <sherlocky31415@gmail.com>
2024-09-20 01:50:07 +0900

    fix: 스크립트 오류 수정

--- backend/src/main/resources/db/migration/V4__add_member_code.sql
+++ backend/src/main/resources/db/migration/V4__add_member_code.sql
@@ -1,3 +1,8 @@
-ALTER TABLE member ADD COLUMN code VARCHAR(36) NOT NULL;
-UPDATE member SET code = UUID() WHERE code WHERE code is '';
-ALTER TABLE member ADD UNIQUE (code);
+ALTER TABLE member
+    ADD COLUMN code VARCHAR(36);
+
+UPDATE member
+    SET code = (SELECT UUID());
+
+ALTER TABLE member
+    MODIFY COLUMN code VARCHAR(36) NOT NULL UNIQUE;
