f7d691c5646292e5cb549028df9695e805ec7413
linirini <2001yerin@naver.com>
2024-09-19 22:54:32 +0900

    fix: 스크립트 오류 수정

--- backend/src/main/resources/db/migration/V4__add_member_code.sql
+++ backend/src/main/resources/db/migration/V4__add_member_code.sql
@@ -1,3 +1,3 @@
 ALTER TABLE member ADD COLUMN code VARCHAR(36) NOT NULL;
-UPDATE member SET code = UUID() WHERE code IS NULL;
+UPDATE member SET code = UUID() WHERE code WHERE code is '';
 ALTER TABLE member ADD UNIQUE (code);
