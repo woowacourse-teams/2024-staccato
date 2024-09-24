92071f9f958d00c45102179a09a7cb72e4b7396d
linirini <2001yerin@naver.com>
2024-09-24 21:34:28 +0900

    feat: DB 마이그레이션 #386 (#389)
    * test: 컨트롤러 테스트의 책임이 아닌 테스트 제거
    
    - 사진 5장 초과 테스트의 책임은 MomentImages이므로 제거
    
    * chore: 운영 서버 datasource 설정 변경
    
    * feat: dataSourceConfiguration 구현
    
    * fix: profile 설정
    
    * chore: 다중 운영 환경에 서버 마이그레이션 적용
    
    * fix: OSIV 비활성화
    
    * chore: 트리거 변경

--- backend/src/test/java/com/staccato/moment/controller/MomentControllerTest.java
+++ backend/src/test/java/com/staccato/moment/controller/MomentControllerTest.java
@@ -1,5 +1,17 @@
 package com.staccato.moment.controller;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
