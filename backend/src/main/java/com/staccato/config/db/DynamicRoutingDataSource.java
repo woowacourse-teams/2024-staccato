c70b21ddaf5b449e1c6ad80ad9389299de40feb4
linirini <2001yerin@naver.com>
2024-09-24 18:03:52 +0900

    feat: dataSourceConfiguration 구현

---
+++ backend/src/main/java/com/staccato/config/db/DynamicRoutingDataSource.java
@@ -0,0 +1,23 @@
+package com.staccato.config.db;
+
+import static org.springframework.transaction.support.TransactionSynchronizationManager.isCurrentTransactionReadOnly;
+
+import static com.staccato.config.db.ReaderDataSourceConfig.READER;
+import static com.staccato.config.db.WriterDataSourceConfig.WRITER;
+
+import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class DynamicRoutingDataSource extends AbstractRoutingDataSource {
+    @Override
+    protected Object determineCurrentLookupKey() {
+        if (isCurrentTransactionReadOnly()) {
+            log.info(">>>>>> current data source : {}", READER);
+            return READER;
+        }
+        log.info(">>>>>> current data source : {}", WRITER);
+        return WRITER;
+    }
+}
