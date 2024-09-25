b2468a34f055646728f0d0a8ecc81360e3c864cd
somin <salth0601@gmail.com>
2024-09-25 16:04:21 +0900

    feat: 스타카토 수정 화면 장소 선택 기능 구현

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/moment/MomentUpdateRequest.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/moment/MomentUpdateRequest.kt
@@ -5,6 +5,12 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class MomentUpdateRequest(
+    @SerialName("staccatoTitle") val staccatoTitle: String,
     @SerialName("placeName") val placeName: String,
+    @SerialName("address") val address: String,
+    @SerialName("latitude") val latitude: Double,
+    @SerialName("longitude") val longitude: Double,
+    @SerialName("visitedAt") val visitedAt: String,
+    @SerialName("memoryId") val memoryId: Long,
     @SerialName("momentImageUrls") val momentImageUrls: List<String>,
 )
--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/visitupdate/VisitUpdateActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/visitupdate/VisitUpdateActivity.kt
@@ -1,8 +1,14 @@
 package com.on.staccato.presentation.visitupdate
 
+import android.Manifest
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.location.Location
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.view.MotionEvent
 import android.view.View
@@ -10,12 +16,22 @@ import android.view.WindowManager.LayoutParams.FLAG_NOT_TOUCHABLE
 import android.view.inputmethod.InputMethodManager
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.viewModels
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationServices
+import com.google.android.libraries.places.api.model.Place
 import com.on.staccato.R
 import com.on.staccato.databinding.ActivityVisitUpdateBinding
 import com.on.staccato.presentation.base.BindingActivity
+import com.on.staccato.presentation.common.CustomAutocompleteSupportFragment
+import com.on.staccato.presentation.common.GooglePlaceFragmentEventHandler
 import com.on.staccato.presentation.common.PhotoAttachFragment
 import com.on.staccato.presentation.memory.MemoryFragment.Companion.MEMORY_ID_KEY
 import com.on.staccato.presentation.memory.MemoryFragment.Companion.MEMORY_TITLE_KEY
@@ -31,33 +47,31 @@ import com.on.staccato.presentation.visitupdate.viewmodel.VisitUpdateViewModel
 import com.on.staccato.presentation.visitupdate.viewmodel.VisitUpdateViewModelFactory
 
 class VisitUpdateActivity :
-    BindingActivity<ActivityVisitUpdateBinding>(),
+    GooglePlaceFragmentEventHandler,
+    PlaceSearchHandler,
     OnUrisSelectedListener,
     VisitUpdateHandler,
-    PlaceSearchHandler {
+    BindingActivity<ActivityVisitUpdateBinding>() {
     override val layoutResourceId = R.layout.activity_visit_update
     private val viewModel: VisitUpdateViewModel by viewModels { VisitUpdateViewModelFactory() }
     private val photoAttachFragment by lazy {
         PhotoAttachFragment().apply { setMultipleAbleOption(true) }
     }
     private val fragmentManager: FragmentManager = supportFragmentManager
-    private lateinit var adapter: PhotoAttachAdapter
+    private lateinit var photoAttachAdapter: PhotoAttachAdapter
     private lateinit var itemTouchHelper: ItemTouchHelper
-    private val visitId by lazy { intent.getLongExtra(MOMENT_ID_KEY, 0L) }
+    private val staccatoId by lazy { intent.getLongExtra(MOMENT_ID_KEY, 0L) }
     private val memoryId by lazy { intent.getLongExtra(MEMORY_ID_KEY, 0L) }
     private val memoryTitle by lazy { intent.getStringExtra(MEMORY_TITLE_KEY) ?: "" }
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var address: String
+
     private val inputManager: InputMethodManager by lazy {
         getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
     }
 
-    override fun onUrisSelected(vararg uris: Uri) {
-        viewModel.updateSelectedImageUris(arrayOf(*uris))
-    }
-
-    override fun onUpdateDoneClicked() {
-        window.setFlags(FLAG_NOT_TOUCHABLE, FLAG_NOT_TOUCHABLE)
-        showToast(getString(R.string.visit_update_posting))
-        viewModel.updateVisit()
+    private val autocompleteFragment by lazy {
+        supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as CustomAutocompleteSupportFragment
     }
 
     override fun dispatchTouchEvent(event: MotionEvent): Boolean {
@@ -69,6 +83,50 @@ class VisitUpdateActivity :
             }
         }
         return super.dispatchTouchEvent(event)
+    }
+
+    override fun onNewPlaceSelected(
+        placeId: String,
+        name: String,
+        address: String,
+        longitude: Double,
+        latitude: Double,
+    ) {
+        viewModel.selectNewPlace(
+            placeId,
+            name,
+            address,
+            longitude,
+            latitude,
+        )
+    }
+
+    override fun onSelectedPlaceCleared() {
+        viewModel.clearPlace()
+    }
+
+    override fun onSearchClicked() {
+        fetchCurrentLocationAddress()
+    }
+
+    override fun onUrisSelected(vararg uris: Uri) {
+        viewModel.updateSelectedImageUris(arrayOf(*uris))
+    }
+
+    override fun onUpdateDoneClicked() {
+        window.setFlags(FLAG_NOT_TOUCHABLE, FLAG_NOT_TOUCHABLE)
+        viewModel.updateVisit(staccatoId)
+    }
+
+    override fun initStartView(savedInstanceState: Bundle?) {
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        initBinding()
+        initToolbar()
+        initAdapter()
+        initItemTouchHelper()
+        observeViewModelData()
+        initGooglePlaceSearch()
+        viewModel.initViewModelData(staccatoId, memoryId, memoryTitle)
     }
 
     private fun isTouchInsideView(
@@ -92,30 +150,43 @@ class VisitUpdateActivity :
         )
     }
 
-    override fun initStartView(savedInstanceState: Bundle?) {
-        initBinding()
-        initToolbar()
-        initAdapter()
-        initItemTouchHelper()
-        observeViewModelData()
-        viewModel.initViewModelData(visitId, memoryId, memoryTitle)
+    private fun fetchCurrentLocationAddress() {
+        val isAccessFineLocationGranted =
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ) == PackageManager.PERMISSION_GRANTED
+
+        val isAccessCoarseLocationGranted =
+            ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ) == PackageManager.PERMISSION_GRANTED
+
+        if (isAccessFineLocationGranted || isAccessCoarseLocationGranted) {
+            val currentLocation =
+                fusedLocationProviderClient.getCurrentLocation(
+                    LocationRequest.PRIORITY_HIGH_ACCURACY,
+                    null,
+                )
+            currentLocation.addOnSuccessListener { location ->
+                fetchAddress(location)
+            }
+            return
+        } else {
+            requestLocationPermissions()
+        }
     }
 
     private fun initBinding() {
         binding.lifecycleOwner = this
         binding.viewModel = viewModel
         binding.visitUpdateHandler = this
-        binding.placeSearchHandler = this
-    }
-
-    private fun initToolbar() {
-        binding.toolbarVisitUpdate.setNavigationOnClickListener {
-            finish()
-        }
+        binding.currentLocationHandler = this
     }
 
     private fun initAdapter() {
-        adapter =
+        photoAttachAdapter =
             PhotoAttachAdapter(
                 object : ItemDragListener {
                     override fun onStartDrag(viewHolder: RecyclerView.ViewHolder) {
@@ -128,15 +199,24 @@ class VisitUpdateActivity :
                 },
                 viewModel,
             )
-        binding.rvPhotoAttach.adapter = adapter
+        binding.rvPhotoAttach.adapter = photoAttachAdapter
     }
 
     private fun initItemTouchHelper() {
-        itemTouchHelper = ItemTouchHelper(AttachedPhotoItemTouchHelperCallback(adapter))
+        itemTouchHelper = ItemTouchHelper(AttachedPhotoItemTouchHelperCallback(photoAttachAdapter))
         itemTouchHelper.attachToRecyclerView(binding.rvPhotoAttach)
     }
 
+    private fun initToolbar() {
+        binding.toolbarVisitUpdate.setNavigationOnClickListener {
+            finish()
+        }
+    }
+
     private fun observeViewModelData() {
+        viewModel.placeName.observe(this) {
+            autocompleteFragment.setText(it)
+        }
         viewModel.isAddPhotoClicked.observe(this) {
             if (!photoAttachFragment.isAdded && it) {
                 photoAttachFragment.show(fragmentManager, PhotoAttachFragment.TAG)
@@ -149,7 +229,7 @@ class VisitUpdateActivity :
             viewModel.fetchPhotosUrlsByUris(this)
         }
         viewModel.currentPhotos.observe(this) { photos ->
-            adapter.submitList(
+            photoAttachAdapter.submitList(
                 listOf(AttachedPhotoUiModel.addPhotoButton).plus(photos.attachedPhotos),
             )
         }
@@ -158,16 +238,11 @@ class VisitUpdateActivity :
         }
     }
 
-    private fun handleError(errorMessage: String) {
-        window.clearFlags(FLAG_NOT_TOUCHABLE)
-        showToast(errorMessage)
-    }
-
     private fun handleUpdateComplete(isUpdateCompleted: Boolean) {
         if (isUpdateCompleted) {
             val intent =
                 Intent()
-                    .putExtra(MOMENT_ID_KEY, visitId)
+                    .putExtra(MOMENT_ID_KEY, staccatoId)
                     .putExtra(MEMORY_ID_KEY, memoryId)
                     .putExtra(MEMORY_TITLE_KEY, memoryTitle)
             setResult(RESULT_OK, intent)
@@ -176,7 +251,62 @@ class VisitUpdateActivity :
         }
     }
 
+    private fun handleError(errorMessage: String) {
+        window.clearFlags(FLAG_NOT_TOUCHABLE)
+        showToast(errorMessage)
+    }
+
+    private fun fetchAddress(location: Location) {
+        val geocoder = Geocoder(this)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            val geocodeListener = initGeocodeListener(location)
+            geocoder.getFromLocation(location.latitude, location.longitude, 1, geocodeListener)
+        } else {
+            address =
+                geocoder.getFromLocation(location.latitude, location.longitude, 1)?.get(0)
+                    ?.getAddressLine(0).toString()
+            viewModel.setPlaceByCurrentAddress(address, location)
+        }
+    }
+
+    private fun initGeocodeListener(location: Location) =
+        @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+        object : Geocoder.GeocodeListener {
+            override fun onGeocode(addresses: MutableList<Address>) {
+                address = addresses[0].getAddressLine(0)
+                viewModel.setPlaceByCurrentAddress(address, location)
+            }
+
+            override fun onError(errorMessage: String?) {
+                showToast(getString(R.string.moment_creation_not_found_address))
+            }
+        }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+            ),
+            LOCATION_PERMISSION_REQUEST_CODE,
+        )
+    }
+
+    private fun initGooglePlaceSearch() {
+        val placeFields: List<Place.Field> =
+            listOf(Place.Field.NAME, Place.Field.ID, Place.Field.ADDRESS, Place.Field.LAT_LNG)
+        autocompleteFragment.setPlaceFields(placeFields)
+
+        lifecycleScope.launchWhenCreated {
+            autocompleteFragment.setHint(getString(R.string.visit_creation_place_search))
+        }
+    }
+
     companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1
+
         fun startWithResultLauncher(
             visitId: Long,
             memoryId: Long,
@@ -191,9 +321,5 @@ class VisitUpdateActivity :
                 activityLauncher.launch(this)
             }
         }
-    }
-
-    override fun onSearchClicked() {
-        showToast("플레이스 검색창 띄우기")
     }
 }
