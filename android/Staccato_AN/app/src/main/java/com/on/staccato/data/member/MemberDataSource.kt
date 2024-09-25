547766be4a2eaec952cb21496e8e2f7cffb1f7b6
Junyoung-WON <wjyo60@gmail.com>
2024-09-25 16:49:49 +0900

    refactor: DataSource 레이어 제거
    - 하나에 요청 메서드를 가진 것에 반해 과도한 계층 분리가 이루어진 것 같아 DataSource를 제거함

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/member/MemberDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.on.staccato.data.member
-
-import com.on.staccato.data.ResponseResult
-import com.on.staccato.data.dto.member.RecoveryCodeResponse
-
-interface MemberDataSource {
-    suspend fun postRecoveryCode(recoveryCode: String): ResponseResult<RecoveryCodeResponse>
-}
--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/member/MemberDefaultRepository.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/member/MemberDefaultRepository.kt
@@ -1,16 +1,19 @@
 package com.on.staccato.data.member
 
+import com.on.staccato.data.ApiResponseHandler.handleApiResponse
 import com.on.staccato.data.ResponseResult
 import com.on.staccato.data.ResponseResult.Exception
 import com.on.staccato.data.ResponseResult.ServerError
 import com.on.staccato.data.ResponseResult.Success
+import com.on.staccato.data.StaccatoClient
 import com.on.staccato.domain.repository.MemberRepository
 
 class MemberDefaultRepository(
-    private val memberDataSource: MemberDataSource = MemberRemoteDataSource(),
+    private val memberApiService: MemberApiService = StaccatoClient.memberApiService,
 ) : MemberRepository {
     override suspend fun fetchTokenWithRecoveryCode(recoveryCode: String): ResponseResult<String> {
-        return when (val responseResult = memberDataSource.postRecoveryCode(recoveryCode)) {
+        val responseResult = handleApiResponse { memberApiService.postRecoveryCode(recoveryCode) }
+        return when (responseResult) {
             is Exception -> Exception(responseResult.e, EXCEPTION_ERROR_MESSAGE)
             is ServerError -> ServerError(responseResult.status, responseResult.message)
             is Success -> Success(responseResult.data.token)
