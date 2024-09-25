b7a6a671322df85fa2802b216cb1dd75aa159f41
linirini <101927543+linirini@users.noreply.github.com>
2024-09-25 16:49:44 +0900

    feat: 이전 기록 불러오기 API 구현 #374 (#379)
    * feat: 중복 닉네임 입력 불가능 검증 복구
    
    * feat: 이전 기록 불러오기 컨트롤러 구현
    
    * feat: 사용자 등록 시 고유 코드 발급하도록 구현
    
    * feat: 고유 코드로 사용자 조회해서 토큰 발급하는 서비스 구현
    
    * test: 존재하지 않는 고유 코드로 사용자 조회 실패 테스트 추가
    
    * docs: 이전 기록 불러오기 API 문서화
    
    * build: flyway 의존성 및 설정 추가
    
    * chore: sql init script 추가
    
    * chore: 로컬 환경에서는 flyway 비활성화
    
    * chore: dev cicd trigger 수정
    
    * chore: visitedAt 타입 변경 스크립트 추가
    
    * chore: startAt, endAt nullable 스크립트 추가
    
    * chore: dev 서버 ddl 옵션 변경
    
    * chore: stage 서버 ddl 옵션 변경
    
    * chore: dev서버 콘솔 로깅 추가
    
    * chore: defer-datasource-initialization 설정 변경
    
    * chore: stage 서버 설정 파일에 flyway 활성화
    
    * chore: member code 필드 스크립트 추가
    
    * chore: 운영 서버에 flyway 임시 비활성화
    
    * fix: 스크립트 오류 수정
    
    * fix: 스크립트 오류 수정
    
    * fix: 스크립트 오류 수정
    
    * fix: 스크립트 오류 수정
    
    * fix: 스크립트 오류 수정
    
    * style: eol추가
    
    * refactor: api http method 변경
    
    ---------
    
    Co-authored-by: BurningFalls <sherlocky31415@gmail.com>

--- backend/src/test/java/com/staccato/auth/service/AuthServiceTest.java
+++ backend/src/test/java/com/staccato/auth/service/AuthServiceTest.java
@@ -4,7 +4,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
-import org.junit.jupiter.api.Disabled;
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.staccato.ServiceSliceTest;
 import com.staccato.auth.service.dto.request.LoginRequest;
 import com.staccato.auth.service.dto.response.LoginResponse;
+import com.staccato.config.auth.TokenProvider;
 import com.staccato.exception.StaccatoException;
 import com.staccato.exception.UnauthorizedException;
 import com.staccato.fixture.Member.MemberFixture;
 import com.staccato.fixture.auth.LoginRequestFixture;
+import com.staccato.member.domain.Member;
 import com.staccato.member.repository.MemberRepository;
 
 class AuthServiceTest extends ServiceSliceTest {
@@ -23,6 +26,8 @@ class AuthServiceTest extends ServiceSliceTest {
     private AuthService authService;
     @Autowired
     private MemberRepository memberRepository;
+    @Autowired
+    private TokenProvider tokenProvider;
 
     @DisplayName("입력받은 닉네임으로 멤버를 저장하고, 토큰을 생성한다.")
     @Test
@@ -40,21 +45,6 @@ class AuthServiceTest extends ServiceSliceTest {
         );
     }
 
-    @DisplayName("입력받은 닉네임이 이미 존재하는 닉네임인 경우 기존에 존재하던 사용자를 반환한다.")
-    @Test
-    void loginByDuplicated() {
-        // given
-        memberRepository.save(MemberFixture.create());
-        LoginRequest loginRequest = LoginRequestFixture.create();
-
-        // when
-        LoginResponse response = authService.login(loginRequest);
-
-        // then
-        assertThat(response.token()).isNotBlank();
-    }
-
-    @Disabled
     @DisplayName("입력받은 닉네임이 이미 존재하는 닉네임인 경우 예외가 발생한다.")
     @Test
     void cannotLoginByDuplicated() {
@@ -78,5 +68,35 @@ class AuthServiceTest extends ServiceSliceTest {
         assertThatThrownBy(() -> authService.extractFromToken(null))
                 .isInstanceOf(UnauthorizedException.class)
                 .hasMessage("인증되지 않은 사용자입니다.");
+    }
+
+    @DisplayName("고유 코드로 사용자를 조회해서 토큰을 발급한다.")
+    @Test
+    void createTokenByCode() {
+        // given
+        String code = UUID.randomUUID().toString();
+        Member member = Member.builder()
+                .nickname("staccato")
+                .code(code)
+                .build();
+        memberRepository.save(member);
+
+        // when
+        LoginResponse loginResponse = authService.loginByCode(code);
+
+        // then
+        assertThat(tokenProvider.extractMemberId(loginResponse.token())).isEqualTo(member.getId());
+    }
+
+    @DisplayName("존재하지 않는 고유 코드로 사용자를 조회하면 예외가 발생한다.")
+    @Test
+    void cannotCreateTokenByCode() {
+        // given
+        String code = UUID.randomUUID().toString();
+
+        // when
+        assertThatThrownBy(() -> authService.loginByCode(code))
+                .isInstanceOf(StaccatoException.class)
+                .hasMessage("유효하지 않은 코드입니다. 올바른 코드인지 확인해주세요.");
     }
 }
