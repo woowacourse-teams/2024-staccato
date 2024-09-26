de8577542aa7ccf3e5e9a25b3b2e894367c84b4b
yoonjuho <dbswn990@naver.com>
2024-09-26 22:40:51 +0900

    feat: Bulk Delete 메서드 구현

--- backend/src/main/java/com/staccato/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/staccato/comment/repository/CommentRepository.java
@@ -1,11 +1,16 @@
 package com.staccato.comment.repository;
 
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import com.staccato.comment.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
     List<Comment> findAllByMomentId(long momentId);
+
+    @Modifying
+    @Query("DELETE FROM Comment c WHERE c.moment.id = :momentId")
+    void deleteAllByMomentIdInBatch(@Param("momentId") Long momentId);
 }
