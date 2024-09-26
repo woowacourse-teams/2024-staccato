6116e93f64d55c17e22a86e6d2bcdac6c71bd5a8
devhoya97 <146502065+devhoya97@users.noreply.github.com>
2024-09-26 20:59:05 +0900

    refactor: 추억 조회 시 created_at 기준 정렬을 DB단이 아닌 애플리케이션 단에서 수행 #404 (#409)
    * refactor: 회원의 모든 추억 조회시 정렬을 앱단에서 수행
    
    * refactor: 특정 날짜를 포함한 특정 회원의 모든 추억 조회시 정렬을 앱단에서 수행
    
    * refactor: fk에 대한 인덱스는 자동적용되므로 코드상에서 제거
    
    * refactor: 특정 스타카토의 모든 댓글 조회시 생성 순 정렬을 앱단에서 수행

--- backend/src/main/java/com/staccato/comment/domain/Comment.java
+++ backend/src/main/java/com/staccato/comment/domain/Comment.java
@@ -8,10 +8,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Index;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotNull;
 
 import com.staccato.config.domain.BaseEntity;
@@ -26,9 +24,6 @@ import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
 @Entity
-@Table(indexes = {
-        @Index(name = "idx_moment_id", columnList = "moment_id")
-})
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
