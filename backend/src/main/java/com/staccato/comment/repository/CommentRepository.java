5cc5cc2682ef88f3e08376a4529488ce5fc4dc6d
devhoya97 <diajbh123@gmail.com>
2024-09-26 20:33:10 +0900

    refactor: 특정 스타카토의 모든 댓글 조회시 생성 순 정렬을 앱단에서 수행

--- backend/src/main/java/com/staccato/comment/repository/CommentRepository.java
+++ backend/src/main/java/com/staccato/comment/repository/CommentRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.staccato.comment.domain.Comment;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
-    List<Comment> findAllByMomentIdOrderByCreatedAtAsc(long momentId);
+    List<Comment> findAllByMomentId(long momentId);
 }
