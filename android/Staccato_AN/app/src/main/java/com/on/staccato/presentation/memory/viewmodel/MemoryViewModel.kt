3946501696412ad60cfdfb3daba95f11b0efbe4d
Junyoung-WON <wjyo60@gmail.com>
2024-09-27 08:08:15 +0900

    style: ktlint 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/memory/viewmodel/MemoryViewModel.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/memory/viewmodel/MemoryViewModel.kt
@@ -21,61 +21,61 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MemoryViewModel
-@Inject
-constructor(
-    private val memoryRepository: MemoryRepository,
-) : ViewModel() {
-    private val _memory = MutableLiveData<MemoryUiModel>()
-    val memory: LiveData<MemoryUiModel> get() = _memory
+    @Inject
+    constructor(
+        private val memoryRepository: MemoryRepository,
+    ) : ViewModel() {
+        private val _memory = MutableLiveData<MemoryUiModel>()
+        val memory: LiveData<MemoryUiModel> get() = _memory
 
-    private val _errorMessage = MutableSingleLiveData<String>()
-    val errorMessage: SingleLiveData<String> get() = _errorMessage
+        private val _errorMessage = MutableSingleLiveData<String>()
+        val errorMessage: SingleLiveData<String> get() = _errorMessage
 
-    private val _isDeleteSuccess = MutableSingleLiveData<Boolean>(false)
-    val isDeleteSuccess: SingleLiveData<Boolean> get() = _isDeleteSuccess
+        private val _isDeleteSuccess = MutableSingleLiveData<Boolean>(false)
+        val isDeleteSuccess: SingleLiveData<Boolean> get() = _isDeleteSuccess
 
-    fun loadMemory(memoryId: Long) {
-        viewModelScope.launch {
-            val result: ResponseResult<Memory> = memoryRepository.getMemory(memoryId)
-            result
-                .onSuccess(::setMemory)
-                .onServerError(::handleServerError)
-                .onException(::handelException)
+        fun loadMemory(memoryId: Long) {
+            viewModelScope.launch {
+                val result: ResponseResult<Memory> = memoryRepository.getMemory(memoryId)
+                result
+                    .onSuccess(::setMemory)
+                    .onServerError(::handleServerError)
+                    .onException(::handelException)
+            }
+        }
+
+        fun deleteMemory(memoryId: Long) {
+            viewModelScope.launch {
+                val result: ResponseResult<Unit> = memoryRepository.deleteMemory(memoryId)
+                result.onSuccess { updateIsDeleteSuccess() }
+                    .onServerError(::handleServerError)
+                    .onException(::handelException)
+            }
+        }
+
+        private fun setMemory(memory: Memory) {
+            _memory.value = memory.toUiModel()
+        }
+
+        private fun updateIsDeleteSuccess() {
+            _isDeleteSuccess.setValue(true)
+        }
+
+        private fun handleServerError(
+            status: Status,
+            message: String,
+        ) {
+            _errorMessage.setValue(message)
+        }
+
+        private fun handelException(
+            e: Throwable,
+            message: String,
+        ) {
+            _errorMessage.setValue(MEMORY_ERROR_MESSAGE)
+        }
+
+        companion object {
+            private const val MEMORY_ERROR_MESSAGE = "추억을 조회할 수 없습니다"
         }
     }
-
-    fun deleteMemory(memoryId: Long) {
-        viewModelScope.launch {
-            val result: ResponseResult<Unit> = memoryRepository.deleteMemory(memoryId)
-            result.onSuccess { updateIsDeleteSuccess() }
-                .onServerError(::handleServerError)
-                .onException(::handelException)
-        }
-    }
-
-    private fun setMemory(memory: Memory) {
-        _memory.value = memory.toUiModel()
-    }
-
-    private fun updateIsDeleteSuccess() {
-        _isDeleteSuccess.setValue(true)
-    }
-
-    private fun handleServerError(
-        status: Status,
-        message: String,
-    ) {
-        _errorMessage.setValue(message)
-    }
-
-    private fun handelException(
-        e: Throwable,
-        message: String,
-    ) {
-        _errorMessage.setValue(MEMORY_ERROR_MESSAGE)
-    }
-
-    companion object {
-        private const val MEMORY_ERROR_MESSAGE = "추억을 조회할 수 없습니다"
-    }
-}
--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/visitupdate/viewmodel/VisitUpdateViewModel.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/visitupdate/viewmodel/VisitUpdateViewModel.kt
@@ -34,258 +34,258 @@ import javax.inject.Inject
 
 @HiltViewModel
 class VisitUpdateViewModel
-@Inject
-constructor(
-    private val timelineRepository: TimelineRepository,
-    private val momentRepository: MomentRepository,
-    private val imageRepository: ImageRepository,
-) : AttachedPhotoHandler, ViewModel() {
-    val staccatoTitle = ObservableField<String>()
+    @Inject
+    constructor(
+        private val timelineRepository: TimelineRepository,
+        private val momentRepository: MomentRepository,
+        private val imageRepository: ImageRepository,
+    ) : AttachedPhotoHandler, ViewModel() {
+        val staccatoTitle = ObservableField<String>()
 
-    private val _placeName = MutableLiveData<String?>(null)
-    val placeName: LiveData<String?> get() = _placeName
+        private val _placeName = MutableLiveData<String?>(null)
+        val placeName: LiveData<String?> get() = _placeName
 
-    private val _address = MutableLiveData<String?>(null)
-    val address: LiveData<String?> get() = _address
+        private val _address = MutableLiveData<String?>(null)
+        val address: LiveData<String?> get() = _address
 
-    private val _currentPhotos =
-        MutableLiveData(AttachedPhotosUiModel(emptyList()))
-    val currentPhotos: LiveData<AttachedPhotosUiModel> get() = _currentPhotos
+        private val _currentPhotos =
+            MutableLiveData(AttachedPhotosUiModel(emptyList()))
+        val currentPhotos: LiveData<AttachedPhotosUiModel> get() = _currentPhotos
 
-    private val _pendingPhotos = MutableSingleLiveData<List<AttachedPhotoUiModel>>()
-    val pendingPhotos: SingleLiveData<List<AttachedPhotoUiModel>> get() = _pendingPhotos
+        private val _pendingPhotos = MutableSingleLiveData<List<AttachedPhotoUiModel>>()
+        val pendingPhotos: SingleLiveData<List<AttachedPhotoUiModel>> get() = _pendingPhotos
 
-    private val _latitude = MutableLiveData<Double?>()
-    private val latitude: LiveData<Double?> get() = _latitude
+        private val _latitude = MutableLiveData<Double?>()
+        private val latitude: LiveData<Double?> get() = _latitude
 
-    private val _longitude = MutableLiveData<Double?>()
-    private val longitude: LiveData<Double?> get() = _longitude
+        private val _longitude = MutableLiveData<Double?>()
+        private val longitude: LiveData<Double?> get() = _longitude
 
-    private val _memoryCandidates = MutableLiveData<MemoryCandidates>()
-    val memoryCandidates: LiveData<MemoryCandidates> get() = _memoryCandidates
+        private val _memoryCandidates = MutableLiveData<MemoryCandidates>()
+        val memoryCandidates: LiveData<MemoryCandidates> get() = _memoryCandidates
 
-    private val _selectedVisitedAt = MutableLiveData<LocalDateTime?>()
-    val selectedVisitedAt: LiveData<LocalDateTime?> get() = _selectedVisitedAt
+        private val _selectedVisitedAt = MutableLiveData<LocalDateTime?>()
+        val selectedVisitedAt: LiveData<LocalDateTime?> get() = _selectedVisitedAt
 
-    private var targetMemoryId: Long = 0
+        private var targetMemoryId: Long = 0
 
-    val selectedMemory =
-        MediatorLiveData<MemoryCandidate>().apply {
-            var latestMemoryCandidates: MemoryCandidates? = null
-            var latestVisitedAt: LocalDateTime? = null
+        val selectedMemory =
+            MediatorLiveData<MemoryCandidate>().apply {
+                var latestMemoryCandidates: MemoryCandidates? = null
+                var latestVisitedAt: LocalDateTime? = null
 
-            addSource(memoryCandidates) { memories ->
-                latestMemoryCandidates = memories
-                if (latestMemoryCandidates != null && latestVisitedAt != null) {
-                    value = findMemory(latestMemoryCandidates!!)
+                addSource(memoryCandidates) { memories ->
+                    latestMemoryCandidates = memories
+                    if (latestMemoryCandidates != null && latestVisitedAt != null) {
+                        value = findMemory(latestMemoryCandidates!!)
+                    }
+                }
+
+                addSource(selectedVisitedAt) { visitedAt ->
+                    latestVisitedAt = visitedAt
+                    if (latestMemoryCandidates != null && latestVisitedAt != null) {
+                        value = findMemory(latestMemoryCandidates!!)
+                    }
                 }
             }
 
-            addSource(selectedVisitedAt) { visitedAt ->
-                latestVisitedAt = visitedAt
-                if (latestMemoryCandidates != null && latestVisitedAt != null) {
-                    value = findMemory(latestMemoryCandidates!!)
+        private fun findMemory(memories: MemoryCandidates) =
+            memories.memoryCandidate.first { memory ->
+                targetMemoryId == memory.memoryId
+            }
+
+        private val _isUpdateCompleted = MutableLiveData(false)
+        val isUpdateCompleted: LiveData<Boolean> get() = _isUpdateCompleted
+
+        private val _errorMessage = MutableSingleLiveData<String>()
+        val errorMessage: SingleLiveData<String> get() = _errorMessage
+
+        private val _isPosting = MutableLiveData<Boolean>(false)
+        val isPosting: LiveData<Boolean> get() = _isPosting
+
+        private val _isAddPhotoClicked = MutableSingleLiveData(false)
+        val isAddPhotoClicked: SingleLiveData<Boolean> get() = _isAddPhotoClicked
+
+        private val photoJobs = mutableMapOf<String, Job>()
+
+        override fun onAddClicked() {
+            if ((currentPhotos.value?.size ?: 0) == MomentCreationViewModel.MAX_PHOTO_NUMBER) {
+                _errorMessage.postValue(MomentCreationViewModel.MAX_PHOTO_NUMBER_MESSAGE)
+            } else {
+                _isAddPhotoClicked.postValue(true)
+            }
+        }
+
+        override fun onDeleteClicked(deletedPhoto: AttachedPhotoUiModel) {
+            _currentPhotos.value = currentPhotos.value?.removePhoto(deletedPhoto)
+            if (photoJobs[deletedPhoto.uri.toString()]?.isActive == true) {
+                photoJobs[deletedPhoto.uri.toString()]?.cancel()
+            }
+        }
+
+        fun selectMemoryVisitedAt(
+            memory: MemoryCandidate,
+            visitedAt: LocalDateTime,
+        ) {
+            selectedMemory.value = memory
+            _selectedVisitedAt.value = visitedAt
+        }
+
+        fun fetchTargetData(
+            staccatoId: Long,
+            memoryId: Long,
+            memoryTitle: String,
+        ) {
+            targetMemoryId = memoryId
+            fetchMemoryCandidates()
+            fetchStaccatoBy(staccatoId)
+        }
+
+        fun selectNewPlace(
+            placeId: String,
+            name: String,
+            address: String,
+            longitude: Double,
+            latitude: Double,
+        ) {
+            _placeName.value = name
+            _address.value = address
+            _longitude.value = longitude
+            _latitude.value = latitude
+        }
+
+        fun clearPlace() {
+            _placeName.value = null
+            _address.value = null
+            _longitude.value = null
+            _latitude.value = null
+        }
+
+        fun setPlaceByCurrentAddress(
+            address: String?,
+            location: Location,
+        ) {
+            _placeName.postValue(address)
+            _address.postValue(address)
+            _latitude.postValue(location.latitude)
+            _longitude.postValue(location.longitude)
+        }
+
+        fun updateSelectedImageUris(newUris: Array<Uri>) {
+            val updatedPhotos = currentPhotos.value!!.addPhotosByUris(newUris.toList())
+            _currentPhotos.value = updatedPhotos
+            _pendingPhotos.postValue(updatedPhotos.getPhotosWithoutUrls())
+        }
+
+        fun setUrisWithNewOrder(list: List<AttachedPhotoUiModel>) {
+            _currentPhotos.value = AttachedPhotosUiModel(list)
+        }
+
+        fun fetchPhotosUrlsByUris(context: Context) {
+            pendingPhotos.getValue()?.forEach { photo ->
+                val job = createPhotoUploadJob(context, photo)
+                job.invokeOnCompletion { _ ->
+                    photoJobs.remove(photo.uri.toString())
                 }
+                photoJobs[photo.uri.toString()] = job
             }
         }
 
-    private fun findMemory(memories: MemoryCandidates) =
-        memories.memoryCandidate.first { memory ->
-            targetMemoryId == memory.memoryId
-        }
+        fun updateVisit(staccatoId: Long) {
+            viewModelScope.launch {
+                _isPosting.value = true
+                val staccatoTitleValue = staccatoTitle.get() ?: return@launch handleFailure()
+                val placeNameValue = placeName.value ?: return@launch handleFailure()
+                val addressValue = address.value ?: return@launch handleFailure()
+                val latitudeValue = latitude.value ?: return@launch handleFailure()
+                val longitudeValue = longitude.value ?: return@launch handleFailure()
+                val visitedAtValue = selectedVisitedAt.value ?: return@launch handleFailure()
+                val memoryIdValue = selectedMemory.value?.memoryId ?: return@launch handleFailure()
+                val momentImageUrlsValue =
+                    currentPhotos.value?.attachedPhotos?.map { it.imageUrl!! }
+                        ?: return@launch handleFailure()
 
-    private val _isUpdateCompleted = MutableLiveData(false)
-    val isUpdateCompleted: LiveData<Boolean> get() = _isUpdateCompleted
-
-    private val _errorMessage = MutableSingleLiveData<String>()
-    val errorMessage: SingleLiveData<String> get() = _errorMessage
-
-    private val _isPosting = MutableLiveData<Boolean>(false)
-    val isPosting: LiveData<Boolean> get() = _isPosting
-
-    private val _isAddPhotoClicked = MutableSingleLiveData(false)
-    val isAddPhotoClicked: SingleLiveData<Boolean> get() = _isAddPhotoClicked
-
-    private val photoJobs = mutableMapOf<String, Job>()
-
-    override fun onAddClicked() {
-        if ((currentPhotos.value?.size ?: 0) == MomentCreationViewModel.MAX_PHOTO_NUMBER) {
-            _errorMessage.postValue(MomentCreationViewModel.MAX_PHOTO_NUMBER_MESSAGE)
-        } else {
-            _isAddPhotoClicked.postValue(true)
-        }
-    }
-
-    override fun onDeleteClicked(deletedPhoto: AttachedPhotoUiModel) {
-        _currentPhotos.value = currentPhotos.value?.removePhoto(deletedPhoto)
-        if (photoJobs[deletedPhoto.uri.toString()]?.isActive == true) {
-            photoJobs[deletedPhoto.uri.toString()]?.cancel()
-        }
-    }
-
-    fun selectMemoryVisitedAt(
-        memory: MemoryCandidate,
-        visitedAt: LocalDateTime,
-    ) {
-        selectedMemory.value = memory
-        _selectedVisitedAt.value = visitedAt
-    }
-
-    fun fetchTargetData(
-        staccatoId: Long,
-        memoryId: Long,
-        memoryTitle: String,
-    ) {
-        targetMemoryId = memoryId
-        fetchMemoryCandidates()
-        fetchStaccatoBy(staccatoId)
-    }
-
-    fun selectNewPlace(
-        placeId: String,
-        name: String,
-        address: String,
-        longitude: Double,
-        latitude: Double,
-    ) {
-        _placeName.value = name
-        _address.value = address
-        _longitude.value = longitude
-        _latitude.value = latitude
-    }
-
-    fun clearPlace() {
-        _placeName.value = null
-        _address.value = null
-        _longitude.value = null
-        _latitude.value = null
-    }
-
-    fun setPlaceByCurrentAddress(
-        address: String?,
-        location: Location,
-    ) {
-        _placeName.postValue(address)
-        _address.postValue(address)
-        _latitude.postValue(location.latitude)
-        _longitude.postValue(location.longitude)
-    }
-
-    fun updateSelectedImageUris(newUris: Array<Uri>) {
-        val updatedPhotos = currentPhotos.value!!.addPhotosByUris(newUris.toList())
-        _currentPhotos.value = updatedPhotos
-        _pendingPhotos.postValue(updatedPhotos.getPhotosWithoutUrls())
-    }
-
-    fun setUrisWithNewOrder(list: List<AttachedPhotoUiModel>) {
-        _currentPhotos.value = AttachedPhotosUiModel(list)
-    }
-
-    fun fetchPhotosUrlsByUris(context: Context) {
-        pendingPhotos.getValue()?.forEach { photo ->
-            val job = createPhotoUploadJob(context, photo)
-            job.invokeOnCompletion { _ ->
-                photoJobs.remove(photo.uri.toString())
-            }
-            photoJobs[photo.uri.toString()] = job
-        }
-    }
-
-    fun updateVisit(staccatoId: Long) {
-        viewModelScope.launch {
-            _isPosting.value = true
-            val staccatoTitleValue = staccatoTitle.get() ?: return@launch handleFailure()
-            val placeNameValue = placeName.value ?: return@launch handleFailure()
-            val addressValue = address.value ?: return@launch handleFailure()
-            val latitudeValue = latitude.value ?: return@launch handleFailure()
-            val longitudeValue = longitude.value ?: return@launch handleFailure()
-            val visitedAtValue = selectedVisitedAt.value ?: return@launch handleFailure()
-            val memoryIdValue = selectedMemory.value?.memoryId ?: return@launch handleFailure()
-            val momentImageUrlsValue =
-                currentPhotos.value?.attachedPhotos?.map { it.imageUrl!! }
-                    ?: return@launch handleFailure()
-
-            momentRepository.updateMoment(
-                momentId = staccatoId,
-                staccatoTitle = staccatoTitleValue,
-                placeName = placeNameValue,
-                address = addressValue,
-                latitude = latitudeValue,
-                longitude = longitudeValue,
-                visitedAt = visitedAtValue,
-                memoryId = memoryIdValue,
-                momentImageUrls = momentImageUrlsValue,
-            ).onSuccess {
-                _isUpdateCompleted.postValue(true)
-            }.onFailure { e ->
-                handleError(e.message)
-            }
-        }
-    }
-
-    private fun fetchStaccatoBy(staccatoId: Long) {
-        viewModelScope.launch {
-            momentRepository.getMoment(momentId = staccatoId)
-                .onSuccess { staccato ->
-                    staccatoTitle.set(staccato.staccatoTitle)
-                    _address.value = staccato.address
-                    _selectedVisitedAt.value = staccato.visitedAt
-                    _placeName.value = staccato.placeName
-                    _currentPhotos.value = createPhotosByUrls(staccato.momentImageUrls)
+                momentRepository.updateMoment(
+                    momentId = staccatoId,
+                    staccatoTitle = staccatoTitleValue,
+                    placeName = placeNameValue,
+                    address = addressValue,
+                    latitude = latitudeValue,
+                    longitude = longitudeValue,
+                    visitedAt = visitedAtValue,
+                    memoryId = memoryIdValue,
+                    momentImageUrls = momentImageUrlsValue,
+                ).onSuccess {
+                    _isUpdateCompleted.postValue(true)
                 }.onFailure { e ->
-                    _errorMessage.postValue(e.message ?: "알 수 없는 오류가 발생했습니다.")
+                    handleError(e.message)
+                }
+            }
+        }
+
+        private fun fetchStaccatoBy(staccatoId: Long) {
+            viewModelScope.launch {
+                momentRepository.getMoment(momentId = staccatoId)
+                    .onSuccess { staccato ->
+                        staccatoTitle.set(staccato.staccatoTitle)
+                        _address.value = staccato.address
+                        _selectedVisitedAt.value = staccato.visitedAt
+                        _placeName.value = staccato.placeName
+                        _currentPhotos.value = createPhotosByUrls(staccato.momentImageUrls)
+                    }.onFailure { e ->
+                        _errorMessage.postValue(e.message ?: "알 수 없는 오류가 발생했습니다.")
+                    }
+            }
+        }
+
+        private fun fetchMemoryCandidates() {
+            viewModelScope.launch {
+                timelineRepository.getMemoryCandidates()
+                    .onSuccess { memoryCandidates ->
+                        _memoryCandidates.value = memoryCandidates
+                    }
+            }
+        }
+
+        private fun createPhotoUploadJob(
+            context: Context,
+            photo: AttachedPhotoUiModel,
+        ) = viewModelScope.async(buildCoroutineExceptionHandler()) {
+            val multiPartBody =
+                convertExcretaFile(
+                    context,
+                    photo.uri,
+                    MomentCreationViewModel.FORM_DATA_NAME,
+                )
+            imageRepository.convertImageFileToUrl(multiPartBody)
+                .onSuccess {
+                    updatePhotoWithUrl(photo, it.imageUrl)
+                }
+                .onException { _, message ->
+                    _errorMessage.postValue(message)
                 }
         }
-    }
 
-    private fun fetchMemoryCandidates() {
-        viewModelScope.launch {
-            timelineRepository.getMemoryCandidates()
-                .onSuccess { memoryCandidates ->
-                    _memoryCandidates.value = memoryCandidates
-                }
+        private fun buildCoroutineExceptionHandler(): CoroutineExceptionHandler {
+            return CoroutineExceptionHandler { _, throwable ->
+                _errorMessage.postValue(throwable.message ?: "이미지 업로드에 실패했습니다.")
+            }
+        }
+
+        private fun updatePhotoWithUrl(
+            targetPhoto: AttachedPhotoUiModel,
+            url: String,
+        ) {
+            val updatedPhoto = targetPhoto.updateUrl(url)
+            _currentPhotos.value = currentPhotos.value?.updateOrAppendPhoto(updatedPhoto)
+        }
+
+        private fun handleFailure() {
+            _isPosting.value = false
+        }
+
+        private fun handleError(errorMessage: String?) {
+            _isPosting.value = false
+            _errorMessage.postValue(errorMessage ?: "알 수 없는 오류가 발생했습니다.")
         }
     }
-
-    private fun createPhotoUploadJob(
-        context: Context,
-        photo: AttachedPhotoUiModel,
-    ) = viewModelScope.async(buildCoroutineExceptionHandler()) {
-        val multiPartBody =
-            convertExcretaFile(
-                context,
-                photo.uri,
-                MomentCreationViewModel.FORM_DATA_NAME,
-            )
-        imageRepository.convertImageFileToUrl(multiPartBody)
-            .onSuccess {
-                updatePhotoWithUrl(photo, it.imageUrl)
-            }
-            .onException { _, message ->
-                _errorMessage.postValue(message)
-            }
-    }
-
-    private fun buildCoroutineExceptionHandler(): CoroutineExceptionHandler {
-        return CoroutineExceptionHandler { _, throwable ->
-            _errorMessage.postValue(throwable.message ?: "이미지 업로드에 실패했습니다.")
-        }
-    }
-
-    private fun updatePhotoWithUrl(
-        targetPhoto: AttachedPhotoUiModel,
-        url: String,
-    ) {
-        val updatedPhoto = targetPhoto.updateUrl(url)
-        _currentPhotos.value = currentPhotos.value?.updateOrAppendPhoto(updatedPhoto)
-    }
-
-    private fun handleFailure() {
-        _isPosting.value = false
-    }
-
-    private fun handleError(errorMessage: String?) {
-        _isPosting.value = false
-        _errorMessage.postValue(errorMessage ?: "알 수 없는 오류가 발생했습니다.")
-    }
-}
