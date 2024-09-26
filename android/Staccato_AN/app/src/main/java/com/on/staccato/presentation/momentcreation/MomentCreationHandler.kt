85a869825835ed682603ade1773984f48feb6594
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2024-09-27 01:47:37 +0900

    feat: 스타카토 생성/수정 화면에서 추억, 날짜, 시간 선택 기능 구현 #401 (#412)
    * refactor: startAt과 endAt nullable하게 수정
    
    * refactor: 스타카토의 유효한 일시를 계산하는 메서드 추가
    
    * refactor: 추억 화면에서 스타카토 생성 가능 기간 확인 로직 제거
    
    * feat: MemoryVisitedAtSelectionFragment 구현
    
    * feat: 스타카토 생성 화면에서 추억, 날짜, 시간 선택 기능 구현
    
    * feat: 스타카토 수정 화면에서 추억, 날짜, 시간 선택 기능 구현
    
    * style: 불필요한 주석 제거
    
    * style: KtLint CI 에러 수정

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/momentcreation/MomentCreationHandler.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/momentcreation/MomentCreationHandler.kt
@@ -3,7 +3,5 @@ package com.on.staccato.presentation.momentcreation
 interface MomentCreationHandler {
     fun onMemorySelectionClicked()
 
-    fun onVisitedAtSelectionClicked()
-
     fun onCreateDoneClicked()
 }
