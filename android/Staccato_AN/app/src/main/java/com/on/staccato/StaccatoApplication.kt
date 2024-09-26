00c2240e53b08b41465dba4c4b06447205853688
hxeyexn <kck85075@kumoh.ac.kr>
2024-09-26 19:58:58 +0900

    feat: Application 클래스에 Hilt를 설정

--- android/Staccato_AN/app/src/main/java/com/on/staccato/StaccatoApplication.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/StaccatoApplication.kt
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatDelegate
 import com.google.android.libraries.places.api.net.PlacesClient
 import com.on.staccato.data.PlacesClientProvider
 import com.on.staccato.data.UserInfoPreferencesManager
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class StaccatoApplication : Application() {
     override fun onCreate() {
         super.onCreate()
