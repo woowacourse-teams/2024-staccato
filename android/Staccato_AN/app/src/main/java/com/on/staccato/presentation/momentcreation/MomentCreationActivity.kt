c878e15c72ef38c40f6cf53e18ff3d96c7cd298d
hxeyexn <kck85075@kumoh.ac.kr>
2024-09-30 02:45:57 +0900

    feat: 스타카토 생성 화면 onCreate 내 위치 권한 설정 메서드 호출 제거
    - onCreate와 onResume에서 중복 호출하고 있어 onCreate 내 위치 권한 설정 메서드 호출을 제거함

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/momentcreation/MomentCreationActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/momentcreation/MomentCreationActivity.kt
@@ -79,7 +79,6 @@ class MomentCreationActivity :
         viewModel.fetchMemoryCandidates(memoryId)
         setupPermissionRequestLauncher()
         setupFusedLocationProviderClient()
-        fetchCurrentLocationAddress()
         initBinding()
         initAdapter()
         initItemTouchHelper()
