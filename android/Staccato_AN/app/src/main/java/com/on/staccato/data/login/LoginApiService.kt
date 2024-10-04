4cb579c44544e53e963ed8a3d7cde91bf3403bff
Junyoung-WON <wjyo60@gmail.com>
2024-10-04 13:49:20 +0900

    refactor: api path 경로를 동반객체로 분리 및 변수명 수정

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/login/LoginApiService.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/login/LoginApiService.kt
@@ -7,8 +7,12 @@ import retrofit2.http.Body
 import retrofit2.http.POST
 
 interface LoginApiService {
-    @POST("/login")
+    @POST(LOGIN_PATH)
     suspend fun postNicknameLogin(
         @Body nicknameLoginRequest: NicknameLoginRequest,
     ): Response<NicknameLoginResponse>
+
+    companion object {
+        private const val LOGIN_PATH = "/login"
+    }
 }
--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/member/MemberApiService.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/member/MemberApiService.kt
@@ -6,13 +6,13 @@ import retrofit2.http.POST
 import retrofit2.http.Query
 
 interface MemberApiService {
-    @POST(MEMBERS_URL)
+    @POST(MEMBERS_PATH)
     suspend fun postRecoveryCode(
         @Query(RECOVERY_CODE) recoveryCode: String,
     ): Response<RecoveryCodeResponse>
 
     companion object {
-        private const val MEMBERS_URL = "/members"
+        private const val MEMBERS_PATH = "/members"
         private const val RECOVERY_CODE = "code"
     }
 }
