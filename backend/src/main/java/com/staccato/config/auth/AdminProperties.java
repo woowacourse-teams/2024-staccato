d9fc2349280c1c87f4b23280bc0b78d9d321fa40
linirini <101927543+linirini@users.noreply.github.com>
2024-10-06 19:33:11 +0900

    feat: 개발용 닉네임 관련 로직 제거 #424 (#425)

--- backend/src/main/java/com/staccato/config/auth/AdminProperties.java
+++
@@ -1,8 +0,0 @@
-package com.staccato.config.auth;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-
-@ConfigurationProperties(prefix = "security.admin")
-public record AdminProperties(String key, String token) {
-}
-
