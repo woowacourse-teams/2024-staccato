6da595de182106d8edf2c1a7d8ac944c03962dcd
Hyeyeon Gong <103019852+hxeyexn@users.noreply.github.com>
2024-10-08 19:24:13 +0900

    fix: conflict 해결 (#461)

--- backend/src/main/java/com/staccato/memory/repository/MemoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.staccato.memory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.staccato.memory.domain.Memory;
-
-public interface MemoryRepository extends JpaRepository<Memory, Long> {
-    boolean existsByTitle(String title);
-}
--- backend/src/main/java/com/staccato/moment/service/dto/response/MomentLocationResponses.java
+++
@@ -1,9 +0,0 @@
-package com.staccato.moment.service.dto.response;
-
-import java.util.List;
-
-import io.swagger.v3.oas.annotations.media.Schema;
-
-@Schema(description = "스타카토 목록에 해당하는 응답입니다.")
-public record MomentLocationResponses(List<MomentLocationResponse> momentLocationResponses) {
-}
