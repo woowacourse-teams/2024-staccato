d14955fb53f1758cc5f7c2ca78ebe79c07bfff02
yoonjuho <dbswn990@naver.com>
2024-10-10 18:54:04 +0900

    refactor: 모든 추억 조회 시 발생하는 N+1문제 해결

--- backend/src/main/java/com/staccato/memory/repository/MemoryMemberRepository.java
+++ backend/src/main/java/com/staccato/memory/repository/MemoryMemberRepository.java
@@ -2,16 +2,16 @@ package com.staccato.memory.repository;
 
 import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-
 import com.staccato.member.domain.Member;
 import com.staccato.memory.domain.MemoryMember;
 
 public interface MemoryMemberRepository extends JpaRepository<MemoryMember, Long> {
-    List<MemoryMember> findAllByMemberIdOrderByMemory(long memberId);
+
+    @Query("SELECT mm FROM MemoryMember mm JOIN FETCH mm.memory WHERE mm.member.id = :memberId")
+    List<MemoryMember> findAllByMemberId(long memberId);
 
     @Query("""
             SELECT mm FROM MemoryMember mm WHERE mm.member.id = :memberId
