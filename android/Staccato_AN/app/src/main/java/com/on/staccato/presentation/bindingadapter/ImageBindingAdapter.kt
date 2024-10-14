3ef1e5de24d6aad348e0af2fe02cff04df9a4f26
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2024-10-14 16:23:29 +0900

    refactor: BindingAdapter 분리 및 리팩토링 #469 (#472)
    * refactor: BindingAdapter 파일 분리
    
    * refactor: ButtonBindingAdapter 리팩토링
    
    * refactor: ImageBindingAdapter 리팩토링
    
    * refactor: TextBindingAdapter 리팩토링
    
    * refactor: ExtBindingAdapter 리팩토링
    
    * refactor: 오전, 오후 텍스트 strings.xml로 분리
    
    * refactor: 매직 넘버 상수로 분리
    
    * style: EtxBindingAdapters -> BindingAdapters.kt
    
    * chore: 불필요한 메서드 제거
    
    * refactor: 상수화 및 String formatting
    
    * fix: 추억 수정 화면 dateRangePicker 중복 버그 해결
    
    * refactor: 추억 기간 선택 시 visibility 바인딩 어댑터 적용
    
    * refactor: okhttp3 import 제거 및 scrollToBottom rename

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/bindingadapter/ImageBindingAdapter.kt
@@ -0,0 +1,91 @@
+package com.on.staccato.presentation.bindingadapter
+
+import android.graphics.drawable.Drawable
+import android.net.Uri
+import android.widget.ImageView
+import androidx.databinding.BindingAdapter
+import coil.load
+import coil.transform.RoundedCornersTransformation
+
+@BindingAdapter("selected")
+fun ImageView.setSelected(selected: Boolean) {
+    isSelected = selected
+}
+
+@BindingAdapter(
+    value = ["coilImageUrl", "coilPlaceHolder"],
+)
+fun ImageView.loadCoilImage(
+    url: String?,
+    placeHolder: Drawable? = null,
+) {
+    load(url) {
+        placeholder(placeHolder)
+        error(placeHolder)
+    }
+}
+
+@BindingAdapter(
+    value = ["coilCircleImageUrl", "coilPlaceHolder"],
+)
+fun ImageView.loadCoilCircleImage(
+    url: String?,
+    placeHolder: Drawable? = null,
+) {
+    load(url) {
+        placeholder(placeHolder)
+        transformations(RoundedCornersTransformation(1000f))
+        error(placeHolder)
+    }
+}
+
+@BindingAdapter(
+    value = ["coilRoundedCornerImageUrl", "coilRoundedCornerPlaceHolder", "coilRoundingRadius"],
+)
+fun ImageView.setCoilRoundedCornerImage(
+    url: String?,
+    placeHolder: Drawable? = null,
+    roundingRadius: Float,
+) {
+    load(url) {
+        placeholder(placeHolder)
+        transformations(RoundedCornersTransformation(roundingRadius))
+        error(placeHolder)
+    }
+}
+
+@BindingAdapter(
+    value = ["coilRoundedCornerImageUrl", "coilRoundedCornerImageUri", "coilRoundedCornerPlaceHolder", "coilRoundingRadius"],
+)
+fun ImageView.setCoilRoundedCornerImageWithUri(
+    url: String?,
+    uri: Uri?,
+    placeHolder: Drawable? = null,
+    roundingRadius: Float,
+) {
+    val image = uri ?: url
+    load(image) {
+        placeholder(placeHolder)
+        transformations(RoundedCornersTransformation(roundingRadius))
+        error(placeHolder)
+    }
+}
+
+@BindingAdapter(
+    value = [
+        "colorImageResource",
+        "grayImageResource",
+    ],
+)
+fun ImageView.setImageResourceWithId(
+    colorResId: Int,
+    grayResId: Int,
+) {
+    setImageResource(
+        if (isSelected) {
+            colorResId
+        } else {
+            grayResId
+        },
+    )
+}
