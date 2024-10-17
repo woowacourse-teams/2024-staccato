6d523361b8a461cf85b903071fd2263f4eb8d0d6
Junyoung-WON <wjyo60@gmail.com>
2024-10-17 21:59:52 +0900

    refactor: 댓글과 조회 Fragment 병합 및 댓글 화면 개선
    StaccatoFragment
    - 메서드 순서 조정 및 이름 변경
    - staccatoId를 lazy 로 지연초기화하여 받아오도록 수정
    - 댓글 ViewModel과 Adapter 추가 및 연결
    - 댓글 업로드 시 스크롤 뷰의 스크롤을 하단으로 옮기도록 구현
    
    StaccatoCommentsViewModel
    - 댓글 전송 성공한 상태를 저장하는 LiveData 추가
    - Staccato ID 기본값 상수화
    
    fragment_staccato.xml
    - 댓글 화면 마이그레이션
    - 댓글 입력 창을 화면 최하단에 고정
    - 전송 버튼 UI 오류 수정: 고정된 크기

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/staccato/StaccatoFragment.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/staccato/StaccatoFragment.kt
@@ -2,7 +2,10 @@ package com.on.staccato.presentation.staccato
 
 import android.os.Bundle
 import android.view.View
+import android.view.ViewTreeObserver
+import android.widget.ScrollView
 import androidx.core.os.bundleOf
+import androidx.core.widget.NestedScrollView
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -13,22 +16,24 @@ import com.on.staccato.presentation.base.BindingFragment
 import com.on.staccato.presentation.common.DeleteDialogFragment
 import com.on.staccato.presentation.main.MainActivity
 import com.on.staccato.presentation.main.viewmodel.SharedViewModel
-import com.on.staccato.presentation.staccato.comments.StaccatoCommentsFragment
+import com.on.staccato.presentation.staccato.comments.CommentsAdapter
+import com.on.staccato.presentation.staccato.comments.StaccatoCommentsViewModel
 import com.on.staccato.presentation.staccato.detail.ViewpagePhotoAdapter
 import com.on.staccato.presentation.staccato.feeling.StaccatoFeelingSelectionFragment
 import com.on.staccato.presentation.staccato.viewmodel.StaccatoViewModel
 import com.on.staccato.presentation.staccatoupdate.StaccatoUpdateActivity
 import com.on.staccato.presentation.util.showToast
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.properties.Delegates
 
 @AndroidEntryPoint
 class StaccatoFragment :
     BindingFragment<FragmentStaccatoBinding>(R.layout.fragment_staccato), StaccatoToolbarHandler {
-    private val staccatoViewModel: StaccatoViewModel by viewModels()
-    private var staccatoId by Delegates.notNull<Long>()
-    private lateinit var pagePhotoAdapter: ViewpagePhotoAdapter
     private val sharedViewModel: SharedViewModel by activityViewModels<SharedViewModel>()
+    private val staccatoViewModel: StaccatoViewModel by viewModels()
+    private val commentsViewModel: StaccatoCommentsViewModel by viewModels()
+    private val commentsAdapter: CommentsAdapter by lazy { CommentsAdapter(commentsViewModel) }
+    private val staccatoId by lazy { arguments?.getLong(STACCATO_ID_KEY) ?: DEFAULT_STACCATO_ID }
+    private lateinit var pagePhotoAdapter: ViewpagePhotoAdapter
     private val deleteDialog =
         DeleteDialogFragment {
             staccatoViewModel.deleteStaccato(staccatoId)
@@ -38,71 +43,15 @@ class StaccatoFragment :
         view: View,
         savedInstanceState: Bundle?,
     ) {
-        staccatoId = arguments?.getLong(STACCATO_ID_KEY) ?: return
-        binding.viewModel = staccatoViewModel
+        setUpBindings()
+        setUpComments()
         initToolbarHandler()
         initViewPagerAdapter()
         loadStaccatoData()
-        observeData()
-        createChildFragments(savedInstanceState)
-        observeViewModel()
-    }
-
-    private fun initViewPagerAdapter() {
-        pagePhotoAdapter = ViewpagePhotoAdapter()
-        binding.vpStaccatoPhotoHorizontal.adapter = pagePhotoAdapter
-        TabLayoutMediator(
-            binding.tabStaccatoPhotoHorizontal,
-            binding.vpStaccatoPhotoHorizontal,
-        ) { _, _ -> }.attach()
-    }
-
-    private fun observeViewModel() {
-        staccatoViewModel.staccatoDetail.observe(viewLifecycleOwner) { staccatoDetail ->
-            pagePhotoAdapter.submitList(staccatoDetail.staccatoImageUrls)
-        }
-    }
-
-    private fun loadStaccatoData() {
-        staccatoViewModel.loadStaccato(staccatoId)
-    }
-
-    private fun createChildFragments(savedInstanceState: Bundle?) {
-        if (savedInstanceState == null) {
-            val bundle =
-                bundleOf(STACCATO_ID_KEY to staccatoId)
-            val staccatoFeelingSelectionFragment =
-                StaccatoFeelingSelectionFragment().apply {
-                    arguments = bundle
-                }
-            val staccatoCommentsFragment = StaccatoCommentsFragment().apply { arguments = bundle }
-            childFragmentManager.beginTransaction()
-                .replace(R.id.container_staccato_feeling_selection, staccatoFeelingSelectionFragment)
-                .replace(R.id.container_staccato_comments, staccatoCommentsFragment)
-                .commit()
-        }
-    }
-
-    private fun initToolbarHandler() {
-        binding.toolbarHandler = this
-        binding.toolbarStaccato.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }
-    }
-
-    private fun observeData() {
-        staccatoViewModel.isError.observe(viewLifecycleOwner) { isError ->
-            if (isError) {
-                showToast("스타카토를 불러올 수 없어요!")
-                findNavController().popBackStack()
-            }
-        }
-        staccatoViewModel.isDeleted.observe(viewLifecycleOwner) { isDeleted ->
-            if (isDeleted) {
-                sharedViewModel.setStaccatosHasUpdated()
-                findNavController().popBackStack()
-            }
-        }
+        loadComments()
+        observeStaccatoViewModel()
+        observeCommentsViewModel()
+        setStaccatoIdToFeelingFragment(savedInstanceState)
     }
 
     override fun onDeleteClicked() {
@@ -121,6 +70,116 @@ class StaccatoFragment :
             context = requireContext(),
             activityLauncher = staccatoUpdateLauncher,
         )
+    }
+
+    private fun setUpBindings() {
+        binding.staccatoViewModel = staccatoViewModel
+        binding.commentsViewModel = commentsViewModel
+        binding.commentHandler = commentsViewModel
+    }
+
+    private fun setUpComments() {
+        binding.rvStaccatoComments.adapter = commentsAdapter
+        binding.rvStaccatoComments.itemAnimator = null
+
+    }
+
+    private fun initToolbarHandler() {
+        binding.toolbarHandler = this
+        binding.toolbarStaccato.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    private fun initViewPagerAdapter() {
+        pagePhotoAdapter = ViewpagePhotoAdapter()
+        binding.vpStaccatoPhotoHorizontal.adapter = pagePhotoAdapter
+        TabLayoutMediator(
+            binding.tabStaccatoPhotoHorizontal,
+            binding.vpStaccatoPhotoHorizontal,
+        ) { _, _ -> }.attach()
+    }
+
+    private fun loadStaccatoData() {
+        staccatoViewModel.loadStaccato(staccatoId)
+    }
+
+    private fun loadComments() {
+        commentsViewModel.setMemoryId(staccatoId)
+        commentsViewModel.fetchComments()
+    }
+
+    private fun observeStaccatoViewModel() {
+        staccatoViewModel.staccatoDetail.observe(viewLifecycleOwner) { staccatoDetail ->
+            pagePhotoAdapter.submitList(staccatoDetail.staccatoImageUrls)
+        }
+
+        staccatoViewModel.isError.observe(viewLifecycleOwner) { isError ->
+            if (isError) {
+                showToast(getString(R.string.staccato_loading_failure))
+                findNavController().popBackStack()
+            }
+        }
+
+        staccatoViewModel.isDeleted.observe(viewLifecycleOwner) { isDeleted ->
+            if (isDeleted) {
+                sharedViewModel.setStaccatosHasUpdated()
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    private fun observeCommentsViewModel() {
+        commentsViewModel.comments.observe(viewLifecycleOwner) { comments ->
+            commentsAdapter.updateComments(comments)
+        }
+
+        commentsViewModel.isDeleteSuccess.observe(viewLifecycleOwner) { isDeleted ->
+            if (isDeleted) {
+                showToast(getString(R.string.staccato_comment_has_been_deleted))
+            }
+        }
+
+        commentsViewModel.isSendingSuccess.observe(viewLifecycleOwner) { isSuccess ->
+            if (isSuccess) {
+                scrollToBottom()
+            }
+        }
+    }
+
+    private fun scrollToBottom() {
+        with(binding.nsvStaccato) {
+            viewTreeObserver.addOnGlobalLayoutListener(
+                scrollableToBottomListener()
+            )
+        }
+    }
+
+    private fun NestedScrollView.scrollableToBottomListener() =
+        object : ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+                post {
+                    fullScroll(ScrollView.FOCUS_DOWN)
+                }
+                viewTreeObserver.removeOnGlobalLayoutListener(this)
+            }
+        }
+
+    private fun setStaccatoIdToFeelingFragment(savedInstanceState: Bundle?) {
+        if (savedInstanceState == null) {
+            val bundle =
+                bundleOf(STACCATO_ID_KEY to staccatoId)
+            val staccatoFeelingSelectionFragment =
+                StaccatoFeelingSelectionFragment().apply {
+                    arguments = bundle
+                }
+            childFragmentManager.beginTransaction()
+                .replace(
+                    R.id.container_staccato_feeling_selection,
+                    staccatoFeelingSelectionFragment,
+                )
+                .commit()
+        }
     }
 
     companion object {
