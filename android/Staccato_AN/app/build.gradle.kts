f2434a5200889fcac93243fff33ccf1735f53887
hodu <92203597+Junyoung-WON@users.noreply.github.com>
2024-11-27 17:41:44 +0900

    deploy: v.1.2.1 배포 준비 #539 (#541)
    * ui: 마이페이지의 앱 버전 업데이트
    
    - 1.2.0 -> 1.2.1 로 업데이트
    
    * build: versionCode 와 versionName 업데이트
    
    - versionCode: 6 -> 7
    - versionName: 1.2.0 -> 1.2.1
    
    * docs: 출시 노트 수정
    
    - 1.2.1 버전: 버그 수정 내용 추가
    
    * docs: 출시 노트의 업데이트 날짜 수정
    
    - 10.31 -> 11.27

--- android/Staccato_AN/app/build.gradle.kts
+++ android/Staccato_AN/app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         applicationId = "com.on.staccato"
         minSdk = 26
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.2.0"
+        versionCode = 7
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
