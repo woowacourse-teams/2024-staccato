d2e5e9db249088df46b568100266be7f01d6913a
hxeyexn <kck85075@kumoh.ac.kr>
2024-11-28 20:40:09 +0900

    refactor: 카테고리(전 추억) 기능 CallAdapter 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/memory/MemoryRemoteDataSource.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/memory/MemoryRemoteDataSource.kt
@@ -1,6 +1,5 @@
 package com.on.staccato.data.memory
 
-import com.on.staccato.data.ApiResponseHandler.handleApiResponse
 import com.on.staccato.data.ResponseResult
 import com.on.staccato.data.dto.mapper.toDto
 import com.on.staccato.data.dto.memory.MemoriesResponse
@@ -14,29 +13,22 @@ class MemoryRemoteDataSource
     constructor(
         private val memoryApiService: MemoryApiService,
     ) : MemoryDataSource {
-        override suspend fun getMemory(memoryId: Long): ResponseResult<MemoryResponse> =
-            handleApiResponse { memoryApiService.getMemory(memoryId) }
+        override suspend fun getMemory(memoryId: Long): ResponseResult<MemoryResponse> = memoryApiService.getMemory(memoryId)
 
-        override suspend fun getMemories(currentDate: String?): ResponseResult<MemoriesResponse> =
-            handleApiResponse { memoryApiService.getMemories(currentDate) }
+        override suspend fun getMemories(currentDate: String?): ResponseResult<MemoriesResponse> = memoryApiService.getMemories(currentDate)
 
         override suspend fun createMemory(newMemory: NewMemory): ResponseResult<MemoryCreationResponse> =
-            handleApiResponse {
-                memoryApiService.postMemory(newMemory.toDto())
-            }
+            memoryApiService.postMemory(
+                newMemory.toDto(),
+            )
 
         override suspend fun updateMemory(
             memoryId: Long,
             newMemory: NewMemory,
-        ): ResponseResult<Unit> =
-            handleApiResponse {
-                memoryApiService.putMemory(memoryId, newMemory.toDto())
-            }
+        ): ResponseResult<Unit> = memoryApiService.putMemory(memoryId, newMemory.toDto())
 
         override suspend fun deleteMemory(memoryId: Long): ResponseResult<Unit> =
-            handleApiResponse {
-                memoryApiService.deleteMemory(
-                    memoryId,
-                )
-            }
+            memoryApiService.deleteMemory(
+                memoryId,
+            )
     }
