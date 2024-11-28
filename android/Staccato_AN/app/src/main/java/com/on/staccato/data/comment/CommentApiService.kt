535a3e9f6808556047e8a30ba08eef2bcb2ae1a1
hxeyexn <kck85075@kumoh.ac.kr>
2024-11-28 20:47:30 +0900

    refactor: 댓글 기능 CallAdapter 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/comment/CommentApiService.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/comment/CommentApiService.kt
@@ -1,9 +1,9 @@
 package com.on.staccato.data.comment
 
+import com.on.staccato.data.ResponseResult
 import com.on.staccato.data.dto.comment.CommentRequest
 import com.on.staccato.data.dto.comment.CommentUpdateRequest
 import com.on.staccato.data.dto.comment.CommentsResponse
-import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.DELETE
 import retrofit2.http.GET
@@ -16,23 +16,23 @@ interface CommentApiService {
     @GET(COMMENTS_URI)
     suspend fun getComments(
         @Query(STACCATO_ID) staccatoId: Long,
-    ): Response<CommentsResponse>
+    ): ResponseResult<CommentsResponse>
 
     @POST(COMMENTS_URI)
     suspend fun postComment(
         @Body commentRequest: CommentRequest,
-    ): Response<Unit>
+    ): ResponseResult<Unit>
 
     @PUT(COMMENTS_URI_WITH_COMMENT_ID)
     suspend fun putComment(
         @Path(COMMENT_ID) commentId: Long,
         @Body commentUpdateRequest: CommentUpdateRequest,
-    ): Response<Unit>
+    ): ResponseResult<Unit>
 
     @DELETE(COMMENTS_URI_WITH_COMMENT_ID)
     suspend fun deleteComment(
         @Path(COMMENT_ID) commentId: Long,
-    ): Response<Unit>
+    ): ResponseResult<Unit>
 
     companion object {
         private const val COMMENTS_URI = "/comments"
--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/comment/CommentRemoteDataSource.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/comment/CommentRemoteDataSource.kt
@@ -1,6 +1,5 @@
 package com.on.staccato.data.comment
 
-import com.on.staccato.data.ApiResponseHandler.handleApiResponse
 import com.on.staccato.data.ResponseResult
 import com.on.staccato.data.dto.comment.CommentRequest
 import com.on.staccato.data.dto.comment.CommentUpdateRequest
@@ -12,23 +11,21 @@ class CommentRemoteDataSource
     constructor(
         private val commentApiService: CommentApiService,
     ) : CommentDataSource {
-        override suspend fun getComments(staccatoId: Long): ResponseResult<CommentsResponse> =
-            handleApiResponse { commentApiService.getComments(staccatoId) }
+        override suspend fun getComments(staccatoId: Long): ResponseResult<CommentsResponse> = commentApiService.getComments(staccatoId)
 
         override suspend fun createComment(commentRequest: CommentRequest): ResponseResult<Unit> =
-            handleApiResponse { commentApiService.postComment(commentRequest) }
+            commentApiService.postComment(
+                commentRequest,
+            )
 
         override suspend fun updateComment(
             commentId: Long,
             commentUpdateRequest: CommentUpdateRequest,
         ): ResponseResult<Unit> =
-            handleApiResponse {
-                commentApiService.putComment(
-                    commentId,
-                    commentUpdateRequest,
-                )
-            }
+            commentApiService.putComment(
+                commentId,
+                commentUpdateRequest,
+            )
 
-        override suspend fun deleteComment(commentId: Long): ResponseResult<Unit> =
-            handleApiResponse { commentApiService.deleteComment(commentId) }
+        override suspend fun deleteComment(commentId: Long): ResponseResult<Unit> = commentApiService.deleteComment(commentId)
     }
