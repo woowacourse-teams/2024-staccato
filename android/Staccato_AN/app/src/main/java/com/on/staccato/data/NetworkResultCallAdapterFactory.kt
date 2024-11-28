2ce3a7df9ddb70eab978044e6d492f1e4685e276
hxeyexn <kck85075@kumoh.ac.kr>
2024-11-28 19:58:32 +0900

    feat: CallAdapterFactory 구현

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/NetworkResultCallAdapterFactory.kt
@@ -0,0 +1,31 @@
+package com.on.staccato.data
+
+import retrofit2.Call
+import retrofit2.CallAdapter
+import retrofit2.Retrofit
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+
+class NetworkResultCallAdapterFactory : CallAdapter.Factory() {
+    override fun get(
+        returnType: Type,
+        annotations: Array<out Annotation>,
+        retrofit: Retrofit,
+    ): CallAdapter<*, *>? {
+        if (getRawType(returnType) != Call::class.java) {
+            return null
+        }
+
+        val callType = getParameterUpperBound(0, returnType as ParameterizedType)
+        if (getRawType(callType) != ResponseResult::class.java) {
+            return null
+        }
+
+        val resultType = getParameterUpperBound(0, callType as ParameterizedType)
+        return NetworkResultCallAdapter(resultType)
+    }
+
+    companion object {
+        fun create(): NetworkResultCallAdapterFactory = NetworkResultCallAdapterFactory()
+    }
+}
