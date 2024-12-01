a9b13eb3ea1d4cf8be8622796d4be64afe9e7532
hxeyexn <kck85075@kumoh.ac.kr>
2024-12-01 22:06:52 +0900

    refactor: 카테고리 리포지터리 ApiResult 처리 확장 함수 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/memory/MemoryDefaultRepository.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/memory/MemoryDefaultRepository.kt
@@ -1,11 +1,9 @@
 package com.on.staccato.data.memory
 
 import com.on.staccato.data.ApiResult
-import com.on.staccato.data.Exception
-import com.on.staccato.data.ServerError
-import com.on.staccato.data.Success
 import com.on.staccato.data.dto.mapper.toDomain
 import com.on.staccato.data.dto.memory.MemoryCreationResponse
+import com.on.staccato.data.handle
 import com.on.staccato.domain.model.Memory
 import com.on.staccato.domain.model.MemoryCandidates
 import com.on.staccato.domain.model.NewMemory
@@ -17,46 +15,18 @@ class MemoryDefaultRepository
     constructor(
         private val memoryDataSource: MemoryDataSource,
     ) : MemoryRepository {
-        override suspend fun getMemory(memoryId: Long): ApiResult<Memory> {
-            return when (val responseResult = memoryDataSource.getMemory(memoryId)) {
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Success -> Success(responseResult.data.toDomain())
-            }
-        }
+        override suspend fun getMemory(memoryId: Long): ApiResult<Memory> = memoryDataSource.getMemory(memoryId).handle { it.toDomain() }
 
-        override suspend fun getMemories(currentDate: String?): ApiResult<MemoryCandidates> {
-            return when (val responseResult = memoryDataSource.getMemories(currentDate)) {
-                is Success -> Success(responseResult.data.toDomain())
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-            }
-        }
+        override suspend fun getMemories(currentDate: String?): ApiResult<MemoryCandidates> =
+            memoryDataSource.getMemories(currentDate).handle { it.toDomain() }
 
-        override suspend fun createMemory(newMemory: NewMemory): ApiResult<MemoryCreationResponse> {
-            return when (val responseResult = memoryDataSource.createMemory(newMemory)) {
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Success -> Success(responseResult.data)
-            }
-        }
+        override suspend fun createMemory(newMemory: NewMemory): ApiResult<MemoryCreationResponse> =
+            memoryDataSource.createMemory(newMemory).handle { it }
 
         override suspend fun updateMemory(
             memoryId: Long,
             newMemory: NewMemory,
-        ): ApiResult<Unit> {
-            return when (val responseResult = memoryDataSource.updateMemory(memoryId, newMemory)) {
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Success -> Success(responseResult.data)
-            }
-        }
+        ): ApiResult<Unit> = memoryDataSource.updateMemory(memoryId, newMemory).handle { Unit }
 
-        override suspend fun deleteMemory(memoryId: Long): ApiResult<Unit> {
-            return when (val responseResult = memoryDataSource.deleteMemory(memoryId)) {
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Success -> Success(Unit)
-            }
-        }
+        override suspend fun deleteMemory(memoryId: Long): ApiResult<Unit> = memoryDataSource.deleteMemory(memoryId).handle { Unit }
     }
