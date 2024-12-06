7c0a30c5ee5a7ca26c502de55af818b068e059c2
somin <salth0601@gmail.com>
2024-12-06 18:05:16 +0900

    feat: MemoryCandidates에 getValidMemoryCandidate 메서드 오버로딩

--- android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/MemoryCandidates.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/MemoryCandidates.kt
@@ -4,4 +4,6 @@ import java.time.LocalDate
 
 data class MemoryCandidates(val memoryCandidate: List<MemoryCandidate>) {
     fun getValidMemoryCandidate(date: LocalDate) = memoryCandidate.filter { it.isDateWithinPeriod(date) }
+
+    fun getValidMemoryCandidate(memoryId: Long) = memoryCandidate.filter { it.memoryId == memoryId }
 }
