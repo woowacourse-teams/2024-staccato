1c53970a6f62b911ff2a14b50a10a18a0108495f
somin <salth0601@gmail.com>
2024-12-06 18:05:39 +0900

    test: MemoryCandidates의 getValidMemoryCandidate 메서드 테스트 추가

--- android/Staccato_AN/app/src/test/java/com/on/staccato/domain/model/MemoryCandidatesTest.kt
+++ android/Staccato_AN/app/src/test/java/com/on/staccato/domain/model/MemoryCandidatesTest.kt
@@ -2,6 +2,7 @@ package com.on.staccato.domain.model
 
 import junitparams.JUnitParamsRunner
 import junitparams.Parameters
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -11,7 +12,7 @@ import java.time.LocalDate
 class MemoryCandidatesTest {
     @Test
     @Parameters(method = "parameters")
-    fun `getValidMemoryCandidate는 date를 포함하는 후보만 필터링`(date: LocalDate) {
+    fun `date를 포함하는 후보들을 필터링해 반환`(date: LocalDate) {
         // given
         val memoryCandidates = dummyMemoryCandidates
 
@@ -19,8 +20,21 @@ class MemoryCandidatesTest {
         val selectableMemoryCandidate = memoryCandidates.getValidMemoryCandidate(date)
 
         // then
-        val actual = selectableMemoryCandidate.all { it.isDateWithinPeriod(date) }
-        assertTrue(actual)
+        val isEveryCandidatesContainDate = selectableMemoryCandidate.all { it.isDateWithinPeriod(date) }
+        assertTrue(isEveryCandidatesContainDate)
+    }
+
+    @Test
+    fun `아이디가 정확히 일치하는 memoryCandidate를 찾아 반환`() {
+        // given
+        val memoryCandidates = dummyMemoryCandidates
+
+        // when
+        val actual = memoryCandidates.getValidMemoryCandidate(TARGET_MEMORY_ID)
+        val expected = listOf(targetMemoryCandidate)
+
+        // then
+        assertEquals(expected, actual)
     }
 
     private fun parameters(): List<LocalDate> = listOf(yearEnd2023, yearStart2024, yearMiddle2024, yearEnd2024, yearStart2025)
