334d5636787a59f2bf437c093a4190f62e295bcc
hxeyexn <kck85075@kumoh.ac.kr>
2024-12-08 02:54:40 +0900

    refactor: 댓글 리포지터리 ApiResult 처리 확장 함수 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/comment/CommentDefaultRepository.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/comment/CommentDefaultRepository.kt
@@ -1,15 +1,14 @@
 package com.on.staccato.data.comment
 
 import com.on.staccato.data.ApiResult
-import com.on.staccato.data.Exception
-import com.on.staccato.data.ServerError
-import com.on.staccato.data.Success
 import com.on.staccato.data.dto.comment.CommentUpdateRequest
 import com.on.staccato.data.dto.mapper.toDomain
 import com.on.staccato.data.dto.mapper.toDto
+import com.on.staccato.data.handle
 import com.on.staccato.domain.model.Comment
 import com.on.staccato.domain.model.NewComment
 import com.on.staccato.domain.repository.CommentRepository
+import kotlinx.serialization.json.JsonNull.content
 import javax.inject.Inject
 
 class CommentDefaultRepository
@@ -18,39 +17,21 @@ class CommentDefaultRepository
         private val commentDataSource: CommentDataSource,
     ) : CommentRepository {
         override suspend fun fetchComments(staccatoId: Long): ApiResult<List<Comment>> =
-            when (val responseResult = commentDataSource.getComments(staccatoId)) {
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Exception -> Exception(responseResult.e)
-                is Success -> Success(responseResult.data.toDomain())
-            }
+            commentDataSource.getComments(staccatoId).handle { it.toDomain() }
 
         override suspend fun createComment(newComment: NewComment): ApiResult<Unit> =
-            when (val responseResult = commentDataSource.createComment(newComment.toDto())) {
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Exception -> Exception(responseResult.e)
-                is Success -> Success(responseResult.data)
-            }
+            commentDataSource.createComment(
+                newComment.toDto(),
+            ).handle { Unit }
 
         override suspend fun updateComment(
             commentId: Long,
             content: String,
-        ): ApiResult<Unit> {
-            val responseResult =
-                commentDataSource.updateComment(
-                    commentId,
-                    CommentUpdateRequest(content),
-                )
-            return when (responseResult) {
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Exception -> Exception(responseResult.e)
-                is Success -> Success(responseResult.data)
-            }
-        }
+        ): ApiResult<Unit> =
+            commentDataSource.updateComment(
+                commentId,
+                CommentUpdateRequest(content),
+            ).handle { Unit }
 
-        override suspend fun deleteComment(commentId: Long): ApiResult<Unit> =
-            when (val responseResult = commentDataSource.deleteComment(commentId)) {
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Exception -> Exception(responseResult.e)
-                is Success -> Success(responseResult.data)
-            }
+        override suspend fun deleteComment(commentId: Long): ApiResult<Unit> = commentDataSource.deleteComment(commentId).handle { Unit }
     }
