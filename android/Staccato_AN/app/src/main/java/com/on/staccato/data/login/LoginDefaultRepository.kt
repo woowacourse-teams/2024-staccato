ca5b48ea098a5b855f6d60f759cc60fc3e522938
hxeyexn <kck85075@kumoh.ac.kr>
2024-12-08 02:59:31 +0900

    refactor: 로그인 리포지터리 ApiResult 처리 확장 함수 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/login/LoginDefaultRepository.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/login/LoginDefaultRepository.kt
@@ -1,9 +1,7 @@
 package com.on.staccato.data.login
 
 import com.on.staccato.data.ApiResult
-import com.on.staccato.data.Exception
-import com.on.staccato.data.ServerError
-import com.on.staccato.data.Success
+import com.on.staccato.data.handle
 import com.on.staccato.domain.repository.LoginRepository
 import javax.inject.Inject
 
@@ -12,11 +10,6 @@ class LoginDefaultRepository
     constructor(
         private val loginDataSource: LoginDataSource,
     ) : LoginRepository {
-        override suspend fun loginWithNickname(nickname: String): ApiResult<String> {
-            return when (val result = loginDataSource.requestLoginWithNickname(nickname)) {
-                is Exception -> Exception(result.e)
-                is ServerError -> ServerError(result.status, result.message)
-                is Success -> Success(result.data.token)
-            }
-        }
+        override suspend fun loginWithNickname(nickname: String): ApiResult<String> =
+            loginDataSource.requestLoginWithNickname(nickname).handle { it.token }
     }
