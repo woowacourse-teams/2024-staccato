76d4517dbee520db5dc7cbe6b9a0144c6b918c5a
hxeyexn <kck85075@kumoh.ac.kr>
2024-12-08 03:00:17 +0900

    refactor: 이미지 리포지터리 ApiResult 처리 확장 함수 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/image/ImageDefaultRepository.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/image/ImageDefaultRepository.kt
@@ -1,10 +1,8 @@
 package com.on.staccato.data.image
 
 import com.on.staccato.data.ApiResult
-import com.on.staccato.data.Exception
-import com.on.staccato.data.ServerError
-import com.on.staccato.data.Success
 import com.on.staccato.data.dto.image.ImageResponse
+import com.on.staccato.data.handle
 import com.on.staccato.domain.repository.ImageRepository
 import okhttp3.MultipartBody
 import javax.inject.Inject
@@ -14,11 +12,6 @@ class ImageDefaultRepository
     constructor(
         private val imageApiService: ImageApiService,
     ) : ImageRepository {
-        override suspend fun convertImageFileToUrl(imageFile: MultipartBody.Part): ApiResult<ImageResponse> {
-            return when (val responseResult = imageApiService.postImage(imageFile)) {
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Success -> Success(responseResult.data)
-            }
-        }
+        override suspend fun convertImageFileToUrl(imageFile: MultipartBody.Part): ApiResult<ImageResponse> =
+            imageApiService.postImage(imageFile).handle { it }
     }
