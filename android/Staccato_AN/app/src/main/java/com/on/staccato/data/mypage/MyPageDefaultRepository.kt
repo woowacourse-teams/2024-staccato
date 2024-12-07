d5c359ceaee4ae7f68c5e3025a902f6606f01807
hxeyexn <kck85075@kumoh.ac.kr>
2024-12-08 02:51:57 +0900

    refactor: 마이페이지 리포지터리 ApiResult 처리 확장 함수 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/mypage/MyPageDefaultRepository.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/mypage/MyPageDefaultRepository.kt
@@ -1,11 +1,9 @@
 package com.on.staccato.data.mypage
 
 import com.on.staccato.data.ApiResult
-import com.on.staccato.data.Exception
-import com.on.staccato.data.ServerError
-import com.on.staccato.data.Success
 import com.on.staccato.data.dto.mapper.toDomain
 import com.on.staccato.data.dto.mypage.ProfileImageResponse
+import com.on.staccato.data.handle
 import com.on.staccato.domain.model.MemberProfile
 import com.on.staccato.domain.repository.MyPageRepository
 import okhttp3.MultipartBody
@@ -16,22 +14,12 @@ class MyPageDefaultRepository
     constructor(
         private val myPageApiService: MyPageApiService,
     ) : MyPageRepository {
-        override suspend fun getMemberProfile(): ApiResult<MemberProfile> {
-            return when (val responseResult = myPageApiService.getMemberProfile()) {
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Success -> {
-                    val myProfile = responseResult.data.toDomain()
-                    Success(myProfile)
-                }
-            }
-        }
+        override suspend fun getMemberProfile(): ApiResult<MemberProfile> = myPageApiService.getMemberProfile().handle { it.toDomain() }
 
-        override suspend fun changeProfileImage(profileImageFile: MultipartBody.Part): ApiResult<ProfileImageResponse> {
-            return when (val responseResult = myPageApiService.postProfileImageChange(profileImageFile)) {
-                is Exception -> Exception(responseResult.e)
-                is ServerError -> ServerError(responseResult.status, responseResult.message)
-                is Success -> Success(responseResult.data)
+        override suspend fun changeProfileImage(profileImageFile: MultipartBody.Part): ApiResult<ProfileImageResponse> =
+            myPageApiService.postProfileImageChange(
+                profileImageFile,
+            ).handle {
+                it
             }
-        }
     }
