36fda43464af7c740bb0cb630397f03980d42318
Somin Lee <salth0601@gmail.com>
2024-12-17 01:35:07 +0900

    refactor: MemoryCandidateTest 메서드명 개선 및 Parameters 추가

--- android/Staccato_AN/app/src/test/java/com/on/staccato/domain/model/MemoryCandidateTest.kt
+++ android/Staccato_AN/app/src/test/java/com/on/staccato/domain/model/MemoryCandidateTest.kt
@@ -1,130 +1,120 @@
 package com.on.staccato.domain.model
 
+import junitparams.JUnitParamsRunner
+import junitparams.Parameters
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertTrue
 import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDate
 
+@RunWith(JUnitParamsRunner::class)
 class MemoryCandidateTest {
-    // 아래부터 isDateWithinPeriod 테스트
     @Test
-    fun `특정 날짜가 startAt과 endAt 사이에 포함되면 true 반환`() {
+    @Parameters(method = "validLocalDateParameters")
+    fun `타겟 날짜가 기간 내면 true를 반환`(targetLocalDate: LocalDate) {
+        // given
         val category =
             makeTestMemoryCandidate(
                 startAt = yearStart2024,
                 endAt = yearEnd2024,
             )
-
-        val actual = category.isDateWithinPeriod(yearMiddle2024)
+        // when
+        val actual = category.isDateWithinPeriod(targetLocalDate)
+        // then
         assertTrue(actual)
     }
 
     @Test
-    fun `특정 날짜와 startAt이 같으면 true 반환`() {
+    fun `타겟 날짜가 기간보다 과거라면 false를 반환`() {
+        // given
         val category =
             makeTestMemoryCandidate(
                 startAt = yearStart2024,
                 endAt = yearEnd2024,
             )
-
-        val actual = category.isDateWithinPeriod(yearStart2024)
-        assertTrue(actual)
-    }
-
-    @Test
-    fun `특정 날짜와 endAt이 같으면 true 반환`() {
-        val category =
-            makeTestMemoryCandidate(
-                startAt = yearStart2024,
-                endAt = yearEnd2024,
-            )
-
-        val actual = category.isDateWithinPeriod(yearEnd2024)
-        assertTrue(actual)
-    }
-
-    @Test
-    fun `특정 날짜가 startAt 이전이면 false 반환`() {
-        val category =
-            makeTestMemoryCandidate(
-                startAt = yearStart2024,
-                endAt = yearEnd2024,
-            )
-
-        val actual = category.isDateWithinPeriod(yearEnd2023)
+        val targetLocalDate = yearEnd2023
+        // when
+        val actual = category.isDateWithinPeriod(targetLocalDate)
+        // then
         assertFalse(actual)
     }
 
     @Test
-    fun `특정 날짜가 endAt 이후면 false 반환`() {
+    fun `타겟 날짜가 기간보다 미래라면 false를 반환`() {
+        // given
         val category =
             makeTestMemoryCandidate(
                 startAt = yearStart2024,
                 endAt = yearEnd2024,
             )
-
-        val actual = category.isDateWithinPeriod(yearStart2025)
+        val targetLocalDate = yearStart2025
+        // when
+        val actual = category.isDateWithinPeriod(targetLocalDate)
+        // then
         assertFalse(actual)
     }
 
     @Test
-    fun `startAt과 endAt이 null이면 모든 날짜에 true 반환`() {
+    @Parameters(method = "localDateParameters")
+    fun `기간 없는 카테고리는 모든 날짜에 true를 반환`(date: LocalDate) {
         // given
         val categoryWithoutPeriod =
             makeTestMemoryCandidate(
                 startAt = null,
                 endAt = null,
             )
-
-        // when & then
-        val actual1 = categoryWithoutPeriod.isDateWithinPeriod(yearEnd2023)
-        val actual2 = categoryWithoutPeriod.isDateWithinPeriod(yearMiddle2024)
-        val actual3 = categoryWithoutPeriod.isDateWithinPeriod(yearStart2025)
-
-        assertTrue(actual1)
-        assertTrue(actual2)
-        assertTrue(actual3)
-    }
-
-    // 아래부터 getClosestDateTime 테스트
-    @Test
-    fun `현재 시간이 startAt과 endAt의 범위 내일 때 현재 시간을 반환`() {
-        // given
-        val category =
-            makeTestMemoryCandidate(memoryId = 2L, startAt = yearStart2024, endAt = yearEnd2024)
-        val currentLocalDateTime = yearMiddle2024.atTime(13, 30)
-
-        // when & then
-        val actual = category.getClosestDateTime(currentLocalDateTime)
-
-        assertEquals(currentLocalDateTime, actual)
+        // when
+        val actual = categoryWithoutPeriod.isDateWithinPeriod(date)
+        // then
+        assertTrue(actual)
     }
 
     @Test
-    fun `현재 시간이 startAt보다 과거일 때 startAt의 정오를 반환`() {
+    fun `타겟 일시가 기간 범위 내면 그대로 반환`() {
         // given
         val category =
             makeTestMemoryCandidate(memoryId = 2L, startAt = yearStart2024, endAt = yearEnd2024)
-        val currentLocalDateTime = yearEnd2023.atTime(13, 30)
+        val targetLocalDateTime = yearMiddle2024.atTime(13, 30)
+        // when
+        val actual = category.getClosestDateTime(targetLocalDateTime)
+        // then
+        assertEquals(targetLocalDateTime, actual)
+    }
 
-        // when & then
-        val actual = category.getClosestDateTime(currentLocalDateTime)
+    @Test
+    fun `타겟 일시가 기간보다 과거라면 시작일의 정오를 반환`() {
+        // given
+        val category =
+            makeTestMemoryCandidate(memoryId = 2L, startAt = yearStart2024, endAt = yearEnd2024)
+        val targetLocalDateTime = yearEnd2023.atTime(13, 30)
+        // when
+        val actual = category.getClosestDateTime(targetLocalDateTime)
         val expected = yearStart2024.atTime(12, 0)
-
+        // then
         assertEquals(expected, actual)
     }
 
     @Test
-    fun `현재 시간이 endAt보다 미래일 때 endAt의 정오를 반환`() {
+    fun `타겟 일시가 기간보다 미래라면 종료일의 정오를 반환`() {
         // given
         val category =
             makeTestMemoryCandidate(memoryId = 2L, startAt = yearStart2024, endAt = yearEnd2024)
-        val currentLocalDateTime = yearStart2025.atTime(13, 30)
-
-        // when & then
-        val actual = category.getClosestDateTime(currentLocalDateTime)
+        val targetLocalDateTime = yearStart2025.atTime(13, 30)
+        // when
+        val actual = category.getClosestDateTime(targetLocalDateTime)
         val expected = yearEnd2024.atTime(12, 0)
-
+        // then
         assertEquals(expected, actual)
     }
+
+    private fun validLocalDateParameters(): List<LocalDate> = listOf(yearStart2024, yearMiddle2024, yearEnd2024)
+
+    private fun localDateParameters(): List<LocalDate> =
+        listOf(
+            LocalDate.now(),
+            LocalDate.now().minusYears(10),
+            LocalDate.now().plusYears(10),
+        )
 }
