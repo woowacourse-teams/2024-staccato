93266704d1bf4abfc7fd92f514775ba74c99d0eb
BurningFalls <sherlocky31415@gmail.com>
2024-12-30 17:56:02 +0900

    refactor: GetMapping 경로에서 index 삭제

--- backend/src/main/java/com/staccato/web/HomeController.java
+++ backend/src/main/java/com/staccato/web/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping({"/", "/index"})
+    @GetMapping("/")
     public String index() {
         return "index";
     }
