53d84c7ef299f6427fdd0c57dd7fd775e175dc64
Somin Lee <salth0601@gmail.com>
2025-01-03 13:40:16 +0900

    feat: DateCalendar 클래스 public 함수 및 유효성 검사 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/DateCalendar.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/DateCalendar.kt
@@ -3,7 +3,9 @@ package com.on.staccato.domain.model
 import java.time.LocalDate
 import java.time.YearMonth
 
-data class DateCalendar(private val availableDates: List<Int>) {
+data class DateCalendar private constructor(val availableDates: List<Int>) {
+    fun getClosestDate(target: Int): Int = target.coerceIn(availableDates.first(), availableDates.last())
+
     companion object {
         private const val FIRST_DATE = 1
 
@@ -13,9 +15,28 @@ data class DateCalendar(private val availableDates: List<Int>) {
             periodStart: LocalDate? = null,
             periodEnd: LocalDate? = null,
         ): DateCalendar {
+            if (periodStart != null && periodEnd != null) checkValid(year, month, periodStart, periodEnd)
             val dateRange = createDateRangeIn(year, month, periodStart, periodEnd)
             return DateCalendar(dateRange)
         }
+
+        private fun checkValid(
+            year: Int,
+            month: Int,
+            periodStart: LocalDate,
+            periodEnd: LocalDate,
+        ) {
+            val startDate = LocalDate.of(year, month, periodStart.dayOfMonth)
+            val endDate = LocalDate.of(year, month, periodEnd.dayOfMonth)
+            val isBetweenPeriod = periodStart.isBeforeOrEqual(startDate) && periodEnd.isAfterOrEqual(endDate)
+            val isValidPeriod = periodStart.isBeforeOrEqual(periodEnd)
+            require(isBetweenPeriod) { IllegalArgumentException("${year}년 ${month}월은 $periodStart $periodEnd 사이여야 합니다.") }
+            require(isValidPeriod) { IllegalArgumentException("$year $month $periodStart $periodEnd 기간이 유효하지 않습니다.") }
+        }
+
+        private fun LocalDate.isAfterOrEqual(target: LocalDate?) = (isAfter(target) || isEqual(target))
+
+        private fun LocalDate.isBeforeOrEqual(target: LocalDate?) = (isBefore(target) || isEqual(target))
 
         private fun createDateRangeIn(
             year: Int,
@@ -39,8 +60,19 @@ data class DateCalendar(private val availableDates: List<Int>) {
             periodEnd: LocalDate,
         ): List<Int> =
             when {
-                isInSameMonth(year, month, periodStart, periodEnd) -> createDatesBetween(periodStart.dayOfMonth, periodEnd.dayOfMonth)
-                isStartMonth(year, month, periodStart) -> createDatesBetween(periodStart.dayOfMonth, lastDateOfMonth)
+                isInSameMonth(
+                    year,
+                    month,
+                    periodStart,
+                    periodEnd,
+                ) -> createDatesBetween(periodStart.dayOfMonth, periodEnd.dayOfMonth)
+
+                isStartMonth(year, month, periodStart) ->
+                    createDatesBetween(
+                        periodStart.dayOfMonth,
+                        lastDateOfMonth,
+                    )
+
                 isEndMonth(year, month, periodEnd) -> createDatesBetween(1, periodEnd.dayOfMonth)
                 else -> createDatesBetween(1, lastDateOfMonth)
             }
