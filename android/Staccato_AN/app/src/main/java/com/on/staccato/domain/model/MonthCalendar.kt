ee0df20ec9809adb4a4f6018263aa4934106ebb1
Somin Lee <salth0601@gmail.com>
2025-01-03 16:48:28 +0900

    feat: MonthCalendar 클래스 public 함수 및 유효성 검사 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/MonthCalendar.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/MonthCalendar.kt
@@ -3,7 +3,19 @@ package com.on.staccato.domain.model
 import com.on.staccato.domain.model.DateCalendar.Companion.of
 import java.time.LocalDate
 
-data class MonthCalendar(private val monthToDateCalendar: Map<Int, DateCalendar>) {
+data class MonthCalendar private constructor(private val monthToDateCalendar: Map<Int, DateCalendar>) {
+    fun getAvailableMonths(): List<Int> = monthToDateCalendar.keys.toList()
+
+    fun getAvailableDates(selectedMonth: Int): List<Int> =
+        monthToDateCalendar[selectedMonth]?.availableDates ?: throw IllegalArgumentException()
+
+    fun getClosestMonth(targetMonth: Int): Int {
+        val availableMonths = getAvailableMonths()
+        return targetMonth.coerceIn(availableMonths.first(), availableMonths.last())
+    }
+
+    fun findDateCalendar(selectedMonth: Int): DateCalendar? = monthToDateCalendar[selectedMonth]
+
     companion object {
         private const val JANUARY = 1
         private const val DECEMBER = 12
@@ -14,6 +26,7 @@ data class MonthCalendar(private val monthToDateCalendar: Map<Int, DateCalendar>
             periodStart: LocalDate? = null,
             periodEnd: LocalDate? = null,
         ): MonthCalendar {
+            if (periodStart != null && periodEnd != null) checkValid(year, periodStart, periodEnd)
             val monthRange = createMonthRangeIn(year, periodStart, periodEnd)
             return MonthCalendar(
                 monthRange.associateWith { month ->
@@ -21,6 +34,19 @@ data class MonthCalendar(private val monthToDateCalendar: Map<Int, DateCalendar>
                 },
             )
         }
+
+        private fun checkValid(
+            year: Int,
+            periodStart: LocalDate,
+            periodEnd: LocalDate,
+        ) {
+            val isBetweenPeriod = (year in periodStart.year..periodEnd.year)
+            val isValidPeriod = periodStart.isBeforeOrEqual(periodEnd)
+            require(isBetweenPeriod) { IllegalArgumentException("${year}년은 ${periodStart.year}년과 ${periodEnd.year}년 사이여야 합니다.") }
+            require(isValidPeriod) { IllegalArgumentException("시작 날짜 ${periodStart}는 종료 날짜 ${periodEnd}보다 같거나 빨라야 합니다.") }
+        }
+
+        private fun LocalDate.isBeforeOrEqual(target: LocalDate?) = (isBefore(target) || isEqual(target))
 
         private fun createMonthRangeIn(
             year: Int,
@@ -39,8 +65,18 @@ data class MonthCalendar(private val monthToDateCalendar: Map<Int, DateCalendar>
             periodEnd: LocalDate,
         ): List<Int> =
             when {
-                isInSameYear(year, periodStart, periodEnd) -> createMonthsBetween(periodStart.monthValue, periodEnd.monthValue)
-                isStartYear(year, periodStart) -> createMonthsBetween(periodStart.monthValue, DECEMBER)
+                isInSameYear(
+                    year,
+                    periodStart,
+                    periodEnd,
+                ) -> createMonthsBetween(periodStart.monthValue, periodEnd.monthValue)
+
+                isStartYear(year, periodStart) ->
+                    createMonthsBetween(
+                        periodStart.monthValue,
+                        DECEMBER,
+                    )
+
                 isEndYear(year, periodEnd) -> createMonthsBetween(JANUARY, periodEnd.monthValue)
                 else -> fullMonths
             }
