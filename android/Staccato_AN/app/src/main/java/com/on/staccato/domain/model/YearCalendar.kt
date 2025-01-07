f3214d7e577b93b9b0cd4817bfffc22899373e70
Somin Lee <salth0601@gmail.com>
2025-01-07 14:18:08 +0900

    feat: YearCalendar 날짜 선택을 위한 프로퍼티와 메서드 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/YearCalendar.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/YearCalendar.kt
@@ -2,8 +2,72 @@ package com.on.staccato.domain.model
 
 import com.on.staccato.domain.model.MonthCalendar.Companion.of
 import java.time.LocalDate
+import java.time.LocalDateTime
 
-data class YearCalendar(private val yearToMonthCalender: Map<Int, MonthCalendar>) {
+data class YearCalendar private constructor(private val yearToMonthCalender: Map<Int, MonthCalendar>) {
+    var selectedYear: Int = yearToMonthCalender.keys.first()
+        private set
+    var selectedMonth: Int = getAvailableMonths().first()
+        private set
+    var selectedDate: Int = getAvailableDates().first()
+        private set
+    var selectedHour: Int = DEFAULT_HOUR
+        private set
+
+    var selectedDateTime: LocalDateTime = LocalDateTime.of(selectedYear, selectedMonth, selectedDate, selectedHour, DEFAULT_MINUTE)
+        get() = LocalDateTime.of(selectedYear, selectedMonth, selectedDate, selectedHour, DEFAULT_MINUTE)
+        private set
+
+    fun getAvailableYears(): List<Int> = yearToMonthCalender.keys.toList()
+
+    fun getAvailableMonths(): List<Int> = yearToMonthCalender[selectedYear]?.getAvailableMonths() ?: throw IllegalArgumentException()
+
+    fun getAvailableDates(): List<Int> =
+        yearToMonthCalender[selectedYear]?.getAvailableDates(selectedMonth)
+            ?: throw IllegalArgumentException()
+
+    fun initSelectedDateTime(dateTime: LocalDateTime) {
+        selectedYear = dateTime.year
+        selectedMonth = dateTime.monthValue
+        selectedDate = dateTime.dayOfMonth
+        selectedHour = dateTime.hour
+    }
+
+    fun changeSelectedYear(newYear: Int) {
+        selectedYear = newYear
+        resetSelectedMonths()
+    }
+
+    fun changeSelectedMonth(newMonth: Int) {
+        selectedMonth = newMonth
+        resetSelectedDates()
+    }
+
+    fun changeSelectedDate(newDate: Int) {
+        selectedDate = newDate
+    }
+
+    fun changeSelectedHour(newHour: Int) {
+        selectedHour = newHour
+    }
+
+    private fun resetSelectedMonths() {
+        findMonthCalendar()?.let { newMonthCalender ->
+            selectedMonth = newMonthCalender.getClosestMonth(selectedMonth)
+            resetSelectedDates()
+        }
+    }
+
+    private fun resetSelectedDates() {
+        findDateCalendar()?.let { newDateCalender ->
+            selectedDate = newDateCalender.getClosestDate(selectedDate)
+        }
+    }
+
+    private fun findMonthCalendar(): MonthCalendar? = yearToMonthCalender[selectedYear]
+
+    private fun findDateCalendar(): DateCalendar? = findMonthCalendar()?.findDateCalendar(selectedMonth)
+
     companion object {
         private const val DEFAULT_HOUR: Int = 12
         private const val DEFAULT_MINUTE: Int = 0
