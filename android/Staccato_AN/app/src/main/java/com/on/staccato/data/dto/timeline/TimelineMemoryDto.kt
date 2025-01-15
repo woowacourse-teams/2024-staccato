5391232652a657d78ec7546efb98b472af269088
Junyoung-WON <wjyo60@gmail.com>
2025-01-16 02:05:06 +0900

    refactor: TimelineMemoryDto의 SerialName 값 변경
    - memoryId -> categoryId
    - memoryTitle -> categoryTitle
    - memoryThumbnailUrl -> categoryThumbnailUrl

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/timeline/TimelineMemoryDto.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/timeline/TimelineMemoryDto.kt
@@ -5,9 +5,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class TimelineMemoryDto(
-    @SerialName("memoryId") val memoryId: Long,
-    @SerialName("memoryTitle") val memoryTitle: String,
-    @SerialName("memoryThumbnailUrl") val memoryThumbnailUrl: String? = null,
+    @SerialName("categoryId") val memoryId: Long,
+    @SerialName("categoryTitle") val memoryTitle: String,
+    @SerialName("categoryThumbnailUrl") val memoryThumbnailUrl: String? = null,
     @SerialName("startAt") val startAt: String? = null,
     @SerialName("endAt") val endAt: String? = null,
     @SerialName("description") val description: String? = null,
