25e63e9a297b38c9303e1148786cf482815616cd
Junyoung-WON <wjyo60@gmail.com>
2025-01-16 01:05:11 +0900

    refactor: StaccatoCreationRequest의 SerialName 변경
    - memoryId -> categoryId

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/staccato/StaccatoCreationRequest.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/staccato/StaccatoCreationRequest.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class StaccatoCreationRequest(
     @SerialName("staccatoTitle") val staccatoTitle: String,
-    @SerialName("memoryId") val memoryId: Long,
+    @SerialName("categoryId") val memoryId: Long,
     @SerialName("placeName") val placeName: String,
     @SerialName("latitude") val latitude: Double,
     @SerialName("longitude") val longitude: Double,
