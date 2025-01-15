d14a6c13a66945876b10b7edde716b5d54fa75d7
linirini <2001yerin@naver.com>
2025-01-16 02:23:37 +0900

    refactor: updatedAt에 대한 setter 메서드만 작성

--- backend/src/main/java/com/staccato/config/domain/BaseEntity.java
+++ backend/src/main/java/com/staccato/config/domain/BaseEntity.java
@@ -7,15 +7,18 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import lombok.Getter;
-import lombok.Setter;
 
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 @Getter
-@Setter
 public abstract class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
+
     @LastModifiedDate
     private LocalDateTime updatedAt;
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
 }
