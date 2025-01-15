14c7832d37295304d633486f0b7afbd0e196cdda
Junyoung-WON <wjyo60@gmail.com>
2025-01-16 01:37:25 +0900

    refactor: MemoryCandidates findBy 메서드의 파라미터명 변경
    - memoryId -> categoryId

--- android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/MemoryCandidates.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/domain/model/MemoryCandidates.kt
@@ -5,7 +5,7 @@ import java.time.LocalDate
 data class MemoryCandidates(val categoryCandidates: List<CategoryCandidate>) {
     fun filterBy(date: LocalDate): MemoryCandidates = copy(categoryCandidates = categoryCandidates.filter { it.isDateWithinPeriod(date) })
 
-    fun findBy(memoryId: Long): CategoryCandidate? = categoryCandidates.find { it.categoryId == memoryId }
+    fun findBy(categoryId: Long): CategoryCandidate? = categoryCandidates.find { it.categoryId == categoryId }
 
     fun findByIdOrFirst(targetId: Long?): CategoryCandidate? =
         categoryCandidates.find { it.categoryId == targetId }
