afb1addc9f45f32d4cb45b69479fe256c9dea41a
Somin Lee <salth0601@gmail.com>
2025-01-16 22:37:42 +0900

    feat: UserInfoPreferencesManager에 프로필 사진, 닉네임, 복구 코드 관련 로직 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/UserInfoPreferencesManager.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/UserInfoPreferencesManager.kt
@@ -2,6 +2,7 @@ package com.on.staccato.data
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.on.staccato.domain.model.MemberProfile
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
@@ -11,7 +12,32 @@ class UserInfoPreferencesManager(context: Context) {
 
     suspend fun getToken(): String? {
         return withContext(Dispatchers.IO) {
-            mUserInfoPrefs.getString(TOKEN_KEY_NAME, "")
+            mUserInfoPrefs.getString(TOKEN_KEY_NAME, EMPTY_STRING)
+        }
+    }
+
+    suspend fun getMemberProfile(): MemberProfile =
+        MemberProfile(
+            profileImageUrl = getProfileImageUrl() ?: EMPTY_STRING,
+            nickname = getNickname() ?: EMPTY_STRING,
+            uuidCode = getRecoveryCode() ?: EMPTY_STRING,
+        )
+
+    private suspend fun getProfileImageUrl(): String? {
+        return withContext(Dispatchers.IO) {
+            mUserInfoPrefs.getString(PROFILE_IMAGE_URL_KEY_NAME, EMPTY_STRING)
+        }
+    }
+
+    private suspend fun getNickname(): String? {
+        return withContext(Dispatchers.IO) {
+            mUserInfoPrefs.getString(NICKNAME_KEY_NAME, null)
+        }
+    }
+
+    private suspend fun getRecoveryCode(): String? {
+        return withContext(Dispatchers.IO) {
+            mUserInfoPrefs.getString(RECOVERY_CODE_KEY_NAME, EMPTY_STRING)
         }
     }
 
@@ -21,8 +47,36 @@ class UserInfoPreferencesManager(context: Context) {
         }
     }
 
+    suspend fun setMemberProfile(memberProfile: MemberProfile) {
+        setProfileImageUrl(memberProfile.profileImageUrl ?: "")
+        setNickname(memberProfile.nickname)
+        setRecoveryCode(memberProfile.uuidCode)
+    }
+
+    suspend fun setProfileImageUrl(url: String) {
+        withContext(Dispatchers.IO) {
+            mUserInfoPrefs.edit().putString(PROFILE_IMAGE_URL_KEY_NAME, url).apply()
+        }
+    }
+
+    private suspend fun setNickname(nickname: String) {
+        withContext(Dispatchers.IO) {
+            mUserInfoPrefs.edit().putString(NICKNAME_KEY_NAME, nickname).apply()
+        }
+    }
+
+    private suspend fun setRecoveryCode(code: String) {
+        withContext(Dispatchers.IO) {
+            mUserInfoPrefs.edit().putString(RECOVERY_CODE_KEY_NAME, code).apply()
+        }
+    }
+
     companion object {
         private const val USER_INFO_PREF_NAME = "com.on.staccato.user_info_prefs"
         private const val TOKEN_KEY_NAME = "com.on.staccato.token"
+        private const val PROFILE_IMAGE_URL_KEY_NAME = "com.on.staccato.profile"
+        private const val NICKNAME_KEY_NAME = "com.on.staccato.nickname"
+        private const val RECOVERY_CODE_KEY_NAME = "com.on.staccato.recovery"
+        const val EMPTY_STRING = ""
     }
 }
