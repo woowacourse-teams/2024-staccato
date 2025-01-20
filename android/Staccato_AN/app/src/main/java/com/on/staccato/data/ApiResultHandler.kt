a554e09ae61809009f5c6cdf0043263f32d55b1f
hxeyexn <kck85075@kumoh.ac.kr>
2025-01-20 16:07:50 +0900

    refactor: ApiResult<T> 확장함수 inline 키워드 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/ApiResultHandler.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/ApiResultHandler.kt
@@ -3,17 +3,17 @@ package com.on.staccato.data
 import com.on.staccato.data.dto.Status
 import com.on.staccato.presentation.util.ExceptionState
 
-suspend fun <T : Any> ApiResult<T>.onSuccess(executable: suspend (T) -> Unit): ApiResult<T> =
+inline fun <T : Any> ApiResult<T>.onSuccess(executable: (T) -> Unit): ApiResult<T> =
     apply {
         if (this is Success<T>) executable(data)
     }
 
-suspend fun <T : Any> ApiResult<T>.onServerError(executable: suspend (status: Status, message: String) -> Unit): ApiResult<T> =
+inline fun <T : Any> ApiResult<T>.onServerError(executable: (status: Status, message: String) -> Unit): ApiResult<T> =
     apply {
         if (this is ServerError<T>) executable(status, message)
     }
 
-suspend fun <T : Any> ApiResult<T>.onException(executable: suspend (exceptionState: ExceptionState) -> Unit): ApiResult<T> =
+inline fun <T : Any> ApiResult<T>.onException(executable: (exceptionState: ExceptionState) -> Unit): ApiResult<T> =
     apply {
         if (this is Exception<T>) {
             when (this) {
