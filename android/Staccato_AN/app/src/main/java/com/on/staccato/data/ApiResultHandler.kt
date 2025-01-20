5db7be4a70ea8f171e7a2d8603c8601f10fcdf10
hxeyexn <kck85075@kumoh.ac.kr>
2025-01-20 16:20:47 +0900

    refactor: ApiResult<T> 성공, 서버에러, 예외 별 처리 확장함수 파라미터명 변경
    - 이전: executable
    - 이후: action

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/ApiResultHandler.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/ApiResultHandler.kt
@@ -3,22 +3,22 @@ package com.on.staccato.data
 import com.on.staccato.data.dto.Status
 import com.on.staccato.presentation.util.ExceptionState
 
-inline fun <T : Any> ApiResult<T>.onSuccess(executable: (T) -> Unit): ApiResult<T> =
+inline fun <T : Any> ApiResult<T>.onSuccess(action: (T) -> Unit): ApiResult<T> =
     apply {
-        if (this is Success<T>) executable(data)
+        if (this is Success<T>) action(data)
     }
 
-inline fun <T : Any> ApiResult<T>.onServerError(executable: (status: Status, message: String) -> Unit): ApiResult<T> =
+inline fun <T : Any> ApiResult<T>.onServerError(action: (status: Status, message: String) -> Unit): ApiResult<T> =
     apply {
-        if (this is ServerError<T>) executable(status, message)
+        if (this is ServerError<T>) action(status, message)
     }
 
-inline fun <T : Any> ApiResult<T>.onException(executable: (exceptionState: ExceptionState) -> Unit): ApiResult<T> =
+inline fun <T : Any> ApiResult<T>.onException(action: (exceptionState: ExceptionState) -> Unit): ApiResult<T> =
     apply {
         if (this is Exception<T>) {
             when (this) {
-                is Exception.NetworkError -> executable(ExceptionState.NetworkError)
-                is Exception.UnknownError -> executable(ExceptionState.UnknownError)
+                is Exception.NetworkError -> action(ExceptionState.NetworkError)
+                is Exception.UnknownError -> action(ExceptionState.UnknownError)
             }
         }
     }
