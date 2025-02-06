19b491326f9a6f0800972d4b4777a8e7991d5780
yoonjuho <dbswn990@naver.com>
2025-02-06 16:55:40 +0900

    refactor: 비즈니스 매트릭 수집 기준 변경

--- backend/src/main/java/com/staccato/config/log/CreateMomentMetricsAspect.java
+++ backend/src/main/java/com/staccato/config/log/CreateMomentMetricsAspect.java
@@ -1,12 +1,11 @@
 package com.staccato.config.log;
 
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
-import com.staccato.member.domain.Member;
 import com.staccato.moment.service.MomentService;
 import com.staccato.moment.service.dto.request.MomentRequest;
 import io.micrometer.core.instrument.Counter;
@@ -17,39 +16,34 @@ import lombok.RequiredArgsConstructor;
 @Component
 @RequiredArgsConstructor
 public class CreateMomentMetricsAspect {
-    private static final long TIME_THRESHOLD_MINUTES = 30;
 
     private final MeterRegistry meterRegistry;
 
-    @Pointcut("execution(public * com.staccato.moment.service.MomentService.createMoment(..)) && args(momentRequest, member)")
-    public void createMomentPointcut(MomentRequest momentRequest, Member member) {
+    @Pointcut("execution(public * com.staccato.moment.service.MomentService.createMoment(..)) && args(momentRequest)")
+    public void createMomentPointcut(MomentRequest momentRequest) {
     }
 
-    @AfterReturning(pointcut = "createMomentPointcut(momentRequest, member)", returning = "result")
-    public void afterSuccessfulCreateMoment(MomentRequest momentRequest, Member member, Object result) {
-        LocalDateTime visitedAt = momentRequest.visitedAt();
-        LocalDateTime now = LocalDateTime.now();
-        if (!isPastDateTime(visitedAt, now) && !isFutureDateTime(visitedAt, now)) {
-            recordCounter("now");
-            return;
-        }
-        checkViewPoint(visitedAt, now);
-    }
-
-    private void checkViewPoint(LocalDateTime visitedAt, LocalDateTime now) {
-        if (isPastDateTime(visitedAt, now)) {
+    @AfterReturning(pointcut = "createMomentPointcut(momentRequest)")
+    public void afterSuccessfulCreateMoment(MomentRequest momentRequest) {
+        LocalDate visitedAt = momentRequest.visitedAt().toLocalDate();
+        LocalDate now = LocalDate.now();
+        if (isPastDate(visitedAt, now)) {
             recordCounter("past");
             return;
         }
-        recordCounter("future");
+        if (isFutureDate(visitedAt, now)) {
+            recordCounter("future");
+            return;
+        }
+        recordCounter("now");
     }
 
-    private boolean isFutureDateTime(LocalDateTime visitedAt, LocalDateTime now) {
-        return visitedAt.isAfter(now.plusMinutes(TIME_THRESHOLD_MINUTES));
+    private boolean isPastDate(LocalDate visitedAt, LocalDate now) {
+        return visitedAt.isBefore(now);
     }
 
-    private boolean isPastDateTime(LocalDateTime visitedAt, LocalDateTime now) {
-        return visitedAt.isBefore(now.minusMinutes(TIME_THRESHOLD_MINUTES));
+    private boolean isFutureDate(LocalDate visitedAt, LocalDate now) {
+        return visitedAt.isAfter(now);
     }
 
     private void recordCounter(String viewPoint) {
