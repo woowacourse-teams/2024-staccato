7f71687f005a188a0d48bc2fa25bfe9236cfdce6
BurningFalls <sherlocky31415@gmail.com>
2025-02-12 22:43:32 +0900

    refactor: 주석 삭제

--- backend/src/main/resources/static/js/share.js
+++ backend/src/main/resources/static/js/share.js
@@ -1,4 +1,3 @@
-// JSON 데이터 가져오기 및 처리
 fetch('/staccatos/share')
     .then(response => response.json())
     .then(data => {
@@ -14,7 +13,6 @@ fetch('/staccatos/share')
             comments,
         } = data;
 
-        // Header 섹션 업데이트
         document.querySelector('.memory-title').innerText = `${userName}님이 공유한 추억`;
 
         const expiredDate = new Date(expiredAt);
@@ -24,9 +22,8 @@ fetch('/staccatos/share')
         const formattedExpiredAt = `${expiredYear}년 ${expiredMonth}월 ${expiredDay}일`;
         document.querySelector('.memory-subtitle').innerText = `${formattedExpiredAt}까지 열람할 수 있어요!`;
 
-        // Image Slider 섹션 업데이트
         const sliderWrapper = document.querySelector('.swiper-wrapper');
-        sliderWrapper.innerHTML = ''; // 초기화
+        sliderWrapper.innerHTML = '';
         momentImageUrls.forEach((url) => {
             const slideElement = document.createElement('div');
             slideElement.classList.add('swiper-slide');
@@ -34,16 +31,12 @@ fetch('/staccatos/share')
             sliderWrapper.appendChild(slideElement);
         });
 
-        // Swiper 업데이트
         swiper.update();
 
-        // 화살표 상태 초기화
         updateNavigationButtons(swiper);
 
-        // Title 섹션 업데이트
         document.querySelector('.title h1').innerText = staccatoTitle;
 
-        // Place and Date 섹션 업데이트
         document.querySelector('.details .place-name').innerText = placeName;
         document.querySelector('.details .address').innerText = address;
 
@@ -54,7 +47,6 @@ fetch('/staccatos/share')
         const formattedVisitedAt = `${visitedYear}년 ${visitedMonth}월 ${visitedDay}일`;
         document.querySelector('.details .visited-at').innerText = `${formattedVisitedAt}에 방문했어요`;
 
-        // 고정된 경로 템플릿 정의
         const emojiPaths = {
             happy: "https://image.staccato.kr/web/share/happy",
             angry: "https://image.staccato.kr/web/share/angry",
@@ -63,11 +55,9 @@ fetch('/staccatos/share')
             excited: "https://image.staccato.kr/web/share/excited"
         };
 
-        // Feeling 섹션 업데이트
         const emojisContainer = document.querySelector('.feeling .emojis');
-        emojisContainer.innerHTML = ''; // 초기화
+        emojisContainer.innerHTML = '';
 
-        // 각 감정에 해당하는 이미지를 추가
         Object.keys(emojiPaths).forEach((key) => {
             const emojiImg = document.createElement('img');
             emojiImg.src = key === feeling ? `${emojiPaths[key]}.png` : `${emojiPaths[key]}-gray.png`;
@@ -76,15 +66,13 @@ fetch('/staccatos/share')
             emojisContainer.appendChild(emojiImg);
         });
 
-        // Comments 섹션 업데이트
         const commentsContainer = document.querySelector('.comments-list');
-        commentsContainer.innerHTML = ''; // 초기화
+        commentsContainer.innerHTML = '';
         comments.forEach((comment) => {
             const commentElement = document.createElement('div');
             commentElement.classList.add('comment');
             commentElement.setAttribute('data-nickname', comment.nickname);
 
-            // 댓글 정렬 클래스 추가
             const commentAlignmentClass = comment.nickname === userName ? 'comment-right' : 'comment-left';
             const nicknameAlignmentClass = comment.nickname === userName ? 'nickname-right' : 'nickname-left';
             const contentWrapperAlignmentClass = comment.nickname === userName ? 'margin-right-auto' : 'margin-left-auto';
@@ -107,14 +95,12 @@ fetch('/staccatos/share')
             commentsContainer.appendChild(commentElement);
         });
 
-        // 휠 스크롤 방지 설정
         setupWheelScrollPrevention();
     })
     .catch((error) => {
         console.error('Error fetching data:', error);
     });
 
-// Swiper 설정
 const swiper = new Swiper('.swiper', {
     loop: false,
     speed: 500,
@@ -127,20 +113,19 @@ const swiper = new Swiper('.swiper', {
         clickable: true,
     },
     mousewheel: {
-        forceToAxis: true, // 가로 또는 세로 축으로 제한
-        sensitivity: 1, // 마우스 휠 민감도
+        forceToAxis: true,
+        sensitivity: 1,
     },
     on: {
         init: function () {
-            updateNavigationButtons(this); // 초기화 시 화살표 상태 업데이트
+            updateNavigationButtons(this);
         },
         slideChange: function () {
-            updateNavigationButtons(this); // 슬라이드 변경 시 화살표 상태 업데이트
+            updateNavigationButtons(this);
         },
     },
 });
 
-// 화살표 상태 업데이트 함수
 function updateNavigationButtons(swiper) {
     const prevButton = document.querySelector('.swiper-button-prev');
     const nextButton = document.querySelector('.swiper-button-next');
@@ -150,21 +135,17 @@ function updateNavigationButtons(swiper) {
         return;
     }
 
-    // 슬라이드가 없는 경우 화살표 숨김
     if (swiper.slides.length <= 1) {
         prevButton.style.display = 'none';
         nextButton.style.display = 'none';
         return;
     }
 
-    // 첫 번째 슬라이드일 경우 이전 버튼 숨김
     prevButton.style.display = swiper.isBeginning ? 'none' : 'block';
 
-    // 마지막 슬라이드일 경우 다음 버튼 숨김
     nextButton.style.display = swiper.isEnd ? 'none' : 'block';
 }
 
-// Swiper 내부 마우스 휠 동작 방지
 function setupWheelScrollPrevention() {
     const swiperContainer = document.querySelector('.swiper');
 
