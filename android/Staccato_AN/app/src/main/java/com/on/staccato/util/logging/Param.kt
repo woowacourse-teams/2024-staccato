aaba28eeb10287c53aad49f9bd8dfc74f10d1321
Junyoung-WON <wjyo60@gmail.com>
2025-02-13 19:25:39 +0900

    refactor: 프래그먼트 로깅 전략 제거에 따라 관련 변수 제거
    AnalyticsEvent
    - NAME_FRAGMENT_PAGE 제거
    
    Param
    - KEY_FRAGMENT_NAME 제거
    - PARAM_CATEGORY_LIST 제거
    - PARAM_CATEGORY_FRAGMENT 제거
    - PARAM_STACCATO_FRAGMENT 제거

--- android/Staccato_AN/app/src/main/java/com/on/staccato/util/logging/Param.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/util/logging/Param.kt
@@ -14,11 +14,6 @@ data class Param<T : Any>(val key: String, val value: T) {
         const val KEY_IS_VIEWED_BY_MARKER = "마커 클릭으로 조회된 스타카토인가?"
         const val KEY_IS_GALLERY = "갤러리에서 사진을 첨부했나?"
 
-        const val KEY_FRAGMENT_NAME = "프래그먼트 이름"
-        const val PARAM_CATEGORY_LIST = "카테고리 리스트"
-        const val PARAM_CATEGORY_FRAGMENT = "카테고리 조회"
-        const val PARAM_STACCATO_FRAGMENT = "스타카토 조회"
-
         const val KEY_EXCEPTION = "예외"
         const val KEY_EXCEPTION_MESSAGE = "예외 메시지"
 
