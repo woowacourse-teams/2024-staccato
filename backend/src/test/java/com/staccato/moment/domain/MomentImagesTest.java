296e1d6e012c0601f069a1f0a39777e5d8d98edc
YoonJuHo <dbswn990@naver.com>
2025-02-25 18:00:26 +0900

    fix: 이미지 순서 수정 시 반영 안되는 문제 #650 (#651)
    * fix: 기존 이미지 전체 삭제 후 갱신
    
    * test: ParameterizedTest로 변경
    
    * refactor: 메서드 명 변경
    
    * test: static 메서드 위치 변경

--- backend/src/test/java/com/staccato/moment/domain/MomentImagesTest.java
+++ backend/src/test/java/com/staccato/moment/domain/MomentImagesTest.java
@@ -4,9 +4,12 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import com.staccato.exception.StaccatoException;
 import com.staccato.fixture.moment.MomentFixture;
@@ -15,9 +18,18 @@ import com.staccato.memory.domain.Memory;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertAll;
 
 class MomentImagesTest {
+    static Stream<Arguments> provideUpdateData() {
+        return Stream.of(
+                Arguments.of(List.of("picture1", "picture2", "picture3"), List.of()),
+                Arguments.of(List.of("picture1", "picture2", "picture3"), List.of("picture4")),
+                Arguments.of(List.of("picture1", "picture2", "picture3"), List.of("picture3", "picture2")),
+                Arguments.of(List.of("picture1", "picture2", "picture3"), List.of("picture3", "picture1", "picture2")),
+                Arguments.of(List.of("picture1", "picture2", "picture3"), List.of("picture1", "picture2", "picture4", "picture3"))
+        );
+    }
+
     @DisplayName("생성하려는 사진의 갯수가 0장 이상, 5장 이하이면 생성에 성공한다.")
     @ParameterizedTest
     @ValueSource(ints = {0, 5})
@@ -50,44 +62,21 @@ class MomentImagesTest {
                 .hasMessage("사진은 5장을 초과할 수 없습니다.");
     }
 
-    @DisplayName("사진들을 추가할 때 기존 사진이 포함되지 않은 경우 삭제 후 추가한다.")
-    @Test
-    void update() {
+    @DisplayName("사진들의 순서를 변경하거나 새로운 사진이 추가될 때 기존 사진을 전부 삭제 후 추가한다.")
+    @MethodSource("provideUpdateData")
+    @ParameterizedTest
+    void update(List<String> existingImageNames, List<String> updatedImageNames) {
         // given
         Memory memory = Memory.builder().title("Sample Memory").startAt(LocalDate.now().minusDays(1))
                 .endAt(LocalDate.now().plusDays(1)).build();
-        MomentImages existingImages = new MomentImages(List.of("picture1", "picture3"));
-        MomentImages updatedImages = new MomentImages(List.of("picture1", "picture4"));
+        MomentImages existingImages = new MomentImages(existingImageNames);
+        MomentImages updatedImages = new MomentImages(updatedImageNames);
 
         // when
         existingImages.update(updatedImages, MomentFixture.create(memory, LocalDateTime.now()));
-
-        // then
         List<String> images = existingImages.getImages().stream().map(MomentImage::getImageUrl).toList();
-        assertAll(
-                () -> assertThat(images).containsAll(List.of("picture1", "picture4")),
-                () -> assertThat(images.size()).isEqualTo(2)
-        );
-    }
-
-    @DisplayName("포함되지 않는 사진들을 선별할 수 있다.")
-    @Test
-    void findImagesNotPresentIn() {
-        // given
-        MomentImages existingImages = new MomentImages(List.of("picture1", "picture3"));
-        MomentImages newImages = new MomentImages(List.of("picture1", "picture4"));
-
-        // when
-        List<MomentImage> remainingExistingImages = existingImages.findImagesNotPresentIn(newImages);
-        List<MomentImage> remainingNewImages = newImages.findImagesNotPresentIn(existingImages);
 
         // then
-        assertAll(
-
-                () -> assertThat(remainingExistingImages.size()).isEqualTo(1),
-                () -> assertThat(remainingNewImages.size()).isEqualTo(1),
-                () -> assertThat(remainingExistingImages.get(0).getImageUrl()).isEqualTo("picture3"),
-                () -> assertThat(remainingNewImages.get(0).getImageUrl()).isEqualTo("picture4")
-        );
+        assertThat(images).containsExactlyElementsOf(updatedImageNames);
     }
 }
