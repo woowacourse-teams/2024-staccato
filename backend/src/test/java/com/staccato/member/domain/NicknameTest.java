01f75c6c6f043d2e1fbed8ae245ce2e5038e6af6
linirini <2001yerin@naver.com>
2025-03-11 23:57:10 +0900

    feat: 닉네임에 모음/자음/공백 포함 가능하도록 구현

--- backend/src/test/java/com/staccato/member/domain/NicknameTest.java
+++ backend/src/test/java/com/staccato/member/domain/NicknameTest.java
@@ -11,8 +11,20 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class NicknameTest {
     @DisplayName("유효한 닉네임을 생성한다.")
     @Test
-    void CreateNickname() {
+    void createNickname() {
         assertThatNoException().isThrownBy(() -> new Nickname("가ㄱㅏㅣㅎ.AZ1az_"));
+    }
+
+    @DisplayName("닉네임 중간에 공백을 포함할 수 있다.")
+    @Test
+    void createNicknameWithBlank() {
+        assertThatNoException().isThrownBy(() -> new Nickname("가  가"));
+    }
+
+    @DisplayName("닉네임에는 한글 자음/모음을 포함할 수 있다.")
+    @Test
+    void createNicknameWithIncompleteKorean() {
+        assertThatNoException().isThrownBy(() -> new Nickname("ㅎrㅇi"));
     }
 
     @DisplayName("닉네임의 형식이 잘못되었을 경우 예외를 발생시킨다.")
