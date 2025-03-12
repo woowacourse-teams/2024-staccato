9aa9a975e255faeb6a85efd30c2fa8d9ea475086
Junyoung-WON <wjyo60@gmail.com>
2025-03-13 02:16:43 +0900

    test: 닉네임 길이 정책 수정에 따라 테스트 코드 수정

--- android/Staccato_AN/app/src/test/java/com/on/staccato/domain/model/NicknameTest.kt
+++ android/Staccato_AN/app/src/test/java/com/on/staccato/domain/model/NicknameTest.kt
@@ -8,7 +8,7 @@ import org.junit.jupiter.params.provider.ValueSource
 class NicknameTest {
     @ParameterizedTest
     @ValueSource(
-        strings = ["스타카토", "Staccato", "staccato0701", "star_cato", "스타카토 S2", "_sta .c .cato_"],
+        strings = ["스타카토", "Staccato", "stct0701", "star_cato", "스타카토 S2", "_st. .ato_"],
     )
     fun `닉네임은 한글, 영문, 숫자, 마침표, 밑줄, 띄어쓰기로 조합할 수 있다`(nickname: String) {
         // when
@@ -41,7 +41,7 @@ class NicknameTest {
 
     @ParameterizedTest
     @ValueSource(
-        strings = [" ", "  nickname", "   nickname   "],
+        strings = [" ", "  ", "  nickname"],
     )
     fun `닉네임에 공백이 먼저 나온다면 BlankFirst 상태이다`(blankFirstString: String) {
         // when
@@ -53,7 +53,7 @@ class NicknameTest {
 
     @ParameterizedTest
     @ValueSource(
-        strings = ["스타카토!", "st@cc@to", "star-*-cato", "수타카토~^^", "해피 :)"],
+        strings = ["스타카토!", "st@cc@to", "star*cato", "수타카토~^^", "해피 :)"],
     )
     fun `닉네임에 밑줄, 마침표를 제외한 다른 특수기호가 포함되어 있다면 InvalidFormat 상태이다`(invalidNickname: String) {
         // when
@@ -64,9 +64,9 @@ class NicknameTest {
     }
 
     @Test
-    fun `닉네임의 길이는 1자 이상 20자 이내이며, 20자를 초과하면 InvalidLength 상태이다`() {
-        // when : 20자를 초과한 문자열(현재 문자열 = 21자)
-        val nicknameState = Nickname.validate("무지무지하게 기이이이이이이이이인 닉네임")
+    fun `닉네임의 길이는 1자 이상 10자 이내이며, 10자를 초과하면 InvalidLength 상태이다`() {
+        // when : 10자를 초과한 문자열 (테스트 문자열: 11자)
+        val nicknameState = Nickname.validate("엄청 기이이인 닉네임")
 
         // then
         assertThat(nicknameState).isInstanceOf(NicknameState.InvalidLength::class.java)
