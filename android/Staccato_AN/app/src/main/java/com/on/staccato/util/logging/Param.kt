a4932289f3f9d0858517670c54b86a4b96626024
Somin Lee <46596035+s6m1n@users.noreply.github.com>
2025-03-15 01:28:46 +0900

    fix: 이벤트 파라미터 문자열 상수를 한글에서 영문으로 변경 #675 (#676)

--- android/Staccato_AN/app/src/main/java/com/on/staccato/util/logging/Param.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/util/logging/Param.kt
@@ -2,24 +2,24 @@ package com.on.staccato.util.logging
 
 data class Param<T : Any>(val key: String, val value: T) {
     companion object {
-        const val KEY_SDK_VERSION = "안드로이드 SDK 버전"
+        const val KEY_SDK_VERSION = "Android_SDK_version"
 
-        const val KEY_BOTTOM_SHEET_STATE = "바텀시트 상태 종류"
+        const val KEY_BOTTOM_SHEET_STATE = "bottom_sheet_state"
         const val PARAM_BOTTOM_SHEET_COLLAPSED = "COLLAPSED"
         const val PARAM_BOTTOM_SHEET_EXPANDED = "EXPANDED"
         const val PARAM_BOTTOM_SHEET_HALF_EXPANDED = "HALF_EXPANDED"
-        const val KEY_BOTTOM_SHEET_DURATION = "지속시간"
+        const val KEY_BOTTOM_SHEET_DURATION = "bottom_sheet_state_duration"
 
-        const val KEY_IS_CREATED_IN_MAIN = "메인 화면에서 만들어진 스타카토인가?"
-        const val KEY_IS_VIEWED_BY_MARKER = "마커 클릭으로 조회된 스타카토인가?"
-        const val KEY_IS_GALLERY = "갤러리에서 사진을 첨부했나?"
+        const val KEY_IS_CREATED_IN_MAIN = "is_created_in_main_activity"
+        const val KEY_IS_VIEWED_BY_MARKER = "is_viewed_from_marker_click"
+        const val KEY_IS_GALLERY = "is_attached_from_gallery"
 
-        const val KEY_FRAGMENT_NAME = "프래그먼트 이름"
-        const val PARAM_CATEGORY_LIST = "카테고리 리스트"
-        const val PARAM_CATEGORY_FRAGMENT = "카테고리 조회"
-        const val PARAM_STACCATO_FRAGMENT = "스타카토 조회"
+        const val KEY_FRAGMENT_NAME = "fragment_name"
+        const val PARAM_CATEGORY_LIST = "category_list_timeline"
+        const val PARAM_CATEGORY_FRAGMENT = "category_fragment"
+        const val PARAM_STACCATO_FRAGMENT = "staccato_fragment"
 
-        const val KEY_EXCEPTION = "예외"
-        const val KEY_EXCEPTION_MESSAGE = "예외 메시지"
+        const val KEY_EXCEPTION = "exception"
+        const val KEY_EXCEPTION_MESSAGE = "exception_message"
     }
 }
