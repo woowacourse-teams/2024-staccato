1e7be735a4f6107ccb8639a86c09a5f95f697cec
BurningFalls <sherlocky31415@gmail.com>
2025-03-24 14:37:23 +0900

    refactor: 미사용 import문 삭제

--- backend/src/test/java/com/staccato/config/jwt/MemberTokenProviderTest.java
+++ backend/src/test/java/com/staccato/config/jwt/MemberTokenProviderTest.java
@@ -4,26 +4,15 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.staccato.exception.UnauthorizedException;
 import com.staccato.fixture.Member.MemberFixture;
 import com.staccato.member.domain.Member;
 import com.staccato.member.repository.MemberRepository;
+
 import io.jsonwebtoken.Claims;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.failBecauseExceptionWasNotThrown;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.mockito.Mockito.when;
-
-import java.time.LocalDateTime;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MemberTokenProviderTest {
