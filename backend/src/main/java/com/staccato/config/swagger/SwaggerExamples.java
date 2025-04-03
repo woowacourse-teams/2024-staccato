8e83f0c2538c48be1993f205b0773b499ea0687b
BurningFalls <sherlocky31415@gmail.com>
2025-04-03 22:24:22 +0900

    refactor: SwaggerExample 한 파일에 몰아서 관리

---
+++ backend/src/main/java/com/staccato/config/swagger/SwaggerExamples.java
@@ -0,0 +1,28 @@
+package com.staccato.config.swagger;
+
+public class SwaggerExamples {
+    public static final String CATEGORY_ID = "1";
+    public static final String CATEGORY_TITLE = "소소한 일상 기록";
+    public static final String CATEGORY_DESCRIPTION = "무심코 지나친 일상 속 소중한 순간들을 모아두었습니다.";
+    public static final String CATEGORY_START_AT = "2024-01-01";
+    public static final String CATEGORY_END_AT = "2024-12-31";
+    public static final String STACCATO_ID = "1";
+    public static final String STACCATO_TITLE = "햇살 좋은 오후, 창가에서";
+    public static final String STACCATO_VISITED_AT = "2024-06-01T14:30:00";
+    public static final String STACCATO_PLACE_NAME = "스타벅스";
+    public static final String STACCATO_ADDRESS = "서울특별시 마포구 양화로 123";
+    public static final String STACCATO_LATITUDE = "37.5564";
+    public static final String STACCATO_LONGITUDE = "126.9220";
+    public static final String COMMENT_ID = "1";
+    public static final String COMMENT_CONTENT = "아무 약속도 없는 하루, 창가에 앉아 멍하니 하늘을 바라봤다.";
+    public static final String MEMBER_ID = "1";
+    public static final String MEMBER_CODE = "3fa85f64-5717-4562-b3fc-2c963f66afa6";
+    public static final String MEMBER_NICKNAME = "김스타";
+    public static final String IMAGE_URL = "https://image.staccato.kr/web/share/excited.png";
+    public static final String IMAGE_URLS = "[\"https://image.staccato.kr/web/share/happy.png\", " +
+            "\"https://image.staccato.kr/web/share/angry.png\", " +
+            "\"https://image.staccato.kr/web/share/sad.png\"]";
+    public static final String FEELING = "happy";
+    public static final String EXPIRED_AT = "2025-06-01T00:00:00";
+    public static final String SHARE_LINK = "https://staccato.kr/share/eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+}
