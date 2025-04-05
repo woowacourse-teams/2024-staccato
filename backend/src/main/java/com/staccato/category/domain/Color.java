8d04ba26f7a0a21fc3dd639007a01c73e97dff7e
linirini <2001yerin@naver.com>
2025-04-05 21:17:20 +0900

    refactor: HexCode 제거

--- backend/src/main/java/com/staccato/category/domain/Color.java
+++ backend/src/main/java/com/staccato/category/domain/Color.java
@@ -9,18 +9,17 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public enum Color {
 
-    RED("red", "F4828E"),
-    ORANGE("orange", "FFAE8C"),
-    YELLOW("yellow", "F8DB90"),
-    GREEN("green", "C3E28D"),
-    BLUE("blue", "A0C0F6"),
-    INDIGO("indigo", "A6A6F6"),
-    PURPLE("purple", "D0AAFE"),
-    PINK("pink", "FAB0D2"),
-    GRAY("gray", "D2D2D2");
+    RED("red"),
+    ORANGE("orange"),
+    YELLOW("yellow"),
+    GREEN("green"),
+    BLUE("blue"),
+    INDIGO("indigo"),
+    PURPLE("purple"),
+    PINK("pink"),
+    GRAY("gray");
 
     private final String name;
-    private final String hexCode;
 
     public static Color findByName(String color) {
         return Arrays.stream(values())
