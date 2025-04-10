b6c3dbc43c95ab2a93ec840b66d926aa616fb658
BurningFalls <sherlocky31415@gmail.com>
2025-04-11 00:33:48 +0900

    feat: 썸네일에 사진 띄우는 기능 구현

--- backend/src/main/java/com/staccato/web/PageController.java
+++ backend/src/main/java/com/staccato/web/PageController.java
@@ -5,8 +5,15 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.staccato.staccato.service.StaccatoShareService;
+import com.staccato.staccato.service.dto.response.StaccatoSharedResponse;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class PageController {
+    private final StaccatoShareService staccatoShareService;
 
     @GetMapping("/")
     public String index() {
@@ -15,7 +22,14 @@ public class PageController {
 
     @GetMapping("/share/{token}")
     public String share(@PathVariable String token, Model model) {
+        StaccatoSharedResponse response = staccatoShareService.readSharedStaccatoByToken(token);
+        String thumbnailUrl = response.staccatoImageUrls().stream()
+                .findFirst()
+                .orElse("https://image.staccato.kr/web/share/frame.png");
+
         model.addAttribute("token", token);
+        model.addAttribute("thumbnailUrl", thumbnailUrl);
+
         return "share";
     }
 }
