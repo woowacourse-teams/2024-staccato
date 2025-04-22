c5474adae3cb92c893eba7a912b48889eb9d4361
Seongju Lee <sherlocky31415@gmail.com>
2025-04-23 00:18:55 +0900

    feat: 스타카토 공유 링크 썸네일 지정 #728 (#729)
    * refactor: 사진 크기에 반응형 디자인 적용
    
    * feat: 썸네일에 사진 띄우는 기능 구현
    
    * refactor: default thumbnail url 상수화
    
    * refactor: (1) header title과 title의 크기 동일하게 조정
    
    * refactor: (2) feeling 문구 가운데 정렬
    
    * refactor: (3) feeling 문구와 comment 문구 크기 동일하게 조정
    
    * refactor: (4 & 6) 이미지 크기 반응형 적용
    
    * refactor: (3 & 5 & 7) 글씨 크기 조정
    
    * fix: 테스트 오류 수정

--- backend/src/main/java/com/staccato/web/PageController.java
+++ backend/src/main/java/com/staccato/web/PageController.java
@@ -5,8 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import com.staccato.staccato.service.StaccatoShareService;
+import com.staccato.staccato.service.dto.response.StaccatoSharedResponse;
+
+import lombok.RequiredArgsConstructor;
+
 @Controller
+@RequiredArgsConstructor
 public class PageController {
+    private static final String DEFAULT_THUMBNAIL_URL = "https://image.staccato.kr/web/share/frame.png";
+
+    private final StaccatoShareService staccatoShareService;
 
     @GetMapping("/")
     public String index() {
@@ -15,7 +24,14 @@ public class PageController {
 
     @GetMapping("/share/{token}")
     public String share(@PathVariable String token, Model model) {
+        StaccatoSharedResponse response = staccatoShareService.readSharedStaccatoByToken(token);
+        String thumbnailUrl = response.staccatoImageUrls().stream()
+                .findFirst()
+                .orElse(DEFAULT_THUMBNAIL_URL);
+
         model.addAttribute("token", token);
+        model.addAttribute("thumbnailUrl", thumbnailUrl);
+
         return "share";
     }
 }
