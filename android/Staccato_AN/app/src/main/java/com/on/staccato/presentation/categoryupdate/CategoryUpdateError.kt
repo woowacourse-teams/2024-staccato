0ede6592ab165193632550aeca07332d574ad488
hxeyexn <kck85075@kumoh.ac.kr>
2025-05-05 00:43:13 +0900

    feat: CategoryUpdateError에 state 프로퍼티 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/categoryupdate/CategoryUpdateError.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/categoryupdate/CategoryUpdateError.kt
@@ -5,9 +5,11 @@ import com.on.staccato.presentation.common.photo.UploadFile
 import com.on.staccato.presentation.util.ExceptionState2
 
 sealed interface CategoryUpdateError {
-    data class CategoryInitialization(val state: ExceptionState2) : CategoryUpdateError
+    val state: ExceptionState2
 
-    data class Thumbnail(val state: ExceptionState2, val uri: Uri, val file: UploadFile) : CategoryUpdateError
+    data class CategoryInitialization(override val state: ExceptionState2) : CategoryUpdateError
 
-    data class CategoryUpdate(val state: ExceptionState2) : CategoryUpdateError
+    data class Thumbnail(override val state: ExceptionState2, val uri: Uri, val file: UploadFile) : CategoryUpdateError
+
+    data class CategoryUpdate(override val state: ExceptionState2) : CategoryUpdateError
 }
