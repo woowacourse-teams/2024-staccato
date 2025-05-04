91085a2a27ec2e7b03e815c17f9759b9ca16a19b
yoonjuho <dbswn990@naver.com>
2025-05-04 22:03:41 +0900

    feat: S3 Fake Object 설정

--- backend/src/test/java/com/staccato/image/infrastructure/FakeS3ObjectClient.java
+++ backend/src/test/java/com/staccato/image/infrastructure/FakeS3ObjectClient.java
@@ -1,16 +1,38 @@
 package com.staccato.image.infrastructure;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class FakeS3ObjectClient extends S3ObjectClient {
+    public static final String FAKE_CLOUD_FRONT_END_POINT = "fakeCloudFrontEndPoint";
+    private final Set<String> storedKeys = new HashSet<>();
+
     public FakeS3ObjectClient() {
-        super("fakeBuket", "fakeEndPoint", "fakeCloudFrontEndPoint", "fakeAccessKey", "fakeSecretAccessKey");
+        super("fakeBuket", "fakeEndPoint", FAKE_CLOUD_FRONT_END_POINT, "fakeAccessKey", "fakeSecretAccessKey");
     }
 
     @Override
     public void putS3Object(String objectKey, String contentType, byte[] imageBytes) {
+        storedKeys.add(objectKey);
     }
 
     @Override
     public String getUrl(String keyName) {
-        return "fakeUrl";
+        return FAKE_CLOUD_FRONT_END_POINT + "/" + keyName;
+    }
+
+    @Override
+    public String extractKeyFromUrl(String url) {
+        return url.replace(FAKE_CLOUD_FRONT_END_POINT + "/", "");
+    }
+
+    @Override
+    public int deleteUnusedObjects(Set<String> usedKeys) {
+        Set<String> toDelete = storedKeys.stream()
+                .filter(stored -> !usedKeys.contains(stored))
+                .collect(Collectors.toSet());
+        storedKeys.removeAll(toDelete);
+        return toDelete.size();
     }
 }
