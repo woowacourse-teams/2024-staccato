38a9678a14132fd6c5bc255f369baae6018db52e
Junyoung-WON <wjyo60@gmail.com>
2025-05-06 21:21:46 +0900

    feat: MiddleDivider 컴포저블 작성
    - 중간 구분선 구현
    - Preview 추가

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/mypage/Components.kt
@@ -0,0 +1,29 @@
+package com.on.staccato.presentation.mypage
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.on.staccato.presentation.component.Divider
+import com.on.staccato.theme.Gray1
+
+@Composable
+fun MiddleDivider(
+    modifier: Modifier = Modifier,
+    thickness: Dp = 10.dp,
+    color: Color = Gray1,
+) {
+    Divider(
+        modifier = modifier,
+        thickness = thickness,
+        color = color,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MiddleDividerPreview() {
+    MiddleDivider()
+}
