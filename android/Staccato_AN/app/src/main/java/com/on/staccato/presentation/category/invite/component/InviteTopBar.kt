fdbf751c4a7cea4fa28ca3370794724098144647
somin <salth0601@gmail.com>
2025-05-14 23:18:00 +0900

    ui: 초대 다이얼로그 상단바 구현

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/category/invite/component/InviteTopBar.kt
@@ -0,0 +1,74 @@
+package com.on.staccato.presentation.category.invite.component
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.on.staccato.R
+import com.on.staccato.presentation.component.clickableWithoutRipple
+import com.on.staccato.theme.Body2
+import com.on.staccato.theme.Gray3
+import com.on.staccato.theme.Title2
+
+@Composable
+fun InviteTopBar(
+    modifier: Modifier = Modifier,
+    onDismissRequest: () -> Unit,
+) {
+    Row(
+        modifier =
+            modifier
+                .fillMaxWidth()
+                .padding(start = 16.dp, end = 16.dp, top = 18.dp, bottom = 20.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.icon_close),
+            contentDescription = stringResource(id = R.string.all_cancel),
+            tint = Gray3,
+            modifier =
+                Modifier
+                    .size(12.dp)
+                    .clickableWithoutRipple(
+                        onClick = onDismissRequest,
+                    ),
+        )
+        Text(
+            text = stringResource(R.string.invite_member_title),
+            style = Title2,
+        )
+        Text(
+            text = stringResource(R.string.all_confirm),
+            style = Body2,
+            modifier =
+                Modifier.clickable(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null,
+                    onClick = onDismissRequest,
+                ),
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InviteTopBarPreview() {
+    InviteTopBar(
+        Modifier,
+        {},
+    )
+}
