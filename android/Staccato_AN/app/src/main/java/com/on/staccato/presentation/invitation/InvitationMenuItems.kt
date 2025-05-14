31c2a284b2a8d9d93129a28457061c19eeb279b4
Junyoung-WON <wjyo60@gmail.com>
2025-05-15 00:13:24 +0900

    feat: 카테고리 초대 관리 화면의 메뉴를 Enum으로 구성
    - menuId: 메뉴에 대한 색인
    - title: 메뉴 이름
    - iconResId: 메뉴 아이콘 리소스 ID
    - iconContentDescription: 아이콘 ContentDescription
    - 열거 아이템: 받은 초대, 보낸 초대

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationMenuItems.kt
@@ -0,0 +1,14 @@
+package com.on.staccato.presentation.invitation
+
+import androidx.annotation.DrawableRes
+import com.on.staccato.R
+
+enum class InvitationMenuItems(
+    val menuId: Int,
+    val title: String,
+    @DrawableRes val iconResId: Int,
+    val iconContentDescription: String,
+) {
+    RECEIVED_INVITATION(0, "받은 초대", R.drawable.icon_receive_box, "Received Icon"),
+    SENT_INVITATION(1, "보낸 초대", R.drawable.icon_send, "Sent Icon"),
+}
