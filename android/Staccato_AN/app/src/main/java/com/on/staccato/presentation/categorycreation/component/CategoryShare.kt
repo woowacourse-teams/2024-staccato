cacd5bcdcd6a06eb2770eab6e9c718d96a46d56d
hxeyexn <kck85075@kumoh.ac.kr>
2025-05-14 19:33:51 +0900

    refactor: Category 생성 화면 입력값 제목 및 힌트 component 분리
    - CategoryInputTitle: 입력값 제목 component
    - CategoryInputHint: 입력값 힌트 component
    - CategoryInputGuide: 입력값 제목 및 힌트 component

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/categorycreation/component/CategoryShare.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/categorycreation/component/CategoryShare.kt
@@ -1,24 +1,16 @@
 package com.on.staccato.presentation.categorycreation.component
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.on.staccato.R
 import com.on.staccato.presentation.component.DefaultSwitch
-import com.on.staccato.theme.Body4
-import com.on.staccato.theme.Gray3
-import com.on.staccato.theme.StaccatoBlack
-import com.on.staccato.theme.Title2
 
 @Composable
 fun CategoryShare(
@@ -32,31 +24,14 @@ fun CategoryShare(
                 .fillMaxWidth(),
         verticalAlignment = Alignment.CenterVertically,
     ) {
-        CategoryShareTitleAndHint()
+        CategoryInputGuide(
+            titleId = R.string.category_creation_share_title,
+            hintId = R.string.category_creation_share_hint,
+        )
         Spacer(modifier = Modifier.weight(1f))
         DefaultSwitch(
             checked = checked,
             onCheckedChange = onCheckedChange,
-        )
-    }
-}
-
-@Composable
-private fun CategoryShareTitleAndHint() {
-    Column(
-        modifier = Modifier.padding(start = 24.dp),
-        horizontalAlignment = AbsoluteAlignment.Left,
-    ) {
-        Text(
-            text = stringResource(id = R.string.category_creation_share_title),
-            color = StaccatoBlack,
-            style = Title2,
-        )
-        Spacer(modifier = Modifier.padding(top = 8.dp))
-        Text(
-            text = stringResource(id = R.string.category_creation_share_hint),
-            color = Gray3,
-            style = Body4,
         )
     }
 }
