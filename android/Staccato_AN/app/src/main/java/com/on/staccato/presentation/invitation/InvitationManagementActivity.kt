9452c1de9475316058523e178dc0e463f08822fd
Junyoung-WON <wjyo60@gmail.com>
2025-05-15 22:57:59 +0900

    feat: 카테고리 초대 관리 Activity 생성
    - Manifest 수정: InvitationManagementActivity 추가, 일부 개행 수정

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementActivity.kt
@@ -0,0 +1,11 @@
+package com.on.staccato.presentation.invitation
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class InvitationManagementActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // Todo: 초대 관리 화면 구현
+    }
+}
