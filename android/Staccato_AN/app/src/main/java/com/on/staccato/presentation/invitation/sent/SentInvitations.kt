0175099a53940d2224d883430b582f9a9017013d
Junyoung-WON <wjyo60@gmail.com>
2025-05-17 16:16:57 +0900

    feat: 보낸 초대 목록 화면 구현
    - ConstraintLayout 활용

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/sent/SentInvitations.kt
@@ -0,0 +1,38 @@
+package com.on.staccato.presentation.invitation.sent
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.on.staccato.presentation.component.DefaultDivider
+import com.on.staccato.presentation.invitation.model.InvitationUiModel
+import com.on.staccato.presentation.invitation.model.dummyInvitationUiModels
+
+@Composable
+fun SentInvitations(
+    sentInvitations: List<InvitationUiModel>,
+    onCancelClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    LazyColumn(modifier = modifier) {
+        itemsIndexed(sentInvitations) { index, invitation ->
+            SentInvitationItem(
+                categoryInvitation = invitation,
+                onCancelClick = onCancelClick,
+            )
+            if (index != sentInvitations.lastIndex) {
+                DefaultDivider()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun SentInvitationPreview() {
+    SentInvitations(
+        sentInvitations = dummyInvitationUiModels,
+        onCancelClick = {},
+    )
+}
