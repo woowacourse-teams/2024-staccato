f0c6af6d5dd4b409cf47fe2beb3d338f180673d0
Junyoung-WON <wjyo60@gmail.com>
2025-05-17 16:04:45 +0900

    refactor: DefaultNavigationTopBar 구성 변경
    - TopAppBarColors 설정
    - Navigation 버튼을 IconButton에서 Icon으로 변경
    - Modifier에 clickableWithoutRipple 속성 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/component/topbar/DefaultNavigationTopBar.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/component/topbar/DefaultNavigationTopBar.kt
@@ -3,23 +3,38 @@ package com.on.staccato.presentation.component.topbar
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.on.staccato.R
+import com.on.staccato.presentation.component.clickableWithoutRipple
 import com.on.staccato.theme.Body4
 import com.on.staccato.theme.Gray3
 import com.on.staccato.theme.Gray5
+import com.on.staccato.theme.StaccatoBlack
 import com.on.staccato.theme.Title2
+import com.on.staccato.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+private val TopAppBarColors = TopAppBarColors(
+    containerColor = White,
+    scrolledContainerColor = White,
+    navigationIconContentColor = Gray3,
+    titleContentColor = StaccatoBlack,
+    actionIconContentColor = Gray3,
+)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -29,6 +44,7 @@ fun DefaultNavigationTopBar(
     isTitleCentered: Boolean = true,
     @DrawableRes vectorResource: Int = R.drawable.icon_arrow_left,
     onNavigationClick: () -> Unit,
+    colors: TopAppBarColors = TopAppBarColors,
 ) {
     if (isTitleCentered) {
         CenterAlignedTopAppBar(
@@ -38,6 +54,7 @@ fun DefaultNavigationTopBar(
             navigationIcon = {
                 NavigationIconButton(vectorResource, onNavigationClick)
             },
+            colors = colors,
         )
     } else {
         TopAppBar(
@@ -46,7 +63,8 @@ fun DefaultNavigationTopBar(
             },
             navigationIcon = {
                 NavigationIconButton(vectorResource, onNavigationClick)
-            }
+            },
+            colors = colors,
         )
     }
 }
@@ -92,48 +110,45 @@ private fun NavigationIconButton(
     @DrawableRes vectorResource: Int,
     onNavigationClick: () -> Unit,
 ) {
-    IconButton(
-        onClick = onNavigationClick,
-    ) {
-        NavigationIcon(vectorResource)
-    }
-}
-
-@Composable
-private fun NavigationIcon(
-    @DrawableRes vectorResource: Int = R.drawable.icon_arrow_left,
-) {
     Icon(
+        modifier = Modifier
+            .padding(5.dp)
+            .clickableWithoutRipple { onNavigationClick() },
         imageVector = ImageVector.vectorResource(id = vectorResource),
         contentDescription = "Navigation Icon",
         tint = Gray3,
     )
 }
 
-@Preview(showBackground = true)
+@OptIn(ExperimentalMaterial3Api::class)
+@Preview(showBackground = true, backgroundColor = 0xFFFFFF.toLong())
 @Composable
 private fun TopBarComponentsPreview() {
     Column {
         DefaultNavigationTopBar(
             title = "상단 앱 바 제목",
             isTitleCentered = true,
-        ) { }
+            onNavigationClick = {},
+        )
 
         DefaultNavigationTopBar(
             title = "상단 앱 바 제목",
             isTitleCentered = false,
-        ) { }
+            onNavigationClick = {},
+        )
 
         DefaultNavigationTopBar(
             title = "상단 앱 바 제목",
             subtitle = "상단 앱 바 부제목입니다",
             isTitleCentered = true,
-        ) { }
+            onNavigationClick = {},
+        )
 
         DefaultNavigationTopBar(
             title = "상단 앱 바 제목",
             subtitle = "상단 앱 바 부제목입니다",
             isTitleCentered = false,
-        ) { }
+            onNavigationClick = {},
+        )
     }
 }
