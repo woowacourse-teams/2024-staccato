f35ce13af5247dc55cca7ec1f755152ff6758b69
somin <salth0601@gmail.com>
2025-05-17 18:16:05 +0900

    feat: 사진 업로드 상태 관련 클래스 추가

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/photo/AttachedPhotoState.kt
@@ -0,0 +1,9 @@
+package com.on.staccato.presentation.common.photo
+
+sealed interface AttachedPhotoState {
+    data object Loading : AttachedPhotoState
+
+    data object Success : AttachedPhotoState
+
+    data object Fail : AttachedPhotoState
+}
