fe90eb37c30f397dd267e0f6107efc22fecefc0c
Junyoung-WON <wjyo60@gmail.com>
2025-05-17 16:18:21 +0900

    feat: InvitationManagementScreen을 Activity에 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementActivity.kt
@@ -1,11 +1,24 @@
 package com.on.staccato.presentation.invitation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 
 class InvitationManagementActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // Todo: 초대 관리 화면 구현
+        setContent {
+            InvitationManagementScreen(
+                onNavigationClick = { finish() }
+            )
+        }
+    }
+
+    companion object {
+        fun launch(context: Context) {
+            context.startActivity(Intent(context, InvitationManagementActivity::class.java))
+        }
     }
 }
