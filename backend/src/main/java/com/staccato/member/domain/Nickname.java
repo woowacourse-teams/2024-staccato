0a18f16b58f65f32ed373f42242c856edd31f8ca
linirini <2001yerin@naver.com>
2025-05-19 16:50:54 +0900

    feat: nickname 길이 검증 추가

--- backend/src/main/java/com/staccato/member/domain/Nickname.java
+++ backend/src/main/java/com/staccato/member/domain/Nickname.java
@@ -17,15 +17,23 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 public class Nickname {
     private static final Pattern NICKNAME_REGEX = Pattern.compile("^(?!\\\\s+$)[ㄱ-ㅎㅏ-ㅣ가-힣a-zA-Z0-9._ ]+$");
-    private static final int MAX_LENGTH = 20;
+    private static final int COLUMN_MAX_LENGTH = 20;
+    private static final int NICKNAME_MAX_LENGTH = 10;
 
-    @Column(nullable = false, length = MAX_LENGTH)
+    @Column(nullable = false, length = COLUMN_MAX_LENGTH)
     private String nickname;
 
     public Nickname(String nickname) {
         String trimmedNickname = nickname.trim();
+        validateLength(trimmedNickname);
         validateRegex(trimmedNickname);
         this.nickname = trimmedNickname;
+    }
+
+    private void validateLength(String trimmedNickname) {
+        if(trimmedNickname.isEmpty() || trimmedNickname.length()>NICKNAME_MAX_LENGTH){
+            throw new StaccatoException("1자 이상 10자 이하의 닉네임으로 설정해주세요.");
+        }
     }
 
     private static void validateRegex(String nickname) {
--- backend/src/test/java/com/staccato/member/domain/NicknameTest.java
+++ backend/src/test/java/com/staccato/member/domain/NicknameTest.java
@@ -6,6 +6,8 @@ import com.staccato.exception.StaccatoException;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.N;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNoException;
@@ -15,7 +17,7 @@ class NicknameTest {
     @DisplayName("유효한 닉네임을 생성한다.")
     @Test
     void createNickname() {
-        assertThatNoException().isThrownBy(() -> new Nickname("가ㄱㅏㅣㅎ.AZ1az_"));
+        assertThatNoException().isThrownBy(() -> new Nickname("가ㄱㅏ.AZ1az_"));
     }
 
     @DisplayName("닉네임 중간에 공백을 포함할 수 있다.")
@@ -32,7 +34,7 @@ class NicknameTest {
 
     @DisplayName("닉네임의 형식이 잘못되었을 경우 예외를 발생시킨다.")
     @ParameterizedTest
-    @ValueSource(strings = {"  ", "//", ""})
+    @ValueSource(strings = {"|", "//", "+"})
     void cannotCreateNicknameByInvalidFormat(String name) {
         assertThatThrownBy(() -> new Nickname(name))
                 .isInstanceOf(StaccatoException.class)
@@ -47,5 +49,29 @@ class NicknameTest {
 
         // when & then
         assertThat(nickname.getNickname()).isEqualTo("staccato");
+    }
+
+    @DisplayName("닉네임은 1자 이상, 10자 이하로 생성할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(ints = {1,10})
+    void createNicknameLengthInRange(int count){
+        // given
+        String nickname = "가".repeat(count);
+
+        // when & then
+        assertThatNoException().isThrownBy(() -> new Nickname(nickname));
+    }
+
+    @DisplayName("닉네임은 1자 미만, 10자 초과로 생성할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0,11})
+    void cannotCreateNicknameLengthOutOfRange(int count){
+        // given
+        String nickname = "가".repeat(count);
+
+        // when & then
+        assertThatThrownBy(() -> new Nickname(nickname))
+                .isInstanceOf(StaccatoException.class)
+                .hasMessage("1자 이상 10자 이하의 닉네임으로 설정해주세요.");
     }
 }
