a9b32fcc0338a7f25509ff66bafe00566be59571
Junyoung-WON <wjyo60@gmail.com>
2025-05-22 08:21:23 +0900

    feat: 보낸 초대 목록 아이템에 활용할 UI 모델 작성

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/model/SentInvitationUiModel.kt
@@ -0,0 +1,54 @@
+package com.on.staccato.presentation.invitation.model
+
+data class SentInvitationUiModel(
+    val categoryId: Long,
+    val categoryTitle: String,
+    val inviteeId: Long,
+    val inviteeNickname: String,
+    val inviteeProfileImageUrl: String? = null,
+)
+
+val dummySentInvitationUiModels = listOf(
+    SentInvitationUiModel(
+        categoryId = 0L,
+        categoryTitle = "우아한테크코스",
+        inviteeId = 0L,
+        inviteeNickname = "호두",
+    ),
+    SentInvitationUiModel(
+        categoryId = 1L,
+        categoryTitle = "우당탕탕 스타카토 개발기",
+        inviteeId = 1L,
+        inviteeNickname = "리니",
+    ),
+    SentInvitationUiModel(
+        categoryId = 1L,
+        categoryTitle = "우당탕탕 스타카토 개발기",
+        inviteeId = 3L,
+        inviteeNickname = "해나",
+    ),
+    SentInvitationUiModel(
+        categoryId = 1L,
+        categoryTitle = "우당탕탕 스타카토 개발기",
+        inviteeId = 4L,
+        inviteeNickname = "폭포",
+    ),
+    SentInvitationUiModel(
+        categoryId = 1L,
+        categoryTitle = "우당탕탕 스타카토 개발기",
+        inviteeId = 2L,
+        inviteeNickname = "빙티",
+    ),
+    SentInvitationUiModel(
+        categoryId = 1L,
+        categoryTitle = "우당탕탕 스타카토 개발기",
+        inviteeId = 5L,
+        inviteeNickname = "호티",
+    ),
+    SentInvitationUiModel(
+        categoryId = 1L,
+        categoryTitle = "우당탕탕 스타카토 개발기",
+        inviteeId = 6L,
+        inviteeNickname = "카고",
+    ),
+)
