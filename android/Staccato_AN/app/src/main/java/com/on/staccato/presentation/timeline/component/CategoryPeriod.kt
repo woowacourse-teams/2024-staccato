06dfb70824b85a400e63736088d136f0e6fa8e12
hxeyexn <kck85075@kumoh.ac.kr>
2025-05-21 19:41:36 +0900

    feat: 카테고리 기간을 동일 연도 여부에 따라 다른 형식으로 표시
    - 동일 연도 형식: 2025. 05. 03 - 07. 15
    - 다른 연도 형식: 2025. 05. 03 - 2026. 07. 15

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/component/CategoryPeriod.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/component/CategoryPeriod.kt
@@ -3,11 +3,9 @@ package com.on.staccato.presentation.timeline.component
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import com.on.staccato.R
 import com.on.staccato.theme.Body4
 import com.on.staccato.theme.Gray3
 import java.time.LocalDate
@@ -18,21 +16,7 @@ fun CategoryPeriod(
     startAt: LocalDate? = null,
     endAt: LocalDate? = null,
 ) {
-    // TODO: 년도에 따라 형식 수정
-    val period =
-        if (startAt != null && endAt != null) {
-            stringResource(
-                R.string.category_period_dot,
-                startAt.year,
-                startAt.monthValue,
-                startAt.dayOfMonth,
-                endAt.year,
-                endAt.monthValue,
-                endAt.dayOfMonth,
-            )
-        } else {
-            null
-        }
+    val period = formatPeriod(startAt, endAt)
 
     if (period != null) {
         Text(
@@ -60,6 +44,7 @@ private class CategoryPeriodPreviewParameterProvider : PreviewParameterProvider<
     override val values: Sequence<Pair<LocalDate?, LocalDate?>> =
         sequenceOf(
             LocalDate.of(2025, 4, 12) to LocalDate.of(2025, 10, 20),
+            LocalDate.of(2025, 4, 12) to LocalDate.of(2026, 10, 20),
             null to null,
         )
 }
