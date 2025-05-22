01145e3f94b750013632f73e12191396531a6768
Junyoung-WON <wjyo60@gmail.com>
2025-05-22 18:00:33 +0900

    ui: 초대 관리 화면의 카테고리 제목 수정
    - 굵기를 SemiBold로 지정
    - 기본 색상을 StaccatoBlack으로 변경

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/component/CategoryTitle.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/component/CategoryTitle.kt
@@ -5,6 +5,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import com.on.staccato.theme.StaccatoBlack
 import com.on.staccato.theme.Title3
@@ -21,6 +22,7 @@ fun CategoryTitle(
         modifier = modifier,
         style = style,
         color = color,
+        fontWeight = FontWeight.SemiBold,
         overflow = TextOverflow.Ellipsis,
         maxLines = 1,
     )
