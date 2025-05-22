0797e28b4dbc3afea55578bb3285ef62626dca2f
Junyoung-WON <wjyo60@gmail.com>
2025-05-22 18:01:10 +0900

    ui: 초대 관리 화면의 닉네임 텍스트 수정
    - 굵기를 SemiBold로 지정
    - 기본 색상을 StaccatoBlack으로 변경

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/component/NicknameText.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/component/NicknameText.kt
@@ -5,20 +5,22 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import com.on.staccato.theme.Body4
-import com.on.staccato.theme.Gray3
+import com.on.staccato.theme.StaccatoBlack
 
 @Composable
 fun NicknameText(
     nickname: String,
     modifier: Modifier = Modifier,
     style: TextStyle = Body4,
-    color: Color = Gray3,
+    color: Color = StaccatoBlack,
 ) {
     Text(
         text = nickname,
         modifier = modifier,
         style = style,
         color = color,
+        fontWeight = FontWeight.SemiBold,
     )
 }
