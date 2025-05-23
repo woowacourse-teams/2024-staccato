48f9ec91f4fa3aa8b300414bdbf31b2bd156d132
Junyoung-WON <wjyo60@gmail.com>
2025-05-23 16:45:14 +0900

    feat: InvitationManagement 컴포저블 분리
    - InvitationManagementScreen의 초대 관리 화면을 분리
    - ViewModel이 아닌 초대 목록과 버튼 클릭 콜백을 주입 받음
    - Preview 및 테스트가 용이하도록 변경

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementScreen.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementScreen.kt
@@ -1,6 +1,5 @@
 package com.on.staccato.presentation.invitation
 
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -11,19 +10,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.tooling.preview.PreviewParameter
-import androidx.compose.ui.tooling.preview.PreviewParameterProvider
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.on.staccato.data.invitation.InvitationTempRepository
-import com.on.staccato.presentation.component.DefaultDivider
 import com.on.staccato.presentation.component.topbar.DefaultNavigationTopBar
 import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuItems
 import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuItems.RECEIVED_INVITATION
 import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuItems.SENT_INVITATION
-import com.on.staccato.presentation.invitation.received.ReceivedInvitations
-import com.on.staccato.presentation.invitation.sent.SentInvitations
 import com.on.staccato.presentation.invitation.viewmodel.InvitationViewModel
 import com.on.staccato.theme.White
 
@@ -42,33 +33,22 @@ fun InvitationManagementScreen(
         containerColor = White,
         topBar = { InvitationManagementTopBar(onNavigationClick) },
     ) { contentPadding ->
-
-        Column(modifier = modifier.padding(contentPadding)) {
-            DefaultDivider()
-
-            InvitationSelectionMenu(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
-                selectedMenu = selectedMenu,
-                onClick = { selectedMenu = it },
-            )
-
-            when(selectedMenu) {
-                RECEIVED_INVITATION -> {
-                    ReceivedInvitations(
-                        receivedInvitations = receivedInvitations,
-                        onAcceptClick = {},
-                        onRejectClick = {},
-                    )
+        InvitationManagement(
+            modifier = modifier.padding(contentPadding),
+            selectedMenu = selectedMenu,
+            onMenuClick = {
+                selectedMenu = it
+                when(it) {
+                    RECEIVED_INVITATION -> invitationViewModel.getReceivedInvitations()
+                    SENT_INVITATION -> invitationViewModel.getSentInvitations()
                 }
-
-                SENT_INVITATION -> {
-                    SentInvitations(
-                        sentInvitations = sentInvitations,
-                        onCancelClick = {},
-                    )
-                }
-            }
-        }
+            },
+            receivedInvitations = receivedInvitations,
+            onRejectClick = {  },
+            onAcceptClick = {  },
+            sentInvitations = sentInvitations,
+            onCancelClick = {  },
+        )
     }
 }
 
@@ -81,21 +61,4 @@ private fun InvitationManagementTopBar(
         title = "카테고리 초대 관리",
         onNavigationClick = onNavigationClick,
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-private fun InvitationManagementScreenPreview(
-    @PreviewParameter(InvitationSelectionMenuProvider::class) menu: InvitationSelectionMenuItems,
-) {
-    InvitationManagementScreen(
-        invitationViewModel = InvitationViewModel(InvitationTempRepository()),
-        onNavigationClick = {},
-        defaultSelectedMenu = menu,
-    )
-}
-
-class InvitationSelectionMenuProvider : PreviewParameterProvider<InvitationSelectionMenuItems> {
-    override val values: Sequence<InvitationSelectionMenuItems>
-        get() = sequenceOf(RECEIVED_INVITATION, SENT_INVITATION)
 }
