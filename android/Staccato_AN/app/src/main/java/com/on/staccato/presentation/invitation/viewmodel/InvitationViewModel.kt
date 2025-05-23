011fdccdaba292374853175f0d85759e9720dfec
Junyoung-WON <wjyo60@gmail.com>
2025-05-23 15:12:25 +0900

    feat: InvitationViewModel 작성
    - 보낸 초대 목록과 받은 초대 목록을 불러온다.
    - 초대 거절, 수락, 취소 로직을 담당한다.(추후 구현 예정)

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/viewmodel/InvitationViewModel.kt
@@ -0,0 +1,50 @@
+package com.on.staccato.presentation.invitation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.on.staccato.data.network.onException2
+import com.on.staccato.data.network.onServerError
+import com.on.staccato.data.network.onSuccess
+import com.on.staccato.domain.repository.InvitationRepository
+import com.on.staccato.presentation.invitation.model.ReceivedInvitationUiModel
+import com.on.staccato.presentation.invitation.model.SentInvitationUiModel
+import com.on.staccato.presentation.mapper.toUiModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class InvitationViewModel(private val invitationRepository: InvitationRepository) : ViewModel() {
+    private val _receivedInvitations: MutableStateFlow<List<ReceivedInvitationUiModel>> = MutableStateFlow((emptyList()))
+    val receivedInvitations: StateFlow<List<ReceivedInvitationUiModel>> = _receivedInvitations.asStateFlow()
+
+    private val _sentInvitations: MutableStateFlow<List<SentInvitationUiModel>> = MutableStateFlow((emptyList()))
+    val sentInvitations: StateFlow<List<SentInvitationUiModel>> = _sentInvitations.asStateFlow()
+
+    init {
+        getReceivedInvitations()
+        getSentInvitations()
+    }
+
+    fun getReceivedInvitations() {
+        viewModelScope.launch {
+            val result = invitationRepository.getReceivedInvitations()
+            result.onSuccess { invitations ->
+                _receivedInvitations.value = invitations.map { it.toUiModel() }
+            }.onServerError {
+            }.onException2 {
+            }
+        }
+    }
+
+    fun getSentInvitations() {
+        viewModelScope.launch {
+            val result = invitationRepository.getSentInvitations()
+            result.onSuccess { invitations ->
+                _sentInvitations.value = invitations.map { it.toUiModel() }
+            }.onServerError {
+            }.onException2 {
+            }
+        }
+    }
+}
