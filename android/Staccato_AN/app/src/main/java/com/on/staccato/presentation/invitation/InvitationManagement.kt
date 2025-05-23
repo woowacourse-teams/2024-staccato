8bb0c021247dc8fed6e40a7d7d98b54f03bcd96a
Junyoung-WON <wjyo60@gmail.com>
2025-05-23 16:41:08 +0900

    feat: InvitationManagement 컴포저블 분리
    - InvitationManagementScreen의 초대 관리 화면을 분리
    - ViewModel이 아닌 초대 목록과 버튼 클릭 콜백을 주입 받음
    - Preview 및 테스트가 용이하도록 변경

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagement.kt
@@ -0,0 +1,79 @@
+package com.on.staccato.presentation.invitation
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import com.on.staccato.presentation.component.DefaultDivider
+import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuItems
+import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuItems.RECEIVED_INVITATION
+import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuItems.SENT_INVITATION
+import com.on.staccato.presentation.invitation.model.ReceivedInvitationUiModel
+import com.on.staccato.presentation.invitation.model.SentInvitationUiModel
+import com.on.staccato.presentation.invitation.model.dummyReceivedInvitationUiModels
+import com.on.staccato.presentation.invitation.model.dummySentInvitationUiModels
+import com.on.staccato.presentation.invitation.received.ReceivedInvitations
+import com.on.staccato.presentation.invitation.sent.SentInvitations
+
+@Composable
+fun InvitationManagement(
+    modifier: Modifier = Modifier,
+    selectedMenu: InvitationSelectionMenuItems,
+    onMenuClick: (InvitationSelectionMenuItems) -> Unit,
+    receivedInvitations: List<ReceivedInvitationUiModel>,
+    onRejectClick: () -> Unit,
+    onAcceptClick: () -> Unit,
+    sentInvitations: List<SentInvitationUiModel>,
+    onCancelClick: () -> Unit,
+) {
+    Column(modifier = modifier) {
+        DefaultDivider()
+
+        InvitationSelectionMenu(
+            modifier = Modifier.padding(horizontal = 16.dp, vertical = 8.dp),
+            selectedMenu = selectedMenu,
+            onClick = onMenuClick,
+        )
+
+        when (selectedMenu) {
+            RECEIVED_INVITATION -> {
+                ReceivedInvitations(
+                    receivedInvitations = receivedInvitations,
+                    onAcceptClick = onAcceptClick,
+                    onRejectClick = onRejectClick,
+                )
+            }
+
+            SENT_INVITATION -> {
+                SentInvitations(
+                    sentInvitations = sentInvitations,
+                    onCancelClick = onCancelClick,
+                )
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun InvitationManagementPreview(
+    @PreviewParameter(InvitationSelectionMenuProvider::class) menu: InvitationSelectionMenuItems,
+) {
+    InvitationManagement(
+        selectedMenu = menu,
+        onMenuClick = { },
+        receivedInvitations = dummyReceivedInvitationUiModels,
+        onRejectClick = { },
+        onAcceptClick = { },
+        sentInvitations = dummySentInvitationUiModels,
+        onCancelClick = { },
+    )
+}
+
+private class InvitationSelectionMenuProvider(
+    override val values: Sequence<InvitationSelectionMenuItems> = sequenceOf(RECEIVED_INVITATION, SENT_INVITATION)
+) : PreviewParameterProvider<InvitationSelectionMenuItems>
