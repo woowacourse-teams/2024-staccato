66111a0a00980de70b0a35d1cb7c8962ecafbc07
Junyoung-WON <wjyo60@gmail.com>
2025-05-23 18:40:02 +0900

    feat: InvitationManagementActivity Hilt 의존성 주입 적용
    - Hilt로부터 의존성을 주입받을 수 있도록, AndroidEntryPoint 어노테이션 추가

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementActivity.kt
@@ -5,7 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class InvitationManagementActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
