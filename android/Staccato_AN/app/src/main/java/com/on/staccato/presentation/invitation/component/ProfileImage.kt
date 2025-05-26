113767b3c19c8ca819bb363052df3e3466fa4b65
Junyoung-WON <wjyo60@gmail.com>
2025-05-26 18:18:26 +0900

    refactor: 프로필 이미지 원형 처리

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/component/ProfileImage.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/component/ProfileImage.kt
@@ -2,8 +2,10 @@ package com.on.staccato.presentation.invitation.component
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.on.staccato.R
@@ -15,7 +17,7 @@ fun ProfileImage(
     url: String?,
 ) {
     DefaultAsyncImage(
-        modifier = modifier,
+        modifier = modifier.clip(shape = CircleShape),
         bitmapPixelSize = 150,
         url = url,
         placeHolder = R.drawable.icon_member,
