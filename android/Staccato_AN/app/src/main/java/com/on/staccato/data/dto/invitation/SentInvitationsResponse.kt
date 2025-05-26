a622ca4463f4fc45e33ac395d3008845c9c8c02b
Junyoung-WON <wjyo60@gmail.com>
2025-05-26 15:00:49 +0900

    feat: 보낸 초대 응답 DTO 작성
    - SentInvitationDto가 리스트 형태로 오는 SentInvitationsResponse

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/invitation/SentInvitationsResponse.kt
@@ -0,0 +1,9 @@
+package com.on.staccato.data.dto.invitation
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class SentInvitationsResponse(
+    @SerialName("invitations") val invitations: List<SentInvitationDto>
+)
