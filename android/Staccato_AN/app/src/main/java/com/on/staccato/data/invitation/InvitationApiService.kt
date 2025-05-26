e8e2e5708fdce74789a61228c3c008633fc4cdea
Junyoung-WON <wjyo60@gmail.com>
2025-05-26 15:06:11 +0900

    feat: 카테고리 초대 관련 API Service 작성
    - 초대 목록 조회: 받은 초대, 보낸 초대
    - 받은 초대 수락
    - 받은 초대 거절
    - 보낸 초대 취소

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/invitation/InvitationApiService.kt
@@ -0,0 +1,42 @@
+package com.on.staccato.data.invitation
+
+import com.on.staccato.data.dto.invitation.ReceivedInvitationsResponse
+import com.on.staccato.data.dto.invitation.SentInvitationsResponse
+import com.on.staccato.data.network.ApiResult
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface InvitationApiService {
+    @GET(RECEIVED_INVITATION_PATH)
+    fun getReceivedInvitations(): ApiResult<ReceivedInvitationsResponse>
+
+    @POST(INVITATION_ACCEPT_PATH)
+    fun postInvitationAccept(
+        @Path(INVITATION_ID) invitationId: Long,
+    ): ApiResult<Unit>
+
+    @POST(INVITATION_REJECT_PATH)
+    fun postInvitationReject(
+        @Path(INVITATION_ID) invitationId: Long,
+    ): ApiResult<Unit>
+
+    @GET(SENT_INVITATION_PATH)
+    fun getSentInvitations(): ApiResult<SentInvitationsResponse>
+
+    @POST(INVITATION_CANCEL_PATH)
+    fun postInvitationCancel(
+        @Path(INVITATION_ID) invitationId: Long,
+    ): ApiResult<Unit>
+
+    companion object {
+        private const val INVITATION_PATH = "/invitations"
+        private const val INVITATION_ID = "invitationId"
+        private const val RECEIVED_INVITATION_PATH = "$INVITATION_PATH/received"
+        private const val SENT_INVITATION_PATH = "$INVITATION_PATH/sent"
+        private const val INVITATION_PATH_WITH_ID = "$INVITATION_PATH/{$INVITATION_ID}"
+        private const val INVITATION_ACCEPT_PATH = "$INVITATION_PATH_WITH_ID/accept"
+        private const val INVITATION_REJECT_PATH = "$INVITATION_PATH_WITH_ID/reject"
+        private const val INVITATION_CANCEL_PATH = "$INVITATION_PATH_WITH_ID/cancel"
+    }
+}
