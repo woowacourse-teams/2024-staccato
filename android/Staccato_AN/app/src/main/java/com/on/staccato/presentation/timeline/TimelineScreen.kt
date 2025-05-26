ba220ab3631726456fd5e3679220938bf4957717
hxeyexn <kck85075@kumoh.ac.kr>
2025-05-27 00:52:27 +0900

    refactor: TimelineScreen이 lazyListState를 상태 호이스팅 하도록 변경

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/TimelineScreen.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/TimelineScreen.kt
@@ -1,12 +1,19 @@
 package com.on.staccato.presentation.timeline
 
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.snapshotFlow
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.on.staccato.presentation.main.viewmodel.SharedViewModel
 import com.on.staccato.presentation.timeline.component.Timeline
 import com.on.staccato.presentation.timeline.viewmodel.TimelineViewModel
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.map
+
+private const val TIMELINE_TOP_INDEX = 0
 
 @Composable
 fun TimelineScreen(
@@ -15,10 +22,20 @@ fun TimelineScreen(
     onCategoryClicked: (Long) -> Unit,
 ) {
     val timeline by timelineViewModel.timeline.collectAsState()
+    val lazyListState = rememberLazyListState()
+
+    LaunchedEffect(lazyListState) {
+        snapshotFlow { lazyListState.firstVisibleItemIndex }
+            .map { it == TIMELINE_TOP_INDEX }
+            .distinctUntilChanged()
+            .collect {
+                sharedViewModel.updateIsDraggable(it)
+            }
+    }
 
     Timeline(
         timeline = timeline,
         onCategoryClicked = onCategoryClicked,
-        updateIsDraggable = { sharedViewModel.updateIsDraggable(it) },
+        lazyListState = lazyListState,
     )
 }
--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/component/Timeline.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/component/Timeline.kt
@@ -1,40 +1,29 @@
 package com.on.staccato.presentation.timeline.component
 
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.on.staccato.presentation.component.DefaultDivider
 import com.on.staccato.presentation.timeline.model.TimelineUiModel
 import com.on.staccato.presentation.timeline.model.dummyTimelineUiModels
-import kotlinx.coroutines.flow.distinctUntilChanged
-import kotlinx.coroutines.flow.map
-
-private const val TIMELINE_TOP_INDEX = 0
 
 @Composable
 fun Timeline(
     timeline: List<TimelineUiModel>,
     onCategoryClicked: (Long) -> Unit,
-    updateIsDraggable: (Boolean) -> Unit,
+    lazyListState: LazyListState = rememberLazyListState(),
 ) {
-    val listState = rememberLazyListState()
-
-    LaunchedEffect(listState) {
-        snapshotFlow { listState.firstVisibleItemIndex }
-            .map { it == TIMELINE_TOP_INDEX }
-            .distinctUntilChanged()
-            .collect {
-                updateIsDraggable(it)
-            }
-    }
-
-    LazyColumn(state = listState) {
-        items(timeline, key = { it.categoryId }) { timelineCategory ->
+    LazyColumn(
+        state = lazyListState,
+    ) {
+        items(
+            items = timeline,
+            key = { it.categoryId },
+        ) { timelineCategory ->
             TimelineItem(
                 modifier = Modifier.animateItem(),
                 category = timelineCategory,
@@ -51,6 +40,5 @@ private fun TimelinePreview() {
     Timeline(
         timeline = dummyTimelineUiModels,
         onCategoryClicked = {},
-        updateIsDraggable = {},
     )
 }
