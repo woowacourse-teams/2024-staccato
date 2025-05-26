a5c57cb124f288531559c10906f8e08fb5583bbe
Junyoung-WON <wjyo60@gmail.com>
2025-05-26 18:27:33 +0900

    refactor: 보낸 초대, 받은 초대 목록을 업데이트하는 로직 메서드 분리

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/viewmodel/InvitationViewModel.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/viewmodel/InvitationViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.viewModelScope
 import com.on.staccato.data.network.onException2
 import com.on.staccato.data.network.onServerError
 import com.on.staccato.data.network.onSuccess
+import com.on.staccato.domain.model.invitation.ReceivedInvitation
+import com.on.staccato.domain.model.invitation.SentInvitation
 import com.on.staccato.domain.repository.InvitationRepository
 import com.on.staccato.presentation.invitation.model.ReceivedInvitationUiModel
 import com.on.staccato.presentation.invitation.model.SentInvitationUiModel
@@ -35,22 +37,28 @@ class InvitationViewModel
         fun getReceivedInvitations() {
             viewModelScope.launch {
                 val result = invitationRepository.getReceivedInvitations()
-                result.onSuccess { invitations ->
-                    _receivedInvitations.value = invitations.map { it.toUiModel() }
-                }.onServerError {
-                }.onException2 {
-                }
+                result
+                    .onSuccess(::updateReceivedInvitations)
+                    .onServerError {}
+                    .onException2 {}
             }
         }
 
         fun getSentInvitations() {
             viewModelScope.launch {
                 val result = invitationRepository.getSentInvitations()
-                result.onSuccess { invitations ->
-                    _sentInvitations.value = invitations.map { it.toUiModel() }
-                }.onServerError {
-                }.onException2 {
-                }
+                result
+                    .onSuccess(::updateSentInvitations)
+                    .onServerError {}
+                    .onException2 {}
             }
+        }
+
+        private fun updateReceivedInvitations(invitations: List<ReceivedInvitation>) {
+            _receivedInvitations.value = invitations.map { it.toUiModel() }
+        }
+
+        private fun updateSentInvitations(invitations: List<SentInvitation>) {
+            _sentInvitations.value = invitations.map { it.toUiModel() }
         }
     }
