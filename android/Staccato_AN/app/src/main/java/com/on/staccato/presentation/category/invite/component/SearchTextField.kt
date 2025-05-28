eca2d2da256558b551cb8cbef8f01d4ee902bcee
somin <salth0601@gmail.com>
2025-05-28 15:46:48 +0900

    ui: Material3 TextField에서 BasicTextField로 변경
    - default padding 제거를 위함

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/category/invite/component/SearchTextField.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/category/invite/component/SearchTextField.kt
@@ -1,15 +1,20 @@
 package com.on.staccato.presentation.category.invite.component
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
@@ -17,6 +22,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.on.staccato.R
@@ -25,64 +31,92 @@ import com.on.staccato.theme.Body2
 import com.on.staccato.theme.Gray1
 import com.on.staccato.theme.Gray3
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun SearchTextField(
     value: String,
     modifier: Modifier = Modifier,
     placeholderText: String = "",
-    keyboardOptions: KeyboardOptions =
-        KeyboardOptions(
-            keyboardType = KeyboardType.Text,
-            imeAction = ImeAction.Search,
-            showKeyboardOnFocus = true,
-        ),
     onValueChange: (String) -> Unit,
 ) {
-    TextField(
+    val interactionSource = remember { MutableInteractionSource() }
+
+    BasicTextField(
         value = value,
         onValueChange = onValueChange,
         singleLine = true,
-        keyboardOptions = keyboardOptions,
-        leadingIcon = {
-            Icon(
-                painter = painterResource(id = R.drawable.icon_search),
-                contentDescription = stringResource(id = R.string.invite_member_search_description),
-                tint = Gray3,
-            )
-        },
-        trailingIcon = {
-            if (value.isNotEmpty()) {
-                Image(
-                    painter = painterResource(R.drawable.icon_x_circle),
-                    contentDescription = null,
-                    modifier =
-                        Modifier
-                            .alpha(0.3f)
-                            .clickableWithoutRipple(onClick = { onValueChange("") }),
-                )
-            }
-        },
-        placeholder = {
-            Text(
-                text = placeholderText,
-                color = Gray3,
-                style = Body2,
-            )
-        },
-        colors =
-            TextFieldDefaults.colors(
-                unfocusedContainerColor = Gray1,
-                focusedContainerColor = Gray1,
-                unfocusedIndicatorColor = Color.Transparent,
-                focusedIndicatorColor = Color.Transparent,
-                cursorColor = Gray3,
+        interactionSource = interactionSource,
+        keyboardOptions =
+            KeyboardOptions(
+                keyboardType = KeyboardType.Text,
+                imeAction = ImeAction.Search,
             ),
-        shape = RoundedCornerShape(5.dp),
         modifier =
             modifier
                 .fillMaxWidth()
-                .height(46.dp),
+                .height(46.dp)
+                .background(Gray1, RoundedCornerShape(5.dp)),
+        decorationBox = { innerTextField ->
+            TextFieldDefaults.DecorationBox(
+                shape = RoundedCornerShape(5.dp),
+                value = value,
+                visualTransformation = VisualTransformation.None,
+                innerTextField = innerTextField,
+                placeholder = { SearchTextPlaceHolder(placeholderText) },
+                leadingIcon = { SearchLeadingIcon() },
+                trailingIcon = { SearchTrailingIcon(value, onValueChange) },
+                singleLine = true,
+                enabled = true,
+                isError = false,
+                interactionSource = interactionSource,
+                contentPadding =
+                    PaddingValues(12.dp),
+                colors =
+                    TextFieldDefaults.colors(
+                        unfocusedContainerColor = Gray1,
+                        focusedContainerColor = Gray1,
+                        unfocusedIndicatorColor = Color.Transparent,
+                        focusedIndicatorColor = Color.Transparent,
+                        cursorColor = Gray3,
+                    ),
+            )
+        },
     )
+}
+
+@Composable
+private fun SearchTextPlaceHolder(placeholderText: String) {
+    Text(
+        text = placeholderText,
+        color = Gray3,
+        style = Body2,
+    )
+}
+
+@Composable
+private fun SearchLeadingIcon() {
+    Icon(
+        painter = painterResource(id = R.drawable.icon_search),
+        contentDescription = stringResource(id = R.string.invite_member_search_description),
+        tint = Gray3,
+    )
+}
+
+@Composable
+private fun SearchTrailingIcon(
+    value: String,
+    onValueChange: (String) -> Unit,
+) {
+    if (value.isNotEmpty()) {
+        Image(
+            painter = painterResource(R.drawable.icon_x_circle),
+            contentDescription = null,
+            modifier =
+                Modifier
+                    .alpha(0.3f)
+                    .clickableWithoutRipple { onValueChange("") },
+        )
+    }
 }
 
 @Composable
@@ -100,6 +134,16 @@ fun StaccatoSearchEmptyTextFieldPreview() {
 fun StaccatoSearchTextFieldPreview() {
     SearchTextField(
         value = "아주아주 긴 입력값 아주아주 긴 입력값 아주아주 긴 입력값 아주아주 긴 입력값",
+        onValueChange = {},
+        placeholderText = "",
+    )
+}
+
+@Composable
+@Preview(showBackground = true)
+fun EnglishStaccatoSearchTextFieldPreview() {
+    SearchTextField(
+        value = "Hello my name",
         onValueChange = {},
         placeholderText = "",
     )
