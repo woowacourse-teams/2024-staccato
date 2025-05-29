b94c5dd0270b41f3c429940e953a04d9ed9d6302
hxeyexn <kck85075@kumoh.ac.kr>
2025-05-29 19:58:34 +0900

    ui: 프로필 이미지가 border 밖으로 얇게 튀어나오는 문제 해결

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/component/ParticipantItem.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/timeline/component/ParticipantItem.kt
@@ -1,11 +1,16 @@
 package com.on.staccato.presentation.timeline.component
 
-import androidx.compose.foundation.border
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.on.staccato.R
@@ -14,17 +19,23 @@ import com.on.staccato.theme.White
 
 @Composable
 fun ParticipantItem(profileImageUrl: String? = null) {
-    DefaultAsyncImage(
+    Box(
         modifier =
-            Modifier
+            Modifier.size(25.dp)
                 .shadow(2.dp, shape = CircleShape, clip = false)
-                .border(width = 1.dp, color = White, shape = CircleShape)
-                .size(25.dp),
-        bitmapPixelSize = 150,
-        url = profileImageUrl,
-        placeHolder = R.drawable.icon_member,
-        contentDescription = R.string.mates_profile_image_description,
-    )
+                .clip(CircleShape)
+                .background(Color.White)
+                .padding(1.dp),
+    ) {
+        DefaultAsyncImage(
+            modifier = Modifier.clip(CircleShape),
+            bitmapPixelSize = 150,
+            url = profileImageUrl,
+            placeHolder = R.drawable.icon_member,
+            contentDescription = R.string.mates_profile_image_description,
+            contentScale = ContentScale.Crop,
+        )
+    }
 }
 
 @Preview(showBackground = true, backgroundColor = 0xFFFFFFFF)
