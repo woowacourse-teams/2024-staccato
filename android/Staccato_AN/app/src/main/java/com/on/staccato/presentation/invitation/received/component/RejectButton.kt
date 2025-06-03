870414db0fe5a666ec94573f61ebe1451c9ff3fb
Junyoung-WON <wjyo60@gmail.com>
2025-06-03 23:01:18 +0900

    ui: 받은 초대 관리 거절 버튼 문자열 리소스 적용

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/received/component/RejectButton.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/received/component/RejectButton.kt
@@ -5,8 +5,10 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.on.staccato.R
 import com.on.staccato.presentation.component.DefaultTextButton
 import com.on.staccato.theme.Gray2
 import com.on.staccato.theme.StaccatoBlack
@@ -19,7 +21,7 @@ fun RejectButton(
 ) {
     DefaultTextButton(
         modifier = modifier,
-        text = "거절",
+        text = stringResource(id = R.string.invitation_management_reject),
         onClick = onClick,
         backgroundColor = White,
         textColor = StaccatoBlack,
