64c5e0ac0ab28835ad842cc201f16087e7da0aaa
Junyoung-WON <wjyo60@gmail.com>
2025-06-04 03:07:56 +0900

    feat: 토스트 메세지를 띄우는 기능 적용
    - LaunchedEffect 활용
    - 지역변수 순서 수정

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementScreen.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementScreen.kt
@@ -1,22 +1,24 @@
 package com.on.staccato.presentation.invitation
 
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.on.staccato.presentation.component.topbar.DefaultNavigationTopBar
 import com.on.staccato.presentation.invitation.component.InvitationManagementTopBar
 import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuUiModel
 import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuUiModel.RECEIVED_INVITATION
 import com.on.staccato.presentation.invitation.model.InvitationSelectionMenuUiModel.SENT_INVITATION
+import com.on.staccato.presentation.invitation.model.ToastMessage
 import com.on.staccato.presentation.invitation.viewmodel.InvitationViewModel
+import com.on.staccato.presentation.util.showToast
 import com.on.staccato.theme.White
 
 @Composable
@@ -26,10 +28,27 @@ fun InvitationManagementScreen(
     onNavigationClick: () -> Unit,
     defaultSelectedMenu: InvitationSelectionMenuUiModel = RECEIVED_INVITATION,
 ) {
-    var selectedMenu by remember { mutableStateOf(defaultSelectedMenu) }
     val receivedInvitations by invitationViewModel.receivedInvitations.collectAsStateWithLifecycle()
     val sentInvitations by invitationViewModel.sentInvitations.collectAsStateWithLifecycle()
     val dialogState by invitationViewModel.dialogState
+    val context = LocalContext.current
+    var selectedMenu by remember { mutableStateOf(defaultSelectedMenu) }
+
+    LaunchedEffect(Unit) {
+        invitationViewModel.toastMessage.collect {
+            val message = when (it) {
+                is ToastMessage.FromResource -> context.getString(it.messageId)
+                is ToastMessage.Plain -> it.errorMessage
+            }
+            context.showToast(message)
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        invitationViewModel.exceptionState.collect { state ->
+            context.showToast(context.getString(state.messageId))
+        }
+    }
 
     Scaffold(
         containerColor = White,
