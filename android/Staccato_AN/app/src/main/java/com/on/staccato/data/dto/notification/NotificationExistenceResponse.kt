be3944205253a55f571e3836bd90a86f7d4c3f25
Junyoung-WON <wjyo60@gmail.com>
2025-06-05 19:59:08 +0900

    feat: 알림 존재 확인 요청에 따른 응답 객체 추가
    - NotificationExistenceResponse: 알림 존재 여부(isExist: Boolean)

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/dto/notification/NotificationExistenceResponse.kt
@@ -0,0 +1,9 @@
+package com.on.staccato.data.dto.notification
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class NotificationExistenceResponse(
+    @SerialName("isExist") val isExist: Boolean
+)
