e34b7d72da52c46313db963eb529b3865507b678
hxeyexn <kck85075@kumoh.ac.kr>
2025-06-06 12:55:29 +0900

    fix: ErrorResponse 변환 오류 해결

--- android/Staccato_AN/app/src/main/java/com/on/staccato/data/network/ApiResultCallAdapterFactory.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/data/network/ApiResultCallAdapterFactory.kt
@@ -5,24 +5,27 @@ import retrofit2.CallAdapter
 import retrofit2.Retrofit
 import java.lang.reflect.ParameterizedType
 import java.lang.reflect.Type
+import javax.inject.Inject
 
-class ApiResultCallAdapterFactory : CallAdapter.Factory() {
-    override fun get(
-        returnType: Type,
-        annotations: Array<out Annotation>,
-        retrofit: Retrofit,
-    ): CallAdapter<*, *>? {
-        if (getRawType(returnType) != Call::class.java) {
-            return null
+class ApiResultCallAdapterFactory
+    @Inject
+    constructor(private val responseHandler: ResponseHandler) : CallAdapter.Factory() {
+        override fun get(
+            returnType: Type,
+            annotations: Array<out Annotation>,
+            retrofit: Retrofit,
+        ): CallAdapter<*, *>? {
+            if (getRawType(returnType) != Call::class.java) {
+                return null
+            }
+
+            val responseType = getParameterUpperBound(0, returnType as ParameterizedType)
+
+            if (getRawType(responseType) != ApiResult::class.java) {
+                return null
+            }
+
+            val resultType = getParameterUpperBound(0, responseType as ParameterizedType)
+            return ApiResultCallAdapter(resultType, responseHandler)
         }
-
-        val responseType = getParameterUpperBound(0, returnType as ParameterizedType)
-
-        if (getRawType(responseType) != ApiResult::class.java) {
-            return null
-        }
-
-        val resultType = getParameterUpperBound(0, responseType as ParameterizedType)
-        return ApiResultCallAdapter(resultType)
     }
-}
