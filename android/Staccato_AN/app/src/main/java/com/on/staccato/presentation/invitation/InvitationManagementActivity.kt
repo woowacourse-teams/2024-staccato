7d44eef8d79f4e29f44778e4165861a69a206cf1
Junyoung-WON <wjyo60@gmail.com>
2025-06-09 20:35:19 +0900

    feat: 초대가 수락됨을 나타내는 ActivityResult 설정
    - InvitationViewModel 생성
    - InvitationViewModel의 hasInvitationAccepted를 collect하여 Activity의 결과 설정
    - 뒤로가기 버튼 클릭 시 명시적으로 finish() 하도록 설정

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementActivity.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/invitation/InvitationManagementActivity.kt
@@ -4,13 +4,29 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.addCallback
 import androidx.activity.compose.setContent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import com.on.staccato.presentation.invitation.viewmodel.InvitationViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class InvitationManagementActivity : ComponentActivity() {
+    private val invitationViewModel by viewModels<InvitationViewModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        showInvitationManagementScreen()
+        collectHasInvitationAccepted()
+        finishOnBackPressed()
+    }
+
+    private fun showInvitationManagementScreen() {
         setContent {
             InvitationManagementScreen(
                 onNavigationClick = { finish() },
@@ -18,7 +34,25 @@ class InvitationManagementActivity : ComponentActivity() {
         }
     }
 
+    private fun collectHasInvitationAccepted() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                invitationViewModel.hasInvitationAccepted.collect { hasInvitationAccepted ->
+                    if (hasInvitationAccepted) setResult(RESULT_INVITATION_ACCEPTED)
+                }
+            }
+        }
+    }
+
+    private fun finishOnBackPressed() {
+        onBackPressedDispatcher.addCallback(this) {
+            finish()
+        }
+    }
+
     companion object {
+        const val RESULT_INVITATION_ACCEPTED = 100
+
         fun launch(context: Context) {
             context.startActivity(Intent(context, InvitationManagementActivity::class.java))
         }
