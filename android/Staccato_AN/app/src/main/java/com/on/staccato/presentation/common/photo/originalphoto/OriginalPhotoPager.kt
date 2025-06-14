0837a5e89bc601c204bfb453d6ba62cd20f2f4a8
Junyoung-WON <wjyo60@gmail.com>
2025-06-14 23:23:21 +0900

    feat: PinchToZoomView를 Pager에 적용
    - 스크롤 가능 여부를 기억하는 scrollable State

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/photo/originalphoto/OriginalPhotoPager.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/photo/originalphoto/OriginalPhotoPager.kt
@@ -5,11 +5,17 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
 import com.on.staccato.R
+import com.on.staccato.presentation.component.DEFAULT_MIN_ZOOM_SCALE
 import com.on.staccato.presentation.component.DefaultAsyncImage
+import com.on.staccato.presentation.component.PinchToZoomView
 import com.on.staccato.theme.Black
 
 @Composable
@@ -23,19 +29,26 @@ fun OriginalPhotoPager(
             initialPage = initialPage,
             pageCount = { imageUrls.size },
         )
+    var scrollable by remember { mutableStateOf(true) }
+
     HorizontalPager(
         state = pagerState,
         modifier =
-            modifier
-                .fillMaxSize()
-                .background(Black),
+        modifier
+            .fillMaxSize()
+            .background(Black),
+        userScrollEnabled = scrollable,
     ) { page ->
-        DefaultAsyncImage(
-            modifier = Modifier.fillMaxSize(),
-            url = imageUrls[page],
-            contentDescription = R.string.all_original_photo,
-            contentScale = ContentScale.Fit,
-        )
+        PinchToZoomView(
+            onScaleChange = { scrollable = it == DEFAULT_MIN_ZOOM_SCALE }
+        ) {
+            DefaultAsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                url = imageUrls[page],
+                contentDescription = R.string.all_original_photo,
+                contentScale = ContentScale.Fit,
+            )
+        }
     }
 }
 
