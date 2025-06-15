c44bd198f2a778fc2069930e8e8834eadff026f8
Junyoung-WON <wjyo60@gmail.com>
2025-06-15 16:12:44 +0900

    feat: 원본 이미지 조회 화면에서 사용할 TopBar 구현
    - AnimatedVisibility로 가시성 제어 및 애니메이션 적용

---
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/photo/originalphoto/OriginalPhotoTopBar.kt
@@ -0,0 +1,73 @@
+package com.on.staccato.presentation.common.photo.originalphoto
+
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.slideInVertically
+import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.on.staccato.presentation.component.topbar.DefaultNavigationTopBar
+import com.on.staccato.presentation.component.topbar.DefaultTopAppBarColors
+import com.on.staccato.theme.TranslucentBlack
+import com.on.staccato.theme.White
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun OriginalPhotoTopBar(
+    isVisible: Boolean,
+    title: String? = null,
+    onNavigationClick: () -> Unit
+) {
+    AnimatedVisibility(
+        visible = isVisible,
+        enter = slideInVertically(
+            initialOffsetY = { -it },
+            animationSpec = tween(durationMillis = 300)
+        ),
+        exit = slideOutVertically(
+            targetOffsetY = { -it },
+            animationSpec = tween(durationMillis = 300)
+        )
+    ) {
+        DefaultNavigationTopBar(
+            title = title,
+            onNavigationClick = onNavigationClick,
+            colors = DefaultTopAppBarColors.copy(
+                containerColor = TranslucentBlack,
+                navigationIconContentColor = White,
+                titleContentColor = White,
+            ),
+        )
+    }
+}
+
+@Preview(name = "제목이 있는 경우", showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun OriginalPhotoTopBarWithTitlePreview() {
+    Column {
+        OriginalPhotoTopBar(
+            isVisible = true,
+            title = "반투명한 상단 바",
+            onNavigationClick = {},
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+    }
+}
+
+@Preview(name = "제목이 없는 경우", showBackground = true, backgroundColor = 0xFFFFFF)
+@Composable
+private fun OriginalPhotoTopBarWithoutTitlePreview() {
+    Column {
+        OriginalPhotoTopBar(
+            isVisible = true,
+            onNavigationClick = {},
+        )
+        Spacer(modifier = Modifier.height(30.dp))
+    }
+}
