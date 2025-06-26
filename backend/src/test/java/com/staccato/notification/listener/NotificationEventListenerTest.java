a76a95dda829c5d80ed26e88f3fb50fe6ac8c9fe
Seongju Lee <sherlocky31415@gmail.com>
2025-06-26 17:06:53 +0900

    feat: 행위 대상자에게는 알림이 가지 않도록 수정 #915 (#921)
    * refactor: 스타카토를 만든 멤버에게는 새로운 스타카토 알림을 보내지 않는다.
    
    * refactor: 초대를 수락한 멤버에게는 초대 수락 알림을 보내지 않는다.
    
    * refactor: 댓글을 작성한 멤버에게는 새로운 댓글 알림을 보내지 않는다.
    
    * fix: 로직 변화에 따른 테스트 데이터 수정
    
    * refactor: slice test 삭제 및 원래 테스트 내용과 통합
    
    * refactor: 저장 후 publishEvent 실행
    
    * refactor: 메서드 리팩토링

--- backend/src/test/java/com/staccato/notification/listener/NotificationEventListenerTest.java
+++ backend/src/test/java/com/staccato/notification/listener/NotificationEventListenerTest.java
@@ -1,18 +1,25 @@
 package com.staccato.notification.listener;
 
+import static org.mockito.Mockito.argThat;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 import java.time.Duration;
 import java.util.List;
 import java.util.Set;
+
 import org.awaitility.Awaitility;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.mock.mockito.SpyBean;
+
 import com.staccato.ServiceSliceTest;
 import com.staccato.category.domain.Category;
 import com.staccato.category.repository.CategoryRepository;
+import com.staccato.comment.domain.Comment;
+import com.staccato.comment.repository.CommentRepository;
 import com.staccato.comment.service.CommentService;
 import com.staccato.comment.service.dto.request.CommentRequest;
 import com.staccato.fixture.category.CategoryFixtures;
@@ -26,22 +33,20 @@ import com.staccato.invitation.service.InvitationService;
 import com.staccato.invitation.service.dto.request.CategoryInvitationRequest;
 import com.staccato.member.domain.Member;
 import com.staccato.member.repository.MemberRepository;
-import com.staccato.notification.service.FcmService;
 import com.staccato.notification.service.NotificationService;
 import com.staccato.staccato.domain.Staccato;
 import com.staccato.staccato.repository.StaccatoRepository;
 import com.staccato.staccato.service.StaccatoService;
 import com.staccato.staccato.service.dto.request.StaccatoRequest;
 
-import static org.mockito.Mockito.argThat;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-
 class NotificationEventListenerTest extends ServiceSliceTest {
 
     @Autowired
     private StaccatoService staccatoService;
+    @Autowired
+    private CommentService commentService;
+    @Autowired
+    private InvitationService invitationService;
     @Autowired
     private MemberRepository memberRepository;
     @Autowired
@@ -49,87 +54,110 @@ class NotificationEventListenerTest extends ServiceSliceTest {
     @Autowired
     private CategoryInvitationRepository categoryInvitationRepository;
     @Autowired
-    private CommentService commentService;
-    @Autowired
     private StaccatoRepository staccatoRepository;
     @Autowired
-    private InvitationService invitationService;
+    private CommentRepository commentRepository;
     @SpyBean
     private NotificationService notificationService;
-    @MockBean
-    private FcmService fcmService;
 
-    private Member host;
-    private Member guest;
-    private Member guest2;
-    private Category category;
-
-    @BeforeEach
-    void init() {
-        host = MemberFixtures.defaultMember().withNickname("host").buildAndSave(memberRepository);
-        guest = MemberFixtures.defaultMember().withNickname("guest").buildAndSave(memberRepository);
-        guest2 = MemberFixtures.defaultMember().withNickname("guest2").buildAndSave(memberRepository);
-        category = CategoryFixtures.defaultCategory()
-                .withHost(host)
-                .withGuests(List.of(guest, guest2))
-                .buildAndSave(categoryRepository);
-    }
-
-    @DisplayName("공유 카테고리에서 Staccato 생성 시 알림을 전송한다.")
+    @DisplayName("공동카테고리에서 Staccato 생성 시 스타카토 생성자를 제외한 함께하는 사람들 모두에게 알림을 전송한다.")
     @Test
     void handleNewStaccato() {
         // given
-        StaccatoRequest request = StaccatoRequestFixtures.defaultStaccatoRequest()
-                .withCategoryId(category.getId())
-                .build();
+        Member host = MemberFixtures.defaultMember()
+                .withNickname("host").buildAndSave(memberRepository);
+        Member guest = MemberFixtures.defaultMember()
+                .withNickname("guest").buildAndSave(memberRepository);
+        Member staccatoCreator = MemberFixtures.defaultMember()
+                .withNickname("creator").buildAndSave(memberRepository);
+
+        Category category = CategoryFixtures.defaultCategory()
+                .withHost(host)
+                .withGuests(List.of(guest, staccatoCreator))
+                .buildAndSave(categoryRepository);
+
+        StaccatoRequest staccatoRequest = StaccatoRequestFixtures.defaultStaccatoRequest()
+                .withCategoryId(category.getId()).build();
 
         // when
-        staccatoService.createStaccato(request, host);
+        staccatoService.createStaccato(staccatoRequest, staccatoCreator);
 
         // then
         Awaitility.await().atMost(Duration.ofSeconds(2)).untilAsserted(() ->
                 verify(notificationService, times(1))
                         .sendNewStaccatoAlert(
-                                eq(host),
+                                eq(staccatoCreator),
                                 eq(category),
-                                argThat(members -> members.containsAll(List.of(guest, guest2)))
+                                argThat(targets ->
+                                        targets.size() == 2 &&
+                                                !targets.contains(staccatoCreator) &&
+                                                targets.containsAll(List.of(host, guest))
+                                )
                         )
         );
     }
 
-    @DisplayName("공유 카테고리에서 댓글 작성 시 알림을 전송한다.")
+    @DisplayName("공동카테고리에서 댓글 작성 시 댓글 생성자를 제외한 함께하는 사람들 모두에게 알림을 전송한다.")
     @Test
     void handleNewComment() {
         // given
+        Member host = MemberFixtures.defaultMember()
+                .withNickname("host").buildAndSave(memberRepository);
+        Member guest = MemberFixtures.defaultMember()
+                .withNickname("guest").buildAndSave(memberRepository);
+        Member commentCreator = MemberFixtures.defaultMember()
+                .withNickname("creator").buildAndSave(memberRepository);
+
+        Category category = CategoryFixtures.defaultCategory()
+                .withHost(host)
+                .withGuests(List.of(guest, commentCreator))
+                .buildAndSave(categoryRepository);
         Staccato staccato = StaccatoFixtures.defaultStaccato()
                 .withCategory(category)
                 .buildAndSave(staccatoRepository);
+
         CommentRequest commentRequest = CommentRequestFixtures.defaultCommentRequest()
                 .withStaccatoId(staccato.getId()).build();
 
         // when
-        commentService.createComment(commentRequest, host);
+        Long commentId = commentService.createComment(commentRequest, commentCreator);
 
         // then
+        Comment comment = commentRepository.findById(commentId).get();
+
         Awaitility.await().atMost(Duration.ofSeconds(2)).untilAsserted(() ->
                 verify(notificationService, times(1))
-                        .sendNewCommentAlert(eq(host),
-                                argThat(comment -> comment.getContent().equals(commentRequest.content())),
-                                argThat(m -> m.containsAll(List.of(guest, guest2, host))))
+                        .sendNewCommentAlert(
+                                eq(commentCreator),
+                                eq(comment),
+                                argThat(targets ->
+                                        targets.size() == 2 &&
+                                                !targets.contains(commentCreator) &&
+                                                targets.containsAll(List.of(host, guest))
+                                )
+                        )
         );
     }
 
-    @DisplayName("카테고리 초대 시 알림을 전송한다.")
+    @DisplayName("공동카테고리 초대 시 초대받은 사람들에게 알림을 전송한다.")
     @Test
     void handleInvitation() {
         // given
-        Member invitee = MemberFixtures.defaultMember()
-                .withNickname("invitee")
-                .buildAndSave(memberRepository);
+        Member host = MemberFixtures.defaultMember()
+                .withNickname("host").buildAndSave(memberRepository);
+        Member guest = MemberFixtures.defaultMember()
+                .withNickname("guest").buildAndSave(memberRepository);
+        Member invitee1 = MemberFixtures.defaultMember()
+                .withNickname("invitee1").buildAndSave(memberRepository);
         Member invitee2 = MemberFixtures.defaultMember()
-                .withNickname("invitee2")
-                .buildAndSave(memberRepository);
-        CategoryInvitationRequest invitationRequest = new CategoryInvitationRequest(category.getId(), Set.of(invitee.getId(), invitee2.getId()));
+                .withNickname("invitee2").buildAndSave(memberRepository);
+
+        Category category = CategoryFixtures.defaultCategory()
+                .withHost(host)
+                .withGuests(List.of(guest))
+                .buildAndSave(categoryRepository);
+
+        CategoryInvitationRequest invitationRequest = new CategoryInvitationRequest(category.getId(), Set.of(invitee1.getId(), invitee2.getId()));
 
         // when
         invitationService.invite(host, invitationRequest);
@@ -137,17 +165,32 @@ class NotificationEventListenerTest extends ServiceSliceTest {
         // then
         Awaitility.await().atMost(Duration.ofSeconds(2)).untilAsserted(() ->
                 verify(notificationService, times(1))
-                        .sendInvitationAlert(eq(host), argThat(list -> list.containsAll(List.of(invitee, invitee2))), eq(category))
+                        .sendInvitationAlert(
+                                eq(host),
+                                argThat(targets ->
+                                        targets.size() == 2 &&
+                                                targets.containsAll(List.of(invitee1, invitee2))),
+                                eq(category)
+                        )
         );
     }
 
-    @DisplayName("카테고리 초대 수락 시 기존 멤버에게 알림을 전송한다.")
+    @DisplayName("카테고리 초대 수락 시 초대 수락자를 제외한 함께하는 사람들 모두에게 알림을 전송한다.")
     @Test
     void handleInvitationAccepted() {
         // given
+        Member host = MemberFixtures.defaultMember()
+                .withNickname("host").buildAndSave(memberRepository);
+        Member guest = MemberFixtures.defaultMember()
+                .withNickname("guest").buildAndSave(memberRepository);
         Member invitee = MemberFixtures.defaultMember()
-                .withNickname("invitee")
-                .buildAndSave(memberRepository);
+                .withNickname("invitee").buildAndSave(memberRepository);
+
+        Category category = CategoryFixtures.defaultCategory()
+                .withHost(host)
+                .withGuests(List.of(guest))
+                .buildAndSave(categoryRepository);
+
         CategoryInvitation categoryInvitation = CategoryInvitation.invite(category, host, invitee);
         categoryInvitationRepository.save(categoryInvitation);
 
@@ -157,7 +200,15 @@ class NotificationEventListenerTest extends ServiceSliceTest {
         // then
         Awaitility.await().atMost(Duration.ofSeconds(2)).untilAsserted(() ->
                 verify(notificationService, times(1))
-                        .sendAcceptAlert(eq(invitee), eq(category), argThat(m -> m.containsAll(List.of(guest, guest2, host))))
+                        .sendAcceptAlert(
+                                eq(invitee),
+                                eq(category),
+                                argThat(targets ->
+                                        targets.size() == 2 &&
+                                                !targets.contains(invitee) &&
+                                                targets.containsAll(List.of(host, guest))
+                                )
+                        )
         );
     }
 }
