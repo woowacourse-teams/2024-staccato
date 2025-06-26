b1cc856aa5e8b5a6f9a27a5f624d0128a02e0628
somin <salth0601@gmail.com>
2025-06-26 17:24:01 +0900

    feat: 포그라운드에서 수신된 알림 클릭 시 앱이 실행되도록 pendingIntent 설정

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/notification/NotificationManager.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/notification/NotificationManager.kt
@@ -3,10 +3,14 @@ package com.on.staccato.presentation.common.notification
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.on.staccato.R
+import com.on.staccato.presentation.login.LoginActivity
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -45,7 +49,20 @@ class NotificationManager
             .setSmallIcon(R.mipmap.ic_launcher_round)
             .setContentTitle(title)
             .setContentText(body)
+            .setContentIntent(createActivityPendingIntent())
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
             .build()
+
+        private fun createActivityPendingIntent(): PendingIntent {
+            val intent = Intent(context, LoginActivity::class.java)
+            return PendingIntent.getActivity(context, 0, intent, pendingIntentFlags)
+        }
+
+        private val pendingIntentFlags =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            } else {
+                PendingIntent.FLAG_UPDATE_CURRENT
+            }
     }
