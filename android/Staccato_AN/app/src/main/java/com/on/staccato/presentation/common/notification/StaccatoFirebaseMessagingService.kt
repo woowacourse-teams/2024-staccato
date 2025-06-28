9d3c49c3537d835600074e712535a5162cb64e11
somin <salth0601@gmail.com>
2025-06-28 19:59:06 +0900

    feat: FCM 수신 시 알림을 제거해 항상 onMessageReceived가 호출되도록 수정

--- android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/notification/StaccatoFirebaseMessagingService.kt
+++ android/Staccato_AN/app/src/main/java/com/on/staccato/presentation/common/notification/StaccatoFirebaseMessagingService.kt
@@ -1,5 +1,7 @@
 package com.on.staccato.presentation.common.notification
 
+import android.content.Intent
+import com.google.firebase.messaging.Constants.MessageNotificationKeys
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.on.staccato.domain.repository.NotificationRepository
@@ -29,6 +31,16 @@ class StaccatoFirebaseMessagingService :
         }
     }
 
+    override fun handleIntent(intent: Intent?) {
+        val bundleWithoutNotification =
+            intent?.extras?.apply {
+                remove(MessageNotificationKeys.ENABLE_NOTIFICATION)
+                remove(ENABLE_NOTIFICATION)
+            }
+        intent?.replaceExtras(bundleWithoutNotification)
+        super.handleIntent(intent)
+    }
+
     override fun onMessageReceived(message: RemoteMessage) {
         super.onMessageReceived(message)
         val title = message.notification?.title
@@ -41,5 +53,9 @@ class StaccatoFirebaseMessagingService :
     override fun onDestroy() {
         _job?.cancel()
         super.onDestroy()
+    }
+
+    companion object {
+        const val ENABLE_NOTIFICATION = "gcm.notification.e"
     }
 }
