ead5855a1b5d8d74abc86993e609634b7517dfa1
Somin Lee <salth0601@gmail.com>
2025-07-04 16:25:52 +0900

    feat: FCM 알림 클릭 시 화면 이동 #936 (#950)
    * refactor: FCM 브랜치 멀티 모듈 대응
    
    * refactor: 서비스 내부의 job 프로퍼티 제거

--- android/Staccato_AN/presentation/src/main/java/com/on/staccato/presentation/notification/StaccatoFirebaseMessagingService.kt
+++ android/Staccato_AN/presentation/src/main/java/com/on/staccato/presentation/notification/StaccatoFirebaseMessagingService.kt
@@ -1,13 +1,16 @@
 package com.on.staccato.presentation.notification
 
+import android.content.Intent
+import com.google.firebase.messaging.Constants.MessageNotificationKeys
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 import com.on.staccato.domain.repository.NotificationRepository
+import com.on.staccato.presentation.notification.model.NotificationType
+import com.on.staccato.presentation.notification.model.StaccatoNotification
+import com.on.staccato.util.launchOnce
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
@@ -19,27 +22,34 @@ class StaccatoFirebaseMessagingService :
     @Inject
     lateinit var staccatoNotificationManager: StaccatoNotificationManager
 
-    private var _job: Job? = null
-    private val job get() = requireNotNull(_job)
-
     override fun onNewToken(token: String) {
-        _job = SupervisorJob()
-        CoroutineScope(job).launch {
+        CoroutineScope(SupervisorJob()).launchOnce {
             notificationRepository.updateFcmToken(token)
         }
     }
 
-    override fun onMessageReceived(message: RemoteMessage) {
-        super.onMessageReceived(message)
-        val title = message.notification?.title
-        val body = message.notification?.body
-        if (title.isNullOrBlank() || body.isNullOrBlank()) return
-
-        staccatoNotificationManager.notify(title = title, body = body)
+    override fun handleIntent(intent: Intent?) {
+        val bundleWithoutNotification =
+            intent?.extras?.apply {
+                remove(MessageNotificationKeys.ENABLE_NOTIFICATION)
+                remove(ENABLE_NOTIFICATION)
+            }
+        intent?.replaceExtras(bundleWithoutNotification)
+        super.handleIntent(intent)
     }
 
-    override fun onDestroy() {
-        _job?.cancel()
-        super.onDestroy()
+    override fun onMessageReceived(message: RemoteMessage) {
+        super.onMessageReceived(message)
+        val data = message.data
+        if (data.isEmpty()) return
+        val typeValue = data[KEY_TYPE] ?: return
+        val type = NotificationType.from(typeValue) ?: return
+        val staccatoNotification = StaccatoNotification.from(type, data) ?: return
+        staccatoNotificationManager.notify(staccatoNotification)
+    }
+
+    companion object {
+        private const val KEY_TYPE = "type"
+        private const val ENABLE_NOTIFICATION = "gcm.notification.e"
     }
 }
--- android/Staccato_AN/presentation/src/main/java/com/on/staccato/presentation/notification/StaccatoNotificationManager.kt
+++ android/Staccato_AN/presentation/src/main/java/com/on/staccato/presentation/notification/StaccatoNotificationManager.kt
@@ -2,14 +2,12 @@ package com.on.staccato.presentation.notification
 
 import android.annotation.SuppressLint
 import android.app.NotificationChannel
-import android.app.PendingIntent
 import android.content.Context
-import android.content.Intent
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.on.staccato.presentation.R
-import com.on.staccato.presentation.login.LoginActivity
+import com.on.staccato.presentation.notification.model.ChannelType.Companion.toChannel
+import com.on.staccato.presentation.notification.model.StaccatoNotification
 import dagger.hilt.android.qualifiers.ApplicationContext
 import javax.inject.Inject
 
@@ -20,41 +18,23 @@ class StaccatoNotificationManager
         private val permissionLauncher: NotificationPermissionManager,
     ) {
         @SuppressLint("MissingPermission")
-        fun notify(
-            title: String,
-            body: String,
-        ) {
+        fun notify(staccatoNotification: StaccatoNotification) {
             if (permissionLauncher.isNotificationUnavailable()) return
-
-            val channel = NotificationChannelType.toChannel(context, title)
-            val notification = createNotification(channel, title, body)
             val notificationId = System.currentTimeMillis().toInt()
-
+            val channel = staccatoNotification.channelType.toChannel(context)
+            val notification = createNotification(staccatoNotification, channel)
             NotificationManagerCompat.from(context).notify(notificationId, notification)
         }
 
         private fun createNotification(
+            notification: StaccatoNotification,
             channel: NotificationChannel,
-            title: String,
-            body: String,
         ) = NotificationCompat.Builder(context, channel.id)
             .setSmallIcon(R.mipmap.ic_launcher_round)
-            .setContentTitle(title)
-            .setContentText(body)
-            .setContentIntent(createActivityPendingIntent())
+            .setContentTitle(notification.title)
+            .setContentText(notification.body)
+            .setContentIntent(notification.createIntent(context))
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
             .build()
-
-        private fun createActivityPendingIntent(): PendingIntent {
-            val intent = Intent(context, LoginActivity::class.java)
-            return PendingIntent.getActivity(context, 0, intent, pendingIntentFlags)
-        }
-
-        private val pendingIntentFlags =
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
-            } else {
-                PendingIntent.FLAG_UPDATE_CURRENT
-            }
     }
