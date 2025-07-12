3fd06338e53c9bd30841088ce5a8c9fe6c7eabf6
hxeyexn <kck85075@kumoh.ac.kr>
2025-07-13 03:59:48 +0900

    refactor: 카테고리 목록 아이템의 스타카토 개수 마커 아이콘 XML 파일을 Kotlin으로 변경 (성능 향상)
    [스타카토 마커 개수 컴포저블 렌더링 시간]
    - 변경 전: 0.334188
    - 변경 후: 0.107048
    - 약 0.22714ms 감소 (약 67.99% 개선)

--- android/Staccato_AN/presentation/src/main/java/com/on/staccato/theme/Icons.kt
+++ android/Staccato_AN/presentation/src/main/java/com/on/staccato/theme/Icons.kt
@@ -4,4 +4,5 @@ import androidx.compose.ui.graphics.vector.ImageVector
 
 object Icons {
     val Folder: ImageVector by lazy { buildFolder() }
+    val StaccatoCountMarker: ImageVector by lazy { buildStaccatoCountMarker() }
 }
