6df3220c5dd2d472fd86bc3af30828d32f06550f
Junyoung-WON <wjyo60@gmail.com>
2025-10-29 18:29:53 +0900

    chore: versionCode, versionName 수정
    - versionCode = 15 -> 16
    - versionName = "2.1.2" -> "2.1.3"

--- android/Staccato_AN/app/build.gradle.kts
+++ android/Staccato_AN/app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.on.staccato"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "2.1.2"
+        versionCode = 16
+        versionName = "2.1.3"
 
         testInstrumentationRunner = "com.on.staccato.HiltTestRunner"
 
